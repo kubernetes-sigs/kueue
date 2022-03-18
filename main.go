b3ebcc58952bb0762398fb58bdbe413145840a00
Carlos Eduardo Arango Gutierrez <carangog@redhat.com>
2022-03-18 15:29:00 -0400

    Add schedulingv1 to scheme
    Signed-off-by: Carlos Eduardo Arango Gutierrez <carangog@redhat.com>

--- main.go
+++ main.go
@@ -24,6 +24,7 @@ import (
 	// to ensure that exec-entrypoint and run can make use of them.
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 
+	schedulingv1 "k8s.io/api/scheduling/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
@@ -48,6 +49,7 @@ var (
 
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+	utilruntime.Must(schedulingv1.AddToScheme(scheme))
 
 	utilruntime.Must(kueuev1alpha1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
