f24c63accaad461dfe582b21819dbf3a5d75dd60
Kante Yin <kerthcet@gmail.com>
2022-11-23 01:14:14 +0800

    Add requeue reason to log (#430)
    * Revise the annotations
    
    Signed-off-by: kerthcet <kerthcet@gmail.com>
    
    * Add requeue reason to log
    
    Signed-off-by: kerthcet <kerthcet@gmail.com>
    
    Signed-off-by: kerthcet <kerthcet@gmail.com>

--- pkg/scheduler/scheduler.go
+++ pkg/scheduler/scheduler.go
@@ -100,8 +100,7 @@ func (s *Scheduler) schedule(ctx context.Context) {
 	// 2. Take a snapshot of the cache.
 	snapshot := s.cache.Snapshot()
 
-	// 3. Calculate requirements for admitting workloads (resource flavors, borrowing).
-	// (resource flavors, borrowing).
+	// 3. Calculate requirements (resource flavors, borrowing) for admitting workloads.
 	entries := s.nominate(ctx, headWorkloads, snapshot)
 
 	// 4. Sort entries based on borrowing and timestamps.
@@ -563,7 +562,7 @@ func (s *Scheduler) requeueAndUpdate(log logr.Logger, ctx context.Context, e ent
 		e.requeueReason = queue.RequeueReasonFailedAfterNomination
 	}
 	added := s.queues.RequeueWorkload(ctx, &e.Info, e.requeueReason)
-	log.V(2).Info("Workload re-queued", "workload", klog.KObj(e.Obj), "clusterQueue", e.ClusterQueue, "queue", klog.KRef(e.Obj.Namespace, e.Obj.Spec.QueueName), "added", added, "status", e.status)
+	log.V(2).Info("Workload re-queued", "workload", klog.KObj(e.Obj), "clusterQueue", e.ClusterQueue, "queue", klog.KRef(e.Obj.Namespace, e.Obj.Spec.QueueName), "requeueReason", e.requeueReason, "added", added)
 
 	if e.status == notNominated {
 		err := workload.UpdateStatus(ctx, s.client, e.Obj, kueue.WorkloadAdmitted, metav1.ConditionFalse, "Pending", e.inadmissibleMsg)
