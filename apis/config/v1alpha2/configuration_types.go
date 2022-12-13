cfe1f31895e6d413bef3496df949765ff76cc166
Michal Wozniak <mw219725@gmail.com>
2022-12-13 09:11:35 +0100

    Remarks

--- apis/config/v1alpha2/configuration_types.go
+++ apis/config/v1alpha2/configuration_types.go
@@ -46,13 +46,16 @@ type Configuration struct {
 	// InternalCertManagement is configuration for internalCertManagement
 	InternalCertManagement *InternalCertManagement `json:"internalCertManagement,omitempty"`
 
-	// WaitForPodsReady is configuration for waitForPodsReady
+	// WaitForPodsReady is configuration to provide simple all-or-nothing
+	// scheduling semantics for jobs to ensure they get resources assigned.
+	// This is achieved by blocking the start of new jobs until the previously
+	// started job has all pods running (ready).
 	WaitForPodsReady *WaitForPodsReady `json:"waitForPodsReady,omitempty"`
 }
 
 type WaitForPodsReady struct {
 	// Enable when true, indicates that each admitted workload
-	// blocks admission of other workloads in the cluster, until it is in the
+	// blocks the admission of all other workloads from all queues until it is in the
 	// `PodsReady` condition. If false, all workloads start as soon as they are
 	// admitted and do not block admission of other workloads. The PodsReady
 	// condition is only added if this setting is enabled. It defaults to false.
