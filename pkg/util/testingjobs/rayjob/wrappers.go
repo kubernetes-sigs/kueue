21af2e219cd380d0231df707af6e2cc5993c9451
Traian Schiau <traian_schiau@epam.com>
2023-04-04 10:38:22 +0300

    [rayjob] Webhook unit tests

---
+++ pkg/util/testingjobs/rayjob/wrappers.go
@@ -0,0 +1,91 @@
+/*
+Copyright 2023 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package rayjob
+
+import (
+	rayjobapi "github.com/ray-project/kuberay/ray-operator/apis/ray/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"sigs.k8s.io/kueue/pkg/controller/jobframework"
+)
+
+// JobWrapper wraps a RayJob.
+type JobWrapper struct{ rayjobapi.RayJob }
+
+// MakeJob creates a wrapper for a suspended rayJob
+func MakeJob(name, ns string) *JobWrapper {
+	return &JobWrapper{rayjobapi.RayJob{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        name,
+			Namespace:   ns,
+			Annotations: make(map[string]string, 1),
+		},
+		Spec: rayjobapi.RayJobSpec{
+			ShutdownAfterJobFinishes: true,
+			RayClusterSpec: &rayjobapi.RayClusterSpec{
+				HeadGroupSpec: rayjobapi.HeadGroupSpec{
+					Template: corev1.PodTemplateSpec{
+						Spec: corev1.PodSpec{},
+					},
+				},
+				WorkerGroupSpecs: []rayjobapi.WorkerGroupSpec{},
+			},
+			Suspend: true,
+		},
+	}}
+}
+
+// Obj returns the inner Job.
+func (j *JobWrapper) Obj() *rayjobapi.RayJob {
+	return &j.RayJob
+}
+
+// Suspend updates the suspend status of the job
+func (j *JobWrapper) Suspend(s bool) *JobWrapper {
+	j.Spec.Suspend = s
+	return j
+}
+
+// Queue updates the queue name of the job
+func (j *JobWrapper) Queue(queue string) *JobWrapper {
+	if j.Labels == nil {
+		j.Labels = make(map[string]string)
+	}
+	j.Labels[jobframework.QueueLabel] = queue
+	return j
+}
+
+func (j *JobWrapper) ShutdownAfterJobFinishes(value bool) *JobWrapper {
+	j.Spec.ShutdownAfterJobFinishes = value
+	return j
+}
+
+func (j *JobWrapper) ClusterSelector(value map[string]string) *JobWrapper {
+	j.Spec.ClusterSelector = value
+	return j
+}
+
+func (j *JobWrapper) WithEnableAutoscaling(value *bool) *JobWrapper {
+	j.Spec.RayClusterSpec.EnableInTreeAutoscaling = value
+	return j
+}
+
+func (j *JobWrapper) WithWorkerGroups(workers ...rayjobapi.WorkerGroupSpec) *JobWrapper {
+	j.Spec.RayClusterSpec.WorkerGroupSpecs = workers
+	return j
+}
