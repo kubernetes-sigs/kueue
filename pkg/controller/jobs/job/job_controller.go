585a241dd2d5b9a6df190549ec7fef52d35e41bc
Yuki Iwai <yuki.iwai.tz@gmail.com>
2023-05-23 19:35:30 +0900

    Add permission to update frameworkjob status
    Signed-off-by: Yuki Iwai <yuki.iwai.tz@gmail.com>

--- pkg/controller/jobs/job/job_controller.go
+++ pkg/controller/jobs/job/job_controller.go
@@ -268,7 +268,7 @@ func SetupIndexes(ctx context.Context, indexer client.FieldIndexer) error {
 //+kubebuilder:rbac:groups=scheduling.k8s.io,resources=priorityclasses,verbs=list;get;watch
 //+kubebuilder:rbac:groups="",resources=events,verbs=create;watch;update;patch
 //+kubebuilder:rbac:groups=batch,resources=jobs,verbs=get;list;watch;update;patch
-//+kubebuilder:rbac:groups=batch,resources=jobs/status,verbs=get
+//+kubebuilder:rbac:groups=batch,resources=jobs/status,verbs=get;update
 //+kubebuilder:rbac:groups=batch,resources=jobs/finalizers,verbs=get;update;patch
 //+kubebuilder:rbac:groups=kueue.x-k8s.io,resources=workloads,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=kueue.x-k8s.io,resources=workloads/status,verbs=get;update;patch
