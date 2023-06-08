c9b6aef817257ccf136772a8071512054104bb5a
Traian Schiau <traian_schiau@epam.com>
2023-06-08 16:36:35 +0300

    [util/map] Add merge functions

--- pkg/util/maps/maps.go
+++ pkg/util/maps/maps.go
@@ -18,7 +18,7 @@ limitations under the License.
 // when `x/exp/maps` graduates to stable.
 package maps
 
-// Clone clones the input
+// Clone clones the input preserving the difference between nil and empty
 func Clone[K comparable, V any, S ~map[K]V](s S) S {
 	if s == nil {
 		return nil
@@ -33,4 +33,27 @@ func Clone[K comparable, V any, S ~map[K]V](s S) S {
 		ret[k] = v
 	}
 	return ret
+}
+
+// Merge merges a and b while resolving the conflicts by calling resolveConflict
+func Merge[K comparable, V any, S ~map[K]V](a, b S, resolveConflict func(a, b V) V) S {
+	if a == nil {
+		return Clone(b)
+	}
+
+	ret := Clone(a)
+
+	for k, v := range b {
+		if _, found := ret[k]; found {
+			ret[k] = resolveConflict(ret[k], v)
+		} else {
+			ret[k] = v
+		}
+	}
+	return ret
+}
+
+// Merge merges a and b keeping the values in a in case of conflict
+func MergeKeepFirst[K comparable, V any, S ~map[K]V](a, b S) S {
+	return Merge(a, b, func(v, _ V) V { return v })
 }
