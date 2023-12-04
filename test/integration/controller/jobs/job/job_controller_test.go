844a8271bb388eb1d3ece0724ccf373a6f3ef418
Traian Schiau <traian_schiau@epam.com>
2023-12-04 18:03:56 +0200

    [flaky][int/job] Fix flaky partial admission suspend check.
    Due to the multistage stop of batch/job the job is suspend
    with an unrestored parallelism for a brief period of time.
    Extend the `Eventually` to cover that time also.

--- test/integration/controller/jobs/job/job_controller_test.go
+++ test/integration/controller/jobs/job/job_controller_test.go
@@ -1530,11 +1530,11 @@ var _ = ginkgo.Describe("Job controller interacting with scheduler", ginkgo.Orde
 		})
 
 		ginkgo.By("job should be suspended and its parallelism restored", func() {
-			gomega.Eventually(func() *bool {
-				gomega.Expect(k8sClient.Get(ctx, jobKey, createdJob)).Should(gomega.Succeed())
-				return createdJob.Spec.Suspend
-			}, util.Timeout, util.Interval).Should(gomega.Equal(ptr.To(true)))
-			gomega.Expect(*createdJob.Spec.Parallelism).To(gomega.BeEquivalentTo(5))
+			gomega.Eventually(func(g gomega.Gomega) {
+				g.Expect(k8sClient.Get(ctx, jobKey, createdJob)).Should(gomega.Succeed())
+				g.Expect(ptr.Deref(createdJob.Spec.Suspend, false)).To(gomega.BeTrue(), "the job should be suspended")
+				g.Expect(ptr.Deref(createdJob.Spec.Parallelism, 0)).To(gomega.BeEquivalentTo(5))
+			}, util.Timeout, util.Interval).Should(gomega.Succeed())
 		})
 	})
 
