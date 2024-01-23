d298b376e74f483acfb777794db5572d81c7ae4b
Traian Schiau <traian_schiau@epam.com>
2024-01-23 15:24:33 +0200

    Fix after rebase

--- pkg/controller/admissionchecks/multikueue/jobset_adapter_test.go
+++ pkg/controller/admissionchecks/multikueue/jobset_adapter_test.go
@@ -223,12 +223,16 @@ func TestWlReconcileJobset(t *testing.T) {
 			manageBuilder = manageBuilder.WithLists(&kueue.WorkloadList{Items: tc.managersWorkloads}, &jobset.JobSetList{Items: tc.managersJobSets})
 			manageBuilder = manageBuilder.WithStatusSubresource(slices.Map(tc.managersWorkloads, func(w *kueue.Workload) client.Object { return w })...)
 			manageBuilder = manageBuilder.WithStatusSubresource(slices.Map(tc.managersJobSets, func(w *jobset.JobSet) client.Object { return w })...)
-			manageBuilder = manageBuilder.WithObjects(utiltesting.MakeAdmissionCheck("ac1").ControllerName(ControllerName).Obj())
+			manageBuilder = manageBuilder.WithObjects(
+				utiltesting.MakeMultiKueueConfig("config1").Clusters("worker1").Obj(),
+				utiltesting.MakeAdmissionCheck("ac1").ControllerName(ControllerName).
+					Parameters(kueue.GroupVersion.Group, "MultiKueueConfig", "config1").
+					Obj(),
+			)
 
 			managerClient := manageBuilder.Build()
-			acr := newACController(managerClient, nil, TestNamespace)
 
-			rc := newRemoteController(ctx, managerClient, nil)
+			cRec := newClustersReconciler(managerClient, TestNamespace)
 
 			worker1Builder, _ := getClientBuilder()
 			worker1Builder = worker1Builder.WithLists(&kueue.WorkloadList{Items: tc.worker1Workloads}, &jobset.JobSetList{Items: tc.worker1JobSets})
@@ -237,13 +241,10 @@ func TestWlReconcileJobset(t *testing.T) {
 			w1remoteClient := newRemoteClient(managerClient, nil)
 			w1remoteClient.client = worker1Client
 
-			rc.remoteClients = map[string]*remoteClient{"worker1": w1remoteClient}
+			cRec.clients["worker1"] = w1remoteClient
 
-			acr.controllers["ac1"] = rc
-
-			reconciler := wlReconciler{
-				acr: acr,
-			}
+			helper, _ := newMultiKueueStoreHelper(managerClient)
+			reconciler := newWlReconciler(managerClient, helper, cRec)
 
 			_, gotErr := reconciler.Reconcile(ctx, reconcile.Request{NamespacedName: types.NamespacedName{Name: "wl1", Namespace: TestNamespace}})
 			if gotErr != nil {
