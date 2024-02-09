3ad72eaee17970f0e69a947746afaa149e7e19a5
Traian Schiau <traian_schiau@epam.com>
2024-02-09 15:41:08 +0200

    Stop the availability wait if at least one kueue container has restarted.

--- test/util/e2e.go
+++ test/util/e2e.go
@@ -74,13 +74,24 @@ func WaitForKueueAvailability(ctx context.Context, k8sClient client.Client) {
 		Name:      "kueue-controller-manager",
 	}
 	deployment := &appsv1.Deployment{}
-	gomega.EventuallyWithOffset(1, func(g gomega.Gomega) {
+	pods := corev1.PodList{}
+	gomega.EventuallyWithOffset(1, func(g gomega.Gomega) error {
 		g.Expect(k8sClient.Get(ctx, kcmKey, deployment)).To(gomega.Succeed())
+		g.Expect(k8sClient.List(ctx, &pods, client.InNamespace("kueue-system"), client.MatchingLabels(deployment.Spec.Selector.MatchLabels))).To(gomega.Succeed())
+		for _, pod := range pods.Items {
+			for _, cs := range pod.Status.ContainerStatuses {
+				if cs.RestartCount > 0 {
+					return gomega.StopTrying(fmt.Sprintf("%q in %q has restarted %d times", cs.Name, pod.Name, cs.RestartCount))
+				}
+			}
+		}
 		g.Expect(deployment.Status.Conditions).To(gomega.ContainElement(gomega.BeComparableTo(
 			appsv1.DeploymentCondition{
 				Type:   appsv1.DeploymentAvailable,
 				Status: corev1.ConditionTrue,
 			},
 			cmpopts.IgnoreFields(appsv1.DeploymentCondition{}, "Reason", "Message", "LastUpdateTime", "LastTransitionTime"))))
+		return nil
+
 	}, StartUpTimeout, Interval).Should(gomega.Succeed())
 }
