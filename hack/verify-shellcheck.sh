77e0654a5b5a765f57b5a9ffe2164145786d479c
Oleksandr Redko <oleksandr.red+github@gmail.com>
2024-03-14 15:28:31 +0200

    Load shellcheck configuration from the .shellcheckrc

--- hack/verify-shellcheck.sh
+++ hack/verify-shellcheck.sh
@@ -21,21 +21,6 @@ DOCKER="${DOCKER:-docker}"
 SHELLCHECK_VERSION="0.9.0"
 SHELLCHECK_IMAGE="docker.io/koalaman/shellcheck-alpine:v0.9.0@sha256:e19ed93c22423970d56568e171b4512c9244fc75dd9114045016b4a0073ac4b7"
 
-SHELLCHECK_DISABLED="SC2002,SC3028,SC3054,SC3014,SC3040,SC3046,SC3030,SC3010,SC3037,SC3045,SC3006,SC3018,SC3016,SC3011,SC3044,SC3043,SC3060,SC3024,SC1091,SC2066,SC2086,SC2034,SC1083,SC1009,SC1073,SC1072,SC2155,SC2046"
-# common arguments we'll pass to shellcheck
-SHELLCHECK_OPTIONS=(
-  # allow following sourced files that are not specified in the command,
-  # we need this because we specify one file at a time in order to trivially
-  # detect which files are failing
-  "--external-sources"
-  # include our disabled lints
-  "--exclude=${SHELLCHECK_DISABLED}"
-  # set colorized output
-  "--color=${SHELLCHECK_COLORIZED_OUTPUT}"
-)
-
-# Currently disabled these errors will take care of them later
-
 scripts_to_check=("$@")
 if [[ "$#" == 0 ]]; then
   # Find all shell scripts excluding:
@@ -60,11 +45,12 @@ fi
 echo "Downloading ShellCheck Docker image..."
 "${DOCKER}" pull "${SHELLCHECK_IMAGE}"
 
-# Run ShellCheck on all shell script files, excluding those in the 'vendor' directory
+# Run ShellCheck on all shell script files, excluding those in the 'vendor' directory.
+# Configuration loaded from the .shelcheckrc file.
 echo "Running ShellCheck..."
 "${DOCKER}" run \
   --rm -v "$(pwd)" -w "$(pwd)" \
     "${SHELLCHECK_IMAGE}" \
-  shellcheck "${SHELLCHECK_OPTIONS[@]}" "${scripts_to_check[@]}" >&2 || res=$?
+  shellcheck "--color=${SHELLCHECK_COLORIZED_OUTPUT}" "${scripts_to_check[@]}" >&2 || res=$?
 
 echo "Shellcheck ran successfully"
