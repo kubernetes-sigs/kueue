d0880c26b28819e06b981d6bc9a6c10f1c64acf1
Dominik Pajak <dominikpajak@google.com>
2024-04-11 19:06:16 +0000

    Addressing PR comments

--- apis/config/v1beta1/configuration_types.go
+++ apis/config/v1beta1/configuration_types.go
@@ -303,12 +303,12 @@ type Integrations struct {
 	PodOptions *PodIntegrationOptions `json:"podOptions,omitempty"`
 
 	// A list of label keys that should be copied from the job into the workload
-	// object. We don't require the job to have all the labels from this list. If
+	// object. It is not required for the job to have all the labels from this list. If
 	// a job does not have some label with the given key from this list, the
 	// constructed workload object will be created without this label. In the case
 	// of creating a workload from a composable job (pod group), if multiple objects
-	// have labels with some key form  the  list, the values of these labels must
-	// agree or otherwise the workload creation would fail. The labels are copied only
+	// have labels with some key form the list, the values of these labels must
+	// match or otherwise the workload creation would fail. The labels are copied only
 	// during the workload creation and are not updated even if the labels of the
 	// underlying job are changed.
 	LabelKeysToCopy []string `json:"labelKeysToCopy,omitempty"`
