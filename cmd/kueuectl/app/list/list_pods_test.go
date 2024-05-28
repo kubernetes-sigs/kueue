cecb41c42b541851dd068fc4be5cc85535f86a41
Kavin <62742678+Kavinraja-G@users.noreply.github.com>
2024-05-28 10:28:42 +0530

    refactor get pods logic for multiple types

--- cmd/kueuectl/app/list/list_pods_test.go
+++ cmd/kueuectl/app/list/list_pods_test.go
@@ -25,162 +25,49 @@ import (
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
 
-	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 	"k8s.io/cli-runtime/pkg/genericiooptions"
-	"k8s.io/cli-runtime/pkg/printers"
-	k8s "k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/kubernetes/fake"
 )
 
-func TestPodOptions_getJobControllerUID(t *testing.T) {
-	type fields struct {
-		PrintFlags    *genericclioptions.PrintFlags
-		AllNamespaces bool
-		Namespace     string
-		LabelSelector string
-		FieldSelector string
-		JobArg        string
-		Clientset     k8s.Clientset
-		PrintObj      printers.ResourcePrinterFunc
-		IOStreams     genericiooptions.IOStreams
-	}
-	type args struct {
-		jobName string
-	}
-	tests := []struct {
-		name    string
-		jobs    []runtime.Object
-		fields  fields
-		args    args
-		want    string
-		wantErr bool
-	}{
-		{
-			name: "get valid controllerUID from a Job",
-			jobs: []runtime.Object{
-				&batchv1.Job{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      "test-job",
-						Namespace: "default",
-						Labels: map[string]string{
-							jobControllerUIDLabel: "test-job-uid",
-						},
-					},
-				},
-			},
-			args: args{
-				jobName: "test-job",
-			},
-			fields: fields{
-				Namespace: "default",
-			},
-			want:    "test-job-uid",
-			wantErr: false,
-		}, {
-			name: "missing controllerUID label from a Job",
-			jobs: []runtime.Object{
-				&batchv1.Job{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      "missing-uid-job",
-						Namespace: "default",
-						Labels:    nil,
-					},
-				},
-			},
-			fields: fields{
-				Namespace: "default",
-			},
-			args: args{
-				jobName: "missing-uid-job",
-			},
-			want:    "",
-			wantErr: false,
-		}, {
-			name: "job not found in the namespace",
-			jobs: []runtime.Object{
-				&batchv1.Job{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      "test-job",
-						Namespace: "test-namespace",
-						Labels:    nil,
-					},
-				},
-			},
-			fields: fields{
-				Namespace: "default",
-			},
-			args: args{
-				jobName: "test-job",
-			},
-			want:    "",
-			wantErr: true,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			t.Parallel()
-			fakeClientset := fake.NewSimpleClientset(tt.jobs...)
-			o := &PodOptions{
-				PrintFlags:    tt.fields.PrintFlags,
-				AllNamespaces: tt.fields.AllNamespaces,
-				Namespace:     tt.fields.Namespace,
-				LabelSelector: tt.fields.LabelSelector,
-				FieldSelector: tt.fields.FieldSelector,
-				JobArg:        tt.fields.JobArg,
-				Clientset:     fakeClientset,
-				PrintObj:      tt.fields.PrintObj,
-				IOStreams:     tt.fields.IOStreams,
-			}
-			got, err := o.getJobControllerUID(context.TODO(), tt.args.jobName)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("getJobControllerUID() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if got != tt.want {
-				t.Errorf("getJobControllerUID() got = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestPodOptions_listPodsByControllerUID(t *testing.T) {
+func TestPodOptions_listPods(t *testing.T) {
 	testStartTime := time.Now()
 
 	type fields struct {
-		PrintFlags    *genericclioptions.PrintFlags
-		AllNamespaces bool
-		Namespace     string
-		LabelSelector string
-		FieldSelector string
-		JobArg        string
-	}
-	type args struct {
-		controllerUID string
+		PrintFlags             *genericclioptions.PrintFlags
+		AllNamespaces          bool
+		Namespace              string
+		LabelSelector          string
+		FieldSelector          string
+		UserSpecifiedForObject string
+		ForObject              objectRef
 	}
 	tests := []struct {
 		name    string
 		pods    []runtime.Object
 		fields  fields
-		args    args
 		wantOut []string
 		wantErr error
 	}{
 		{
-			name: "list pods by valid controllerUID",
+			name: "list pods for batch/job type",
 			pods: []runtime.Object{
 				&corev1.Pod{
 					ObjectMeta: metav1.ObjectMeta{
 						Name:      "valid-pod-1",
 						Namespace: "default",
-						Labels: map[string]string{
-							jobControllerUIDLabel: "valid-controller-uid",
-						},
 						CreationTimestamp: metav1.Time{
 							Time: testStartTime.Add(-time.Hour).Truncate(time.Second),
+						},
+						OwnerReferences: []metav1.OwnerReference{
+							{
+								APIVersion: "batch/v1",
+								Kind:       "Job",
+								Name:       "test-job",
+							},
 						},
 					},
 					Status: corev1.PodStatus{
@@ -190,37 +77,25 @@ func TestPodOptions_listPodsByControllerUID(t *testing.T) {
 					ObjectMeta: metav1.ObjectMeta{
 						Name:      "valid-pod-2",
 						Namespace: "default",
-						Labels: map[string]string{
-							jobControllerUIDLabel: "valid-controller-uid",
-						},
 						CreationTimestamp: metav1.Time{
 							Time: testStartTime.Add(-time.Hour).Truncate(time.Second),
+						},
+						OwnerReferences: []metav1.OwnerReference{
+							{
+								APIVersion: "batch/v1",
+								Kind:       "Job",
+								Name:       "test-job",
+							},
 						},
 					},
 					Status: corev1.PodStatus{
 						Phase: "COMPLETED",
 					},
-				}, &corev1.Pod{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      "invalid-pod",
-						Namespace: "default",
-						Labels: map[string]string{
-							jobControllerUIDLabel: "invalid-controller-uid",
-						},
-						CreationTimestamp: metav1.Time{
-							Time: testStartTime.Add(-time.Hour).Truncate(time.Second),
-						},
-					},
-					Status: corev1.PodStatus{
-						Phase: "RUNNING",
-					},
 				},
 			},
 			fields: fields{
-				Namespace: "default",
-			},
-			args: args{
-				controllerUID: "valid-controller-uid",
+				Namespace:              "default",
+				UserSpecifiedForObject: "job/test-job",
 			},
 			wantOut: []string{
 				"NAME          STATUS      AGE",
@@ -228,52 +103,28 @@ func TestPodOptions_listPodsByControllerUID(t *testing.T) {
 				"valid-pod-2   COMPLETED   60m",
 				"",
 			},
-		}, {
-			name: "no valid pods matching controllerUID",
-			pods: []runtime.Object{
-				&corev1.Pod{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      "test-pod-1",
-						Namespace: "default",
-						Labels: map[string]string{
-							jobControllerUIDLabel: "valid-controller-uid",
-						},
-						CreationTimestamp: metav1.Time{
-							Time: testStartTime.Add(-time.Hour).Truncate(time.Second),
-						},
-					},
-					Status: corev1.PodStatus{
-						Phase: "RUNNING",
-					},
-				},
-			},
-			fields: fields{
-				Namespace: "default",
-			},
-			args: args{
-				controllerUID: "invalid-controller-uid",
-			},
-			wantOut: []string{""},
 		},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			fakeClientset := fake.NewSimpleClientset(tt.pods...)
 			s, _, out, _ := genericiooptions.NewTestIOStreams()
 
 			o := &PodOptions{
-				PrintFlags:    tt.fields.PrintFlags,
-				AllNamespaces: tt.fields.AllNamespaces,
-				Namespace:     tt.fields.Namespace,
-				LabelSelector: tt.fields.LabelSelector,
-				FieldSelector: tt.fields.FieldSelector,
-				JobArg:        tt.fields.JobArg,
-				Clientset:     fakeClientset,
-				PrintObj:      printPodTable,
-				IOStreams:     s,
+				PrintFlags:             tt.fields.PrintFlags,
+				AllNamespaces:          tt.fields.AllNamespaces,
+				Namespace:              tt.fields.Namespace,
+				LabelSelector:          tt.fields.LabelSelector,
+				FieldSelector:          tt.fields.FieldSelector,
+				UserSpecifiedForObject: tt.fields.UserSpecifiedForObject,
+				Clientset:              fakeClientset,
+				PrintObj:               printPodTable,
+				IOStreams:              s,
 			}
 
-			gotErr := o.listPodsByControllerUID(context.TODO(), tt.args.controllerUID)
+			gotErr := o.parseForObject()
+			gotErr = o.listPods(context.TODO())
 			if diff := cmp.Diff(tt.wantErr, gotErr, cmpopts.EquateErrors()); diff != "" {
 				t.Errorf("Unexpected error (-want/+got)\n%s", diff)
 			}
