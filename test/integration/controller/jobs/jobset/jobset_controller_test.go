f7e4c4d9515d501e843ca52e63e33142a9568f14
HighPon <s.shiraki.business@gmail.com>
2024-07-15 12:02:51 +0900

    fix: Add ginkgo.ContinueOnFailure to jobset integration test

--- test/integration/controller/jobs/jobset/jobset_controller_test.go
+++ test/integration/controller/jobs/jobset/jobset_controller_test.go
@@ -1052,7 +1052,7 @@ var _ = ginkgo.Describe("JobSet controller interacting with scheduler", ginkgo.O
 	})
 })
 
-var _ = ginkgo.Describe("Enhanced JobSet controller tests", ginkgo.Ordered, func() {
+var _ = ginkgo.Describe("Enhanced JobSet controller tests", ginkgo.Ordered, ginkgo.ContinueOnFailure, func() {
 	ginkgo.BeforeAll(func() {
 		fwk = &framework.Framework{
 			CRDPath:     crdPath,
