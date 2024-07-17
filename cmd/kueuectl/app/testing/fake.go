26b1bcf028111d1993d10954ccc8669e57040d36
Kavin <62742678+Kavinraja-G@users.noreply.github.com>
2024-07-17 15:12:43 +0530

    More refactor based on comments (1)

--- cmd/kueuectl/app/testing/fake.go
+++ cmd/kueuectl/app/testing/fake.go
@@ -41,11 +41,6 @@ func NewTestClientGetter() *TestClientGetter {
 	}
 }
 
-func (f *TestClientGetter) WithRestMapper(mapper meta.RESTMapper) *TestClientGetter {
-	f.configFlags.WithRESTMapper(mapper)
-	return f
-}
-
 func (f *TestClientGetter) WithNamespace(ns string) *TestClientGetter {
 	f.configFlags.WithNamespace(ns)
 	return f
