f9c86e73c8257c6ab30ace64012d83536a6179e2
Mykhailo Bobrovskyi <mikhail.bobrovsky@gmail.com>
2024-09-17 10:25:49 +0300

    Split kjobctl samples.

--- cmd/experimental/kjobctl/config/samples/slurm/script.sh
+++ cmd/experimental/kjobctl/config/samples/slurm/script.sh
@@ -2,7 +2,5 @@
 
 #SBATCH --array=1-3%2
 
-echo "now processing task id:: ${SLURM_ARRAY_TASK_ID}"
+echo "now processing task id: ${SLURM_ARRAY_TASK_ID}"
 python /home/slurm/samples/sample_code.py
-
-exit 0
