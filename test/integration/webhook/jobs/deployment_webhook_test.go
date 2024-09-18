0278209f393ae4900483b9c31c0c966c1420a523
Traian Schiau <traian_schiau@epam.com>
2024-09-18 15:28:22 +0300

    [test][int][ctrl][jobs][deployment] Move webhook tests.

--- test/integration/webhook/jobs/deployment_webhook_test.go
+++ test/integration/webhook/jobs/deployment_webhook_test.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package deployment
+package jobs
 
 import (
 	"github.com/onsi/ginkgo/v2"
@@ -28,31 +28,24 @@ import (
 
 	"sigs.k8s.io/kueue/pkg/controller/constants"
 	"sigs.k8s.io/kueue/pkg/controller/jobframework"
+	"sigs.k8s.io/kueue/pkg/controller/jobs/deployment"
 	"sigs.k8s.io/kueue/pkg/util/kubeversion"
 	testingdeployment "sigs.k8s.io/kueue/pkg/util/testingjobs/deployment"
-	"sigs.k8s.io/kueue/test/integration/framework"
 	"sigs.k8s.io/kueue/test/util"
 )
 
 var _ = ginkgo.Describe("Deployment Webhook", func() {
 	var ns *corev1.Namespace
-
 	ginkgo.When("with pod integration enabled", ginkgo.Ordered, ginkgo.ContinueOnFailure, func() {
 		ginkgo.BeforeAll(func() {
-			fwk = &framework.Framework{
-				CRDPath:     crdPath,
-				WebhookPath: webhookPath,
-			}
-			cfg = fwk.Init()
-
 			discoveryClient, err := discovery.NewDiscoveryClientForConfig(cfg)
 			gomega.Expect(err).NotTo(gomega.HaveOccurred())
 			serverVersionFetcher = kubeversion.NewServerVersionFetcher(discoveryClient)
 			err = serverVersionFetcher.FetchServerVersion()
 			gomega.Expect(err).NotTo(gomega.HaveOccurred())
 
-			ctx, k8sClient = fwk.RunManager(cfg, managerSetup(
-				nil,
+			fwk.StartManager(ctx, cfg, managerSetup(
+				deployment.SetupWebhook,
 				jobframework.WithManageJobsWithoutQueueName(false),
 				jobframework.WithKubeServerVersion(serverVersionFetcher),
 			))
@@ -70,7 +63,7 @@ var _ = ginkgo.Describe("Deployment Webhook", func() {
 			gomega.Expect(util.DeleteNamespace(ctx, k8sClient, ns)).To(gomega.Succeed())
 		})
 		ginkgo.AfterAll(func() {
-			fwk.Teardown()
+			fwk.StopManager(ctx)
 		})
 
 		ginkgo.When("The queue-name label is set", func() {
