7e27625daefbceacfda98add11d9853c43d6254c
Mykhailo Bobrovskyi <mikhail.bobrovsky@gmail.com>
2024-10-23 16:44:58 +0300

    [kjobctl] Skip removing ConfigMap on delete Slurm. (#3288)

--- cmd/experimental/kjobctl/pkg/cmd/delete/delete_slurm_test.go
+++ cmd/experimental/kjobctl/pkg/cmd/delete/delete_slurm_test.go
@@ -23,7 +23,6 @@ import (
 
 	"github.com/google/go-cmp/cmp"
 	batchv1 "k8s.io/api/batch/v1"
-	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/cli-runtime/pkg/genericiooptions"
@@ -37,147 +36,104 @@ import (
 
 func TestSlurmCmd(t *testing.T) {
 	testCases := map[string]struct {
-		ns             string
-		args           []string
-		objs           []runtime.Object
-		wantJobs       []batchv1.Job
-		wantConfigMaps []corev1.ConfigMap
-		wantOut        string
-		wantOutErr     string
-		wantErr        string
+		ns         string
+		args       []string
+		objs       []runtime.Object
+		wantJobs   []batchv1.Job
+		wantOut    string
+		wantOutErr string
+		wantErr    string
 	}{
 		"shouldn't delete slurm job because it is not found": {
 			args: []string{"j"},
 			objs: []runtime.Object{
 				wrappers.MakeJob("j1", metav1.NamespaceDefault).Profile("p1").Mode(v1alpha1.SlurmMode).Obj(),
-				wrappers.MakeConfigMap("j1", metav1.NamespaceDefault).Profile("p1").Mode(v1alpha1.SlurmMode).Obj(),
 				wrappers.MakeJob("j2", metav1.NamespaceDefault).Profile("p2").Mode(v1alpha1.SlurmMode).Obj(),
-				wrappers.MakeConfigMap("j2", metav1.NamespaceDefault).Profile("p2").Mode(v1alpha1.SlurmMode).Obj(),
 			},
 			wantJobs: []batchv1.Job{
 				*wrappers.MakeJob("j1", metav1.NamespaceDefault).Profile("p1").Mode(v1alpha1.SlurmMode).Obj(),
 				*wrappers.MakeJob("j2", metav1.NamespaceDefault).Profile("p2").Mode(v1alpha1.SlurmMode).Obj(),
 			},
-			wantConfigMaps: []corev1.ConfigMap{
-				*wrappers.MakeConfigMap("j1", metav1.NamespaceDefault).Profile("p1").Mode(v1alpha1.SlurmMode).Obj(),
-				*wrappers.MakeConfigMap("j2", metav1.NamespaceDefault).Profile("p2").Mode(v1alpha1.SlurmMode).Obj(),
-			},
-			wantOutErr: "jobs.batch \"j\" not found\nconfigmaps \"j\" not found\n",
+			wantOutErr: "jobs.batch \"j\" not found\n",
 		},
-		"shouldn't delete job because it is not created via kjob": {
+		"shouldn't delete slurm job because it is not created via kjob": {
 			args: []string{"j1"},
 			objs: []runtime.Object{
 				wrappers.MakeJob("j1", metav1.NamespaceDefault).Obj(),
-				wrappers.MakeConfigMap("j1", metav1.NamespaceDefault).Obj(),
 			},
 			wantJobs: []batchv1.Job{
 				*wrappers.MakeJob("j1", metav1.NamespaceDefault).Obj(),
 			},
-			wantConfigMaps: []corev1.ConfigMap{
-				*wrappers.MakeConfigMap("j1", metav1.NamespaceDefault).Obj(),
-			},
-			wantOutErr: "jobs.batch \"j1\" not created via kjob\nconfigmaps \"j1\" not created via kjob\n",
+			wantOutErr: "jobs.batch \"j1\" not created via kjob\n",
 		},
 		"shouldn't delete slurm job because it is not used for Job mode": {
 			args: []string{"j1", "j2"},
 			objs: []runtime.Object{
 				wrappers.MakeJob("j1", metav1.NamespaceDefault).Profile("p1").Obj(),
-				wrappers.MakeConfigMap("j1", metav1.NamespaceDefault).Profile("p1").Obj(),
 				wrappers.MakeJob("j2", metav1.NamespaceDefault).Profile("p1").Mode(v1alpha1.JobMode).Obj(),
-				wrappers.MakeConfigMap("j2", metav1.NamespaceDefault).Profile("p1").Mode(v1alpha1.JobMode).Obj(),
 			},
 			wantJobs: []batchv1.Job{
 				*wrappers.MakeJob("j1", metav1.NamespaceDefault).Profile("p1").Obj(),
 				*wrappers.MakeJob("j2", metav1.NamespaceDefault).Profile("p1").Mode(v1alpha1.JobMode).Obj(),
 			},
-			wantConfigMaps: []corev1.ConfigMap{
-				*wrappers.MakeConfigMap("j1", metav1.NamespaceDefault).Profile("p1").Obj(),
-				*wrappers.MakeConfigMap("j2", metav1.NamespaceDefault).Profile("p1").Mode(v1alpha1.JobMode).Obj(),
-			},
 			wantOutErr: `jobs.batch "j1" created in "" mode. Switch to the correct mode to delete it
-configmaps "j1" created in "" mode. Switch to the correct mode to delete it
 jobs.batch "j2" created in "Job" mode. Switch to the correct mode to delete it
-configmaps "j2" created in "Job" mode. Switch to the correct mode to delete it
 `,
 		},
 		"should delete slurm job": {
 			args: []string{"j1"},
 			objs: []runtime.Object{
 				wrappers.MakeJob("j1", metav1.NamespaceDefault).Profile("p1").Mode(v1alpha1.SlurmMode).Obj(),
-				wrappers.MakeConfigMap("j1", metav1.NamespaceDefault).Profile("p1").Mode(v1alpha1.SlurmMode).Obj(),
 				wrappers.MakeJob("j2", metav1.NamespaceDefault).Profile("p2").Mode(v1alpha1.SlurmMode).Obj(),
-				wrappers.MakeConfigMap("j2", metav1.NamespaceDefault).Profile("p2").Mode(v1alpha1.SlurmMode).Obj(),
 			},
 			wantJobs: []batchv1.Job{
 				*wrappers.MakeJob("j2", metav1.NamespaceDefault).Profile("p2").Mode(v1alpha1.SlurmMode).Obj(),
 			},
-			wantConfigMaps: []corev1.ConfigMap{
-				*wrappers.MakeConfigMap("j2", metav1.NamespaceDefault).Profile("p2").Mode(v1alpha1.SlurmMode).Obj(),
-			},
-			wantOut: "job.batch/j1 deleted\nconfigmap/j1 deleted\n",
+			wantOut: "job.batch/j1 deleted\n",
 		},
 		"should delete slurm jobs": {
 			args: []string{"j1", "j2"},
 			objs: []runtime.Object{
 				wrappers.MakeJob("j1", metav1.NamespaceDefault).Profile("p1").Mode(v1alpha1.SlurmMode).Obj(),
-				wrappers.MakeConfigMap("j1", metav1.NamespaceDefault).Profile("p1").Mode(v1alpha1.SlurmMode).Obj(),
 				wrappers.MakeJob("j2", metav1.NamespaceDefault).Profile("p2").Mode(v1alpha1.SlurmMode).Obj(),
-				wrappers.MakeConfigMap("j2", metav1.NamespaceDefault).Profile("p2").Mode(v1alpha1.SlurmMode).Obj(),
 			},
-			wantOut: "job.batch/j1 deleted\nconfigmap/j1 deleted\njob.batch/j2 deleted\nconfigmap/j2 deleted\n",
+			wantOut: "job.batch/j1 deleted\njob.batch/j2 deleted\n",
 		},
 		"should delete only one slurm job": {
 			args: []string{"j1", "j"},
 			objs: []runtime.Object{
 				wrappers.MakeJob("j1", metav1.NamespaceDefault).Profile("p1").Mode(v1alpha1.SlurmMode).Obj(),
-				wrappers.MakeConfigMap("j1", metav1.NamespaceDefault).Profile("p1").Mode(v1alpha1.SlurmMode).Obj(),
 				wrappers.MakeJob("j2", metav1.NamespaceDefault).Profile("p2").Mode(v1alpha1.SlurmMode).Obj(),
-				wrappers.MakeConfigMap("j2", metav1.NamespaceDefault).Profile("p2").Mode(v1alpha1.SlurmMode).Obj(),
 			},
 			wantJobs: []batchv1.Job{
 				*wrappers.MakeJob("j2", metav1.NamespaceDefault).Profile("p2").Mode(v1alpha1.SlurmMode).Obj(),
 			},
-			wantConfigMaps: []corev1.ConfigMap{
-				*wrappers.MakeConfigMap("j2", metav1.NamespaceDefault).Profile("p2").Mode(v1alpha1.SlurmMode).Obj(),
-			},
-			wantOut:    "job.batch/j1 deleted\nconfigmap/j1 deleted\n",
-			wantOutErr: "jobs.batch \"j\" not found\nconfigmaps \"j\" not found\n",
+			wantOut:    "job.batch/j1 deleted\n",
+			wantOutErr: "jobs.batch \"j\" not found\n",
 		},
 		"shouldn't delete slurm job with client dry run": {
 			args: []string{"j1", "--dry-run", "client"},
 			objs: []runtime.Object{
 				wrappers.MakeJob("j1", metav1.NamespaceDefault).Profile("p1").Mode(v1alpha1.SlurmMode).Obj(),
-				wrappers.MakeConfigMap("j1", metav1.NamespaceDefault).Profile("p1").Mode(v1alpha1.SlurmMode).Obj(),
 				wrappers.MakeJob("j2", metav1.NamespaceDefault).Profile("p2").Mode(v1alpha1.SlurmMode).Obj(),
-				wrappers.MakeConfigMap("j2", metav1.NamespaceDefault).Profile("p2").Mode(v1alpha1.SlurmMode).Obj(),
 			},
 			wantJobs: []batchv1.Job{
 				*wrappers.MakeJob("j1", metav1.NamespaceDefault).Profile("p1").Mode(v1alpha1.SlurmMode).Obj(),
 				*wrappers.MakeJob("j2", metav1.NamespaceDefault).Profile("p2").Mode(v1alpha1.SlurmMode).Obj(),
 			},
-			wantConfigMaps: []corev1.ConfigMap{
-				*wrappers.MakeConfigMap("j1", metav1.NamespaceDefault).Profile("p1").Mode(v1alpha1.SlurmMode).Obj(),
-				*wrappers.MakeConfigMap("j2", metav1.NamespaceDefault).Profile("p2").Mode(v1alpha1.SlurmMode).Obj(),
-			},
-			wantOut: "job.batch/j1 deleted (client dry run)\nconfigmap/j1 deleted (client dry run)\n",
+			wantOut: "job.batch/j1 deleted (client dry run)\n",
 		},
 		"shouldn't delete slurm job with server dry run": {
 			args: []string{"j1", "--dry-run", "server"},
 			objs: []runtime.Object{
 				wrappers.MakeJob("j1", metav1.NamespaceDefault).Profile("p1").Mode(v1alpha1.SlurmMode).Obj(),
-				wrappers.MakeConfigMap("j1", metav1.NamespaceDefault).Profile("p1").Mode(v1alpha1.SlurmMode).Obj(),
 				wrappers.MakeJob("j2", metav1.NamespaceDefault).Profile("p2").Mode(v1alpha1.SlurmMode).Obj(),
-				wrappers.MakeConfigMap("j2", metav1.NamespaceDefault).Profile("p2").Mode(v1alpha1.SlurmMode).Obj(),
 			},
 			wantJobs: []batchv1.Job{
 				*wrappers.MakeJob("j1", metav1.NamespaceDefault).Profile("p1").Mode(v1alpha1.SlurmMode).Obj(),
 				*wrappers.MakeJob("j2", metav1.NamespaceDefault).Profile("p2").Mode(v1alpha1.SlurmMode).Obj(),
 			},
-			wantConfigMaps: []corev1.ConfigMap{
-				*wrappers.MakeConfigMap("j1", metav1.NamespaceDefault).Profile("p1").Mode(v1alpha1.SlurmMode).Obj(),
-				*wrappers.MakeConfigMap("j2", metav1.NamespaceDefault).Profile("p2").Mode(v1alpha1.SlurmMode).Obj(),
-			},
-			wantOut: "job.batch/j1 deleted (server dry run)\nconfigmap/j1 deleted (server dry run)\n",
+			wantOut: "job.batch/j1 deleted (server dry run)\n",
 		},
 		"no args": {
 			args:    []string{},
@@ -195,12 +151,6 @@ configmaps "j2" created in "Job" mode. Switch to the correct mode to delete it
 
 			clientset := k8sfake.NewSimpleClientset(tc.objs...)
 			clientset.PrependReactor("delete", "jobs", func(action kubetesting.Action) (handled bool, ret runtime.Object, err error) {
-				if slices.Contains(action.(kubetesting.DeleteAction).GetDeleteOptions().DryRun, metav1.DryRunAll) {
-					handled = true
-				}
-				return handled, ret, err
-			})
-			clientset.PrependReactor("delete", "configmaps", func(action kubetesting.Action) (handled bool, ret runtime.Object, err error) {
 				if slices.Contains(action.(kubetesting.DeleteAction).GetDeleteOptions().DryRun, metav1.DryRunAll) {
 					handled = true
 				}
@@ -241,25 +191,13 @@ configmaps "j2" created in "Job" mode. Switch to the correct mode to delete it
 				t.Errorf("Unexpected error output (-want/+got)\n%s", diff)
 			}
 
-			ctx := context.Background()
-
-			gotJobList, err := clientset.BatchV1().Jobs(tc.ns).List(ctx, metav1.ListOptions{})
+			gotJobList, err := clientset.BatchV1().Jobs(tc.ns).List(context.Background(), metav1.ListOptions{})
 			if err != nil {
 				t.Error(err)
 				return
 			}
 
 			if diff := cmp.Diff(tc.wantJobs, gotJobList.Items); diff != "" {
-				t.Errorf("Unexpected jobs (-want/+got)\n%s", diff)
-			}
-
-			gotConfigMapList, err := clientset.CoreV1().ConfigMaps(tc.ns).List(ctx, metav1.ListOptions{})
-			if err != nil {
-				t.Error(err)
-				return
-			}
-
-			if diff := cmp.Diff(tc.wantConfigMaps, gotConfigMapList.Items); diff != "" {
 				t.Errorf("Unexpected jobs (-want/+got)\n%s", diff)
 			}
 		})
