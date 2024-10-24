1fe477b37e3d701583f2c412edf349e77da942c8
Mykhailo Bobrovskyi <mikhail.bobrovsky@gmail.com>
2024-10-24 14:54:52 +0300

    Fix "Stateful set integration when Single CQ should admit group that fits" test. (#3303)
    * Don't wait for statefulset to be removed.
    
    * Add ExpectObjectToBeDeletedWithTimeout.

--- test/e2e/singlecluster/statefulset_test.go
+++ test/e2e/singlecluster/statefulset_test.go
@@ -126,11 +126,11 @@ var _ = ginkgo.Describe("Stateful set integration", func() {
 				conflictingWorkload := &kueue.Workload{}
 				gomega.Expect(k8sClient.Get(ctx, conflictingWlLookupKey, conflictingWorkload)).To(gomega.Succeed())
 				gomega.Expect(createdWorkload.Name).ToNot(gomega.Equal(conflictingWorkload.Name))
-				util.ExpectObjectToBeDeleted(ctx, k8sClient, conflictingStatefulSet, true)
+				util.ExpectObjectToBeDeletedWithTimeout(ctx, k8sClient, conflictingStatefulSet, true, util.LongTimeout)
 				util.ExpectObjectToBeDeleted(ctx, k8sClient, conflictingWorkload, false)
 			})
 
-			util.ExpectObjectToBeDeleted(ctx, k8sClient, statefulSet, true)
+			util.ExpectObjectToBeDeletedWithTimeout(ctx, k8sClient, statefulSet, true, util.LongTimeout)
 			util.ExpectObjectToBeDeleted(ctx, k8sClient, createdWorkload, false)
 		})
 	})
