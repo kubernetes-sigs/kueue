03eb18296a477e21f13f72cc9a90b08ced858390
Patryk Bundyra <pbundyra@google.com>
2024-11-04 10:24:08 +0000

    Change api fields to be optional

--- apis/kueue/v1beta1/provisioningrequestconfig_types.go
+++ apis/kueue/v1beta1/provisioningrequestconfig_types.go
@@ -100,16 +100,16 @@ type ProvisioningRequestRetryStrategy struct {
 	// re-queuing an evicted workload.
 	//
 	// Defaults to 60.
-	// +required
+	// +optional
 	// +kubebuilder:default=60
-	BackoffBaseSeconds int32 `json:"backoffBaseSeconds"`
+	BackoffBaseSeconds int32 `json:"backoffBaseSeconds,omitempty"`
 
 	// BackoffMaxSeconds defines the maximum backoff time to re-queue an evicted workload.
 	//
 	// Defaults to 1800.
-	// +required
+	// +optional
 	// +kubebuilder:default=1800
-	BackoffMaxSeconds int32 `json:"backoffMaxSeconds"`
+	BackoffMaxSeconds int32 `json:"backoffMaxSeconds,omitempty"`
 }
 
 // Parameter is limited to 255 characters.
