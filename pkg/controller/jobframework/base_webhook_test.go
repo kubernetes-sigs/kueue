38a1060aa6d935c90f369d6e99a98c17ea367e2a
Mykhailo Bobrovskyi <mikhail.bobrovsky@gmail.com>
2024-11-05 09:27:32 +0200

    Use testGenericJob on testing BaseWebhook. (#3438)

--- pkg/controller/jobframework/base_webhook_test.go
+++ pkg/controller/jobframework/base_webhook_test.go
@@ -21,39 +21,115 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
-	kfmpi "github.com/kubeflow/mpi-operator/pkg/apis/kubeflow/v2beta1"
+	batchv1 "k8s.io/api/batch/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/utils/ptr"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
+	kueue "sigs.k8s.io/kueue/apis/kueue/v1beta1"
+	"sigs.k8s.io/kueue/pkg/controller/constants"
 	"sigs.k8s.io/kueue/pkg/controller/jobframework"
-	"sigs.k8s.io/kueue/pkg/controller/jobs/mpijob"
-	testingutil "sigs.k8s.io/kueue/pkg/util/testingjobs/mpijob"
+	"sigs.k8s.io/kueue/pkg/podset"
 )
 
-func toMPIJob(o runtime.Object) jobframework.GenericJob {
-	return (*mpijob.MPIJob)(o.(*kfmpi.MPIJob))
+type testGenericJob batchv1.Job
+
+var _ jobframework.GenericJob = (*testGenericJob)(nil)
+
+func (t *testGenericJob) Object() client.Object {
+	return (*batchv1.Job)(t)
+}
+
+func (t *testGenericJob) IsSuspended() bool {
+	return ptr.Deref(t.Spec.Suspend, false)
+}
+
+func (t *testGenericJob) Suspend() {
+	t.Spec.Suspend = ptr.To(true)
+}
+
+func (t *testGenericJob) RunWithPodSetsInfo([]podset.PodSetInfo) error {
+	panic("not implemented")
+}
+
+func (t *testGenericJob) RestorePodSetsInfo([]podset.PodSetInfo) bool {
+	panic("not implemented")
+}
+
+func (t *testGenericJob) Finished() (string, bool, bool) {
+	panic("not implemented")
+}
+
+func (t *testGenericJob) PodSets() []kueue.PodSet {
+	panic("not implemented")
+}
+
+func (t *testGenericJob) IsActive() bool {
+	panic("not implemented")
+}
+
+func (t *testGenericJob) PodsReady() bool {
+	panic("not implemented")
+}
+
+func (t *testGenericJob) GVK() schema.GroupVersionKind {
+	panic("not implemented")
+}
+
+func fromObject(o runtime.Object) jobframework.GenericJob {
+	return (*testGenericJob)(o.(*batchv1.Job))
 }
 
 func TestBaseWebhookDefault(t *testing.T) {
 	testcases := map[string]struct {
-		job                        *kfmpi.MPIJob
 		manageJobsWithoutQueueName bool
-		want                       *kfmpi.MPIJob
+
+		job  *batchv1.Job
+		want *batchv1.Job
 	}{
 		"update the suspend field with 'manageJobsWithoutQueueName=false'": {
-			job:  testingutil.MakeMPIJob("job", "default").Queue("queue").Suspend(false).Obj(),
-			want: testingutil.MakeMPIJob("job", "default").Queue("queue").Obj(),
+			job: &batchv1.Job{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "job",
+					Namespace: "default",
+					Labels:    map[string]string{constants.QueueLabel: "queue"},
+				},
+			},
+			want: &batchv1.Job{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "job",
+					Namespace: "default",
+					Labels:    map[string]string{constants.QueueLabel: "queue"},
+				},
+				Spec: batchv1.JobSpec{Suspend: ptr.To(true)},
+			},
 		},
 		"update the suspend field 'manageJobsWithoutQueueName=true'": {
-			job:                        testingutil.MakeMPIJob("job", "default").Suspend(false).Obj(),
 			manageJobsWithoutQueueName: true,
-			want:                       testingutil.MakeMPIJob("job", "default").Obj(),
+			job: &batchv1.Job{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "job",
+					Namespace: "default",
+					Labels:    map[string]string{constants.QueueLabel: "queue"},
+				},
+			},
+			want: &batchv1.Job{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "job",
+					Namespace: "default",
+					Labels:    map[string]string{constants.QueueLabel: "queue"},
+				},
+				Spec: batchv1.JobSpec{Suspend: ptr.To(true)},
+			},
 		},
 	}
 	for name, tc := range testcases {
 		t.Run(name, func(t *testing.T) {
 			w := &jobframework.BaseWebhook{
 				ManageJobsWithoutQueueName: tc.manageJobsWithoutQueueName,
-				FromObject:                 toMPIJob,
+				FromObject:                 fromObject,
 			}
 			if err := w.Default(context.Background(), tc.job); err != nil {
 				t.Errorf("set defaults to a kubeflow/mpijob by a Defaulter")
