e36eb4c72aa6171b36fa3ca8a35dc5586cc7da7d
Mykhailo Bobrovskyi <mikhail.bobrovsky@gmail.com>
2024-11-13 23:43:40 +0200

    Optimize and extend deployment unit tests.

--- pkg/controller/jobs/deployment/deployment_webhook_test.go
+++ pkg/controller/jobs/deployment/deployment_webhook_test.go
@@ -20,7 +20,10 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 	appsv1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/util/validation/field"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 
 	"sigs.k8s.io/kueue/pkg/controller/jobframework"
 	utiltesting "sigs.k8s.io/kueue/pkg/util/testing"
@@ -29,13 +32,14 @@ import (
 
 func TestDefault(t *testing.T) {
 	testCases := map[string]struct {
-		deployment                 *appsv1.Deployment
-		manageJobsWithoutQueueName bool
-		enableIntegrations         []string
-		want                       *appsv1.Deployment
+		deployment *appsv1.Deployment
+		want       *appsv1.Deployment
 	}{
-		"pod with queue": {
-			enableIntegrations: []string{"pod"},
+		"deployment without queue": {
+			deployment: testingdeployment.MakeDeployment("test-pod", "").Obj(),
+			want:       testingdeployment.MakeDeployment("test-pod", "").Obj(),
+		},
+		"deployment with queue": {
 			deployment: testingdeployment.MakeDeployment("test-pod", "").
 				Queue("test-queue").
 				Obj(),
@@ -48,12 +52,12 @@ func TestDefault(t *testing.T) {
 
 	for name, tc := range testCases {
 		t.Run(name, func(t *testing.T) {
-			t.Cleanup(jobframework.EnableIntegrationsForTest(t, tc.enableIntegrations...))
+			t.Cleanup(jobframework.EnableIntegrationsForTest(t, "pod"))
 			builder := utiltesting.NewClientBuilder()
-			cli := builder.Build()
+			client := builder.Build()
 
 			w := &Webhook{
-				client: cli,
+				client: client,
 			}
 
 			ctx, _ := utiltesting.ContextWithLog(t)
@@ -63,6 +67,99 @@ func TestDefault(t *testing.T) {
 			}
 			if diff := cmp.Diff(tc.want, tc.deployment); len(diff) != 0 {
 				t.Errorf("Default() mismatch (-want,+got):\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestValidateCreate(t *testing.T) {
+	testCases := map[string]struct {
+		deployment *appsv1.Deployment
+		wantErr    error
+		wantWarns  admission.Warnings
+	}{
+		"without queue": {
+			deployment: testingdeployment.MakeDeployment("test-pod", "").Obj(),
+		},
+		"valid queue name": {
+			deployment: testingdeployment.MakeDeployment("test-pod", "").
+				Queue("test-queue").
+				Obj(),
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			t.Cleanup(jobframework.EnableIntegrationsForTest(t, "pod"))
+
+			builder := utiltesting.NewClientBuilder()
+			client := builder.Build()
+
+			w := &Webhook{client: client}
+
+			ctx, _ := utiltesting.ContextWithLog(t)
+
+			warns, err := w.ValidateCreate(ctx, tc.deployment)
+			if diff := cmp.Diff(tc.wantErr, err, cmpopts.IgnoreFields(field.Error{}, "BadValue", "Detail")); diff != "" {
+				t.Errorf("Unexpected error (-want,+got):\n%s", diff)
+			}
+			if diff := cmp.Diff(warns, tc.wantWarns); diff != "" {
+				t.Errorf("Expected different list of warnings (-want,+got):\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestValidateUpdate(t *testing.T) {
+	testCases := map[string]struct {
+		oldDeployment *appsv1.Deployment
+		newDeployment *appsv1.Deployment
+		wantErr       error
+		wantWarns     admission.Warnings
+	}{
+		"without queue": {
+			oldDeployment: testingdeployment.MakeDeployment("test-pod", "").Obj(),
+			newDeployment: testingdeployment.MakeDeployment("test-pod", "").Obj(),
+		},
+		"with queue (no changes)": {
+			oldDeployment: testingdeployment.MakeDeployment("test-pod", "").
+				Queue("test-queue").
+				Obj(),
+			newDeployment: testingdeployment.MakeDeployment("test-pod", "").
+				Queue("test-queue").
+				Obj(),
+		},
+		"with queue": {
+			oldDeployment: testingdeployment.MakeDeployment("test-pod", "").Obj(),
+			newDeployment: testingdeployment.MakeDeployment("test-pod", "").
+				Queue("test-queue").
+				Obj(),
+			wantErr: field.ErrorList{
+				&field.Error{
+					Type:  field.ErrorTypeInvalid,
+					Field: "metadata.labels[kueue.x-k8s.io/queue-name]",
+				},
+			}.ToAggregate(),
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			t.Cleanup(jobframework.EnableIntegrationsForTest(t, "pod"))
+
+			builder := utiltesting.NewClientBuilder()
+			client := builder.Build()
+
+			w := &Webhook{client: client}
+
+			ctx, _ := utiltesting.ContextWithLog(t)
+
+			warns, err := w.ValidateUpdate(ctx, tc.oldDeployment, tc.newDeployment)
+			if diff := cmp.Diff(tc.wantErr, err, cmpopts.IgnoreFields(field.Error{}, "BadValue", "Detail")); diff != "" {
+				t.Errorf("Unexpected error (-want,+got):\n%s", diff)
+			}
+			if diff := cmp.Diff(warns, tc.wantWarns); diff != "" {
+				t.Errorf("Expected different list of warnings (-want,+got):\n%s", diff)
 			}
 		})
 	}
