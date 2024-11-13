fb89a9bcc08fd183d82bc5ffc4348a5894be0c7c
Mykhailo Bobrovskyi <mikhail.bobrovsky@gmail.com>
2024-11-13 23:43:40 +0200

    Add Deployment e2e test.

--- pkg/util/testingjobs/deployment/wrappers.go
+++ pkg/util/testingjobs/deployment/wrappers.go
@@ -21,6 +21,7 @@ import (
 
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"sigs.k8s.io/kueue/pkg/controller/constants"
@@ -87,6 +88,28 @@ func (d *DeploymentWrapper) Queue(q string) *DeploymentWrapper {
 // Name updated the name of the Deployment
 func (d *DeploymentWrapper) Name(n string) *DeploymentWrapper {
 	d.ObjectMeta.Name = n
+	return d
+}
+
+// Image sets an image to the default container.
+func (d *DeploymentWrapper) Image(image string, args []string) *DeploymentWrapper {
+	d.Spec.Template.Spec.Containers[0].Image = image
+	d.Spec.Template.Spec.Containers[0].Args = args
+	return d
+}
+
+// Request adds a resource request to the default container.
+func (d *DeploymentWrapper) Request(r corev1.ResourceName, v string) *DeploymentWrapper {
+	if d.Spec.Template.Spec.Containers[0].Resources.Requests == nil {
+		d.Spec.Template.Spec.Containers[0].Resources.Requests = corev1.ResourceList{}
+	}
+	d.Spec.Template.Spec.Containers[0].Resources.Requests[r] = resource.MustParse(v)
+	return d
+}
+
+// Replicas updated the replicas of the Deployment
+func (d *DeploymentWrapper) Replicas(replicas int32) *DeploymentWrapper {
+	d.Spec.Replicas = &replicas
 	return d
 }
 
