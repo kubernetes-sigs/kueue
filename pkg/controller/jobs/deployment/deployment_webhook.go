12e5f0f4d37100593c01476e01530da13ea779ef
k8s-infra-cherrypick-robot <90416843+k8s-infra-cherrypick-robot@users.noreply.github.com>
2024-11-18 05:46:54 -0800

    Use QueueNameForObject in Deployment Webhook. (#3577)
    Co-authored-by: Mykhailo Bobrovskyi <mikhail.bobrovsky@gmail.com>

--- pkg/controller/jobs/deployment/deployment_webhook.go
+++ pkg/controller/jobs/deployment/deployment_webhook.go
@@ -54,16 +54,16 @@ func SetupWebhook(mgr ctrl.Manager, _ ...jobframework.Option) error {
 var _ admission.CustomDefaulter = &Webhook{}
 
 func (wh *Webhook) Default(ctx context.Context, obj runtime.Object) error {
-	d := fromObject(obj)
-	log := ctrl.LoggerFrom(ctx).WithName("deployment-webhook").WithValues("deployment", klog.KObj(d))
+	deployment := fromObject(obj)
+
+	log := ctrl.LoggerFrom(ctx).WithName("deployment-webhook").WithValues("deployment", klog.KObj(deployment))
 	log.V(5).Info("Applying defaults")
 
-	cqLabel, ok := d.Labels[constants.QueueLabel]
-	if ok {
-		if d.Spec.Template.Labels == nil {
-			d.Spec.Template.Labels = make(map[string]string, 1)
+	if queueName := jobframework.QueueNameForObject(deployment.Object()); queueName != "" {
+		if deployment.Spec.Template.Labels == nil {
+			deployment.Spec.Template.Labels = make(map[string]string, 1)
 		}
-		d.Spec.Template.Labels[constants.QueueLabel] = cqLabel
+		deployment.Spec.Template.Labels[constants.QueueLabel] = queueName
 	}
 
 	return nil
@@ -78,11 +78,10 @@ func (wh *Webhook) ValidateCreate(context.Context, runtime.Object) (warnings adm
 }
 
 var (
-	deploymentLabelsPath         = field.NewPath("metadata", "labels")
-	deploymentQueueNameLabelPath = deploymentLabelsPath.Key(constants.QueueLabel)
-
-	podSpecQueueNameLabelPath = field.NewPath("spec", "template", "metadata", "labels").
-					Key(constants.QueueLabel)
+	labelsPath                = field.NewPath("metadata", "labels")
+	queueNameLabelPath        = labelsPath.Key(constants.QueueLabel)
+	podSpecLabelPath          = field.NewPath("spec", "template", "metadata", "labels")
+	podSpecQueueNameLabelPath = podSpecLabelPath.Key(constants.QueueLabel)
 )
 
 func (wh *Webhook) ValidateUpdate(ctx context.Context, oldObj, newObj runtime.Object) (warnings admission.Warnings, err error) {
@@ -91,11 +90,11 @@ func (wh *Webhook) ValidateUpdate(ctx context.Context, oldObj, newObj runtime.Ob
 
 	log := ctrl.LoggerFrom(ctx).WithName("deployment-webhook").WithValues("deployment", klog.KObj(newDeployment))
 	log.V(5).Info("Validating update")
-	allErrs := apivalidation.ValidateImmutableField(
-		newDeployment.GetLabels()[constants.QueueLabel],
-		oldDeployment.GetLabels()[constants.QueueLabel],
-		deploymentQueueNameLabelPath,
-	)
+
+	oldQueueName := jobframework.QueueNameForObject(oldDeployment.Object())
+	newQueueName := jobframework.QueueNameForObject(newDeployment.Object())
+
+	allErrs := apivalidation.ValidateImmutableField(oldQueueName, newQueueName, queueNameLabelPath)
 	allErrs = append(allErrs, apivalidation.ValidateImmutableField(
 		newDeployment.Spec.Template.GetLabels()[constants.QueueLabel],
 		oldDeployment.Spec.Template.GetLabels()[constants.QueueLabel],
