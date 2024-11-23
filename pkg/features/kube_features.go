2c0d1dd4dbdc23a7231ab1d75ee51e44ac787ed0
David Grove <groved@us.ibm.com>
2024-11-23 12:48:02 -0500

    review comments

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -113,6 +113,7 @@ const (
 	// owner: @dgrove-oss
 	// kep: https://github.com/kubernetes-sigs/kueue/tree/main/keps/2937-resource-transformer
 	// alpha: v0.9
+	// beta: v0.10
 	//
 	// Enable applying configurable resource transformations when computing
 	// the resource requests of a Workload
@@ -121,6 +122,7 @@ const (
 	// owner: @dgrove-oss
 	// kep: https://github.com/kubernetes-sigs/kueue/tree/main/keps/2937-resource-transformer
 	// alpha: v0.9
+	// beta: v0.10
 	//
 	// Summarize the resource requests of non-admitted Workloads in Workload.Status.resourceRequest
 	// to improve observability
