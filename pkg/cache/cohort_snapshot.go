4a7b5bc38646e0f85fbab2ee3260de334b956f89
Gabe <15304068+gabesaba@users.noreply.github.com>
2024-11-29 13:45:26 +0000

    Fix preemption.findCandidates for Hierarchical Cohorts

--- pkg/cache/cohort_snapshot.go
+++ pkg/cache/cohort_snapshot.go
@@ -29,6 +29,38 @@ func (c *CohortSnapshot) GetName() string {
 	return c.Name
 }
 
+// Root returns the root of the Cohort Tree. It expects that no cycles
+// exist in the Cohort graph.
+func (c *CohortSnapshot) Root() *CohortSnapshot {
+	if !c.HasParent() {
+		return c
+	}
+	return c.Parent().Root()
+}
+
+// SubtreeClusterQueues returns all of the ClusterQueues in the
+// subtree starting at the given Cohort. It expects that no cycles
+// exist in the Cohort graph.
+func (c *CohortSnapshot) SubtreeClusterQueues() []*ClusterQueueSnapshot {
+	return c.subtreeClusterQueuesHelper(make([]*ClusterQueueSnapshot, 0, c.subtreeClusterQueueCount()))
+}
+
+func (c *CohortSnapshot) subtreeClusterQueuesHelper(cqs []*ClusterQueueSnapshot) []*ClusterQueueSnapshot {
+	cqs = append(cqs, c.ChildCQs()...)
+	for _, cohort := range c.ChildCohorts() {
+		cqs = cohort.subtreeClusterQueuesHelper(cqs)
+	}
+	return cqs
+}
+
+func (c *CohortSnapshot) subtreeClusterQueueCount() int {
+	count := len(c.ChildCQs())
+	for _, cohort := range c.ChildCohorts() {
+		count += cohort.subtreeClusterQueueCount()
+	}
+	return count
+}
+
 // The methods below implement hierarchicalResourceNode interface.
 
 func (c *CohortSnapshot) getResourceNode() ResourceNode {
