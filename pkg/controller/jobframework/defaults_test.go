4f214e6d955bc9089c925c285868afe88a952c8f
David Grove <groved@us.ibm.com>
2024-12-06 18:36:00 -0500

    unit tests for WorkloadShouldBeSuspended

--- pkg/controller/jobframework/defaults_test.go
+++ pkg/controller/jobframework/defaults_test.go
@@ -16,32 +16,112 @@ limitations under the License.
 
 package jobframework
 
-/*
-
 import (
 	"testing"
 
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
 	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"sigs.k8s.io/kueue/pkg/features"
+	utiltesting "sigs.k8s.io/kueue/pkg/util/testing"
+	utiltestingjob "sigs.k8s.io/kueue/pkg/util/testingjobs/job"
 )
 
 func TestWorkloadShouldBeSuspended(t *testing.T) {
+	managedNamespace := &corev1.Namespace{
+		TypeMeta: metav1.TypeMeta{Kind: "Namespace", APIVersion: "v1"},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   "managed-ns",
+			Labels: map[string]string{"kubernetes.io/metadata.name": "managed-ns"},
+		},
+	}
 
+	unmanagedNamespace := &corev1.Namespace{
+		TypeMeta: metav1.TypeMeta{Kind: "Namespace", APIVersion: "v1"},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   "unmanaged-ns",
+			Labels: map[string]string{"kubernetes.io/metadata.name": "unmanaged-ns"},
+		},
+	}
 
+	ls := &metav1.LabelSelector{
+		MatchExpressions: []metav1.LabelSelectorRequirement{
+			{
+				Key:      "kubernetes.io/metadata.name",
+				Operator: metav1.LabelSelectorOpNotIn,
+				Values:   []string{unmanagedNamespace.Name},
+			},
+		},
+	}
+	namespaceSelector, _ := metav1.LabelSelectorAsSelector(ls)
 
 	cases := map[string]struct {
 		obj                        client.Object
 		manageJobsWithoutQueueName bool
+		featureGateEnabled         bool
 		wantSuspend                bool
 	}{
-		""
-		obj: v1.Object{},
-
-
-
-
+		"job with queue name ": {
+			obj:                        utiltestingjob.MakeJob("test-job", managedNamespace.Name).Queue("default").Obj(),
+			manageJobsWithoutQueueName: false,
+			featureGateEnabled:         true,
+			wantSuspend:                true,
+		},
+		"job with queue name manageJobs": {
+			obj:                        utiltestingjob.MakeJob("test-job", managedNamespace.Name).Queue("default").Obj(),
+			manageJobsWithoutQueueName: true,
+			featureGateEnabled:         true,
+			wantSuspend:                true,
+		},
+		"job without queue name": {
+			obj:                        utiltestingjob.MakeJob("test-job", managedNamespace.Name).Obj(),
+			manageJobsWithoutQueueName: false,
+			featureGateEnabled:         true,
+			wantSuspend:                false,
+		},
+		"job without queue name with manageJobs": {
+			obj:                        utiltestingjob.MakeJob("test-job", managedNamespace.Name).Obj(),
+			manageJobsWithoutQueueName: true,
+			featureGateEnabled:         true,
+			wantSuspend:                true,
+		},
+		"job without queue name with manageJobs with feature disabled": {
+			obj:                        utiltestingjob.MakeJob("test-job", managedNamespace.Name).Obj(),
+			manageJobsWithoutQueueName: true,
+			featureGateEnabled:         false,
+			wantSuspend:                true,
+		},
+		"job without queue name with manageJobs in unmanaged ns": {
+			obj:                        utiltestingjob.MakeJob("test-job", unmanagedNamespace.Name).Obj(),
+			manageJobsWithoutQueueName: true,
+			featureGateEnabled:         true,
+			wantSuspend:                false,
+		},
+		"job without queue name with manageJobs in unmanaged ns with feature disabled": {
+			obj:                        utiltestingjob.MakeJob("test-job", unmanagedNamespace.Name).Obj(),
+			manageJobsWithoutQueueName: true,
+			featureGateEnabled:         false,
+			wantSuspend:                true,
+		},
 	}
 
-}
+	for tcName, tc := range cases {
+		t.Run(tcName, func(t *testing.T) {
+			builder := utiltesting.NewClientBuilder()
+			builder.WithObjects(managedNamespace, unmanagedNamespace, tc.obj)
+			client := builder.Build()
+			ctx, _ := utiltesting.ContextWithLog(t)
 
-*/
+			features.SetFeatureGateDuringTest(t, features.ManagedJobsNamespaceSelector, tc.featureGateEnabled)
+			suspend, err := WorkloadShouldBeSuspended(ctx, tc.obj, client, tc.manageJobsWithoutQueueName, namespaceSelector)
+			if err != nil {
+				t.Errorf("Got error: %v", err)
+			}
+			if suspend != tc.wantSuspend {
+				t.Errorf("Unexpected result: got %v wanted %v", suspend, tc.wantSuspend)
+			}
+		})
+	}
+}
