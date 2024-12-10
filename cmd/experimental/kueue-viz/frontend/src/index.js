794544c2489d8b76614076cb9d4a18227f9d0cd8
Akram Ben Aissi <akram.benaissi@gmail.com>
2024-12-10 18:54:03 +0100

    Migrate to React 19 (#3799)

--- cmd/experimental/kueue-viz/frontend/src/index.js
+++ cmd/experimental/kueue-viz/frontend/src/index.js
@@ -18,12 +18,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './App.css';
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container);
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+root.render(<App />);
 
 
