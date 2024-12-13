d108f23b2d1b1aa865adae77055c2470083cac8c
7h3-3mp7y-m4n <115151332+7h3-3mp7y-m4n@users.noreply.github.com>
2024-12-13 15:26:25 +0530

    Added Log output for LocalQueue and ClusterQueue (#3605)
    * Added Log output for LocalQueue and ClusterQueue
    
    Signed-off-by: 7h3-3mp7y-m4n <emailtorash@gmail.com>
    
    * Added changes
    
    Signed-off-by: 7h3-3mp7y-m4n <emailtorash@gmail.com>
    
    * added println
    
    Signed-off-by: 7h3-3mp7y-m4n <emailtorash@gmail.com>
    
    * updated the log output using default error
    
    Signed-off-by: 7h3-3mp7y-m4n <emailtorash@gmail.com>
    
    * Added test case
    
    Signed-off-by: 7h3-3mp7y-m4n <emailtorash@gmail.com>
    
    * Added changes
    
    Signed-off-by: 7h3-3mp7y-m4n <emailtorash@gmail.com>
    
    * Adding return statements for ci-lint
    
    Signed-off-by: 7h3-3mp7y-m4n <emailtorash@gmail.com>
    
    * removed comments
    
    Signed-off-by: 7h3-3mp7y-m4n <emailtorash@gmail.com>
    
    ---------
    
    Signed-off-by: 7h3-3mp7y-m4n <emailtorash@gmail.com>

--- pkg/controller/core/workload_controller.go
+++ pkg/controller/core/workload_controller.go
@@ -607,8 +607,9 @@ func (r *WorkloadReconciler) Create(e event.CreateEvent) bool {
 	workload.AdjustResources(ctx, r.client, wlCopy)
 
 	if !workload.HasQuotaReservation(wl) {
-		if !r.queues.AddOrUpdateWorkload(wlCopy) {
-			log.V(2).Info("LocalQueue for workload didn't exist or not active; ignored for now")
+		err := r.queues.AddOrUpdateWorkload(wlCopy)
+		if err != nil {
+			log.V(2).Info(fmt.Sprintf("%s; ignored for now", err))
 		}
 		return true
 	}
@@ -711,10 +712,10 @@ func (r *WorkloadReconciler) Update(e event.UpdateEvent) bool {
 		})
 
 	case prevStatus == workload.StatusPending && status == workload.StatusPending:
-		if !r.queues.UpdateWorkload(oldWl, wlCopy) {
-			log.V(2).Info("Queue for updated workload didn't exist; ignoring for now")
+		err := r.queues.UpdateWorkload(oldWl, wlCopy)
+		if err != nil {
+			log.V(2).Info(fmt.Sprintf("%s; ignored for now", err))
 		}
-
 	case prevStatus == workload.StatusPending && (status == workload.StatusQuotaReserved || status == workload.StatusAdmitted):
 		r.queues.DeleteWorkload(oldWl)
 		if !r.cache.AddOrUpdateWorkload(wlCopy) {
@@ -737,8 +738,9 @@ func (r *WorkloadReconciler) Update(e event.UpdateEvent) bool {
 			// Here we don't take the lock as it is already taken by the wrapping
 			// function.
 			if immediate {
-				if !r.queues.AddOrUpdateWorkloadWithoutLock(wlCopy) {
-					log.V(2).Info("LocalQueue for workload didn't exist or not active; ignored for now")
+				err := r.queues.AddOrUpdateWorkloadWithoutLock(wlCopy)
+				if err != nil {
+					log.V(2).Info(fmt.Sprintf("%s; ignored for now", err))
 				}
 			}
 		})
@@ -749,8 +751,9 @@ func (r *WorkloadReconciler) Update(e event.UpdateEvent) bool {
 				updatedWl := kueue.Workload{}
 				err := r.client.Get(ctx, client.ObjectKeyFromObject(wl), &updatedWl)
 				if err == nil && workload.Status(&updatedWl) == workload.StatusPending {
-					if !r.queues.AddOrUpdateWorkload(wlCopy) {
-						log.V(2).Info("LocalQueue for workload didn't exist or not active; ignored for now")
+					err := r.queues.AddOrUpdateWorkload(wlCopy)
+					if err != nil {
+						log.V(2).Info(fmt.Sprintf("%s; ignored for now", err))
 					} else {
 						log.V(3).Info("Workload requeued after backoff")
 					}
@@ -894,8 +897,9 @@ func (h *resourceUpdatesHandler) queueReconcileForPending(ctx context.Context, _
 		log := log.WithValues("workload", klog.KObj(wlCopy))
 		log.V(5).Info("Queue reconcile for")
 		workload.AdjustResources(ctrl.LoggerInto(ctx, log), h.r.client, wlCopy)
-		if !h.r.queues.AddOrUpdateWorkload(wlCopy) {
-			log.V(2).Info("Queue for workload didn't exist")
+		err := h.r.queues.AddOrUpdateWorkload(wlCopy)
+		if err != nil {
+			log.V(2).Info(err.Error())
 		}
 	}
 }
