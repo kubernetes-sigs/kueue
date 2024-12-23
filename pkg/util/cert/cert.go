1b26f3415ba45b29665a137230ea22a87bb0187e
Varsha Prasad Narsing <varshaprasad96@gmail.com>
2024-12-23 11:49:38 -0800

    Remove the requirement for VAP
    VAP is a default admission plugin enabled while
    starting an API server for visibility. The Kueue
    controller has additional permissions to watch those
    GVKs even though it is not required. Disabling the plugin
    from api server helps in keeping it minimal and maintaining
    compatibility with previous versions of K8s.
    
    Signed-off-by: Varsha Prasad Narsing <varshaprasad96@gmail.com>

--- pkg/util/cert/cert.go
+++ pkg/util/cert/cert.go
@@ -38,8 +38,6 @@ const (
 // +kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;watch;update
 // +kubebuilder:rbac:groups="admissionregistration.k8s.io",resources=mutatingwebhookconfigurations,verbs=get;list;watch;update
 // +kubebuilder:rbac:groups="admissionregistration.k8s.io",resources=validatingwebhookconfigurations,verbs=get;list;watch;update
-// +kubebuilder:rbac:groups="admissionregistration.k8s.io",resources=validatingadmissionpolicies,verbs=get;list;watch
-// +kubebuilder:rbac:groups="admissionregistration.k8s.io",resources=validatingadmissionpolicybindings,verbs=get;list;watch
 
 // ManageCerts creates all certs for webhooks. This function is called from main.go.
 func ManageCerts(mgr ctrl.Manager, cfg config.Configuration, setupFinished chan struct{}) error {
