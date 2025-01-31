d88291f110518c72cc9c508fa085e0e2941533cb
Michal Szadkowski <michal_szadkowski@epam.com>
2025-01-31 16:24:31 +0100

    Cleanup Kubeflow MultiKueue e2e tests setup

--- hack/multikueue-e2e-test.sh
+++ hack/multikueue-e2e-test.sh
@@ -40,8 +40,6 @@ function cleanup {
     fi
     #do the image restore here for the case when an error happened during deploy
     restore_managers_image
-    # Remove the `newTag` for the `kubeflow/training-operator` to revert to the default version
-    $YQ eval 'del(.images[] | select(.name == "kubeflow/training-operator").newTag)' -i "$KUBEFLOW_MANIFEST_MANAGER/kustomization.yaml"
 }
 
 
@@ -87,16 +85,7 @@ function kind_load {
     # In order for MPI-operator and Training-operator to work on the same cluster it is required that:
     # 1. 'kubeflow.org_mpijobs.yaml' is removed from base/crds/kustomization.yaml - https://github.com/kubeflow/training-operator/issues/1930
     # 2. Training-operator deployment is modified to enable all kubeflow jobs except for mpi -  https://github.com/kubeflow/training-operator/issues/1777
-   
-    # Modify the `newTag` for the `kubeflow/training-operator` to use the one training-operator version
-    $YQ eval '(.images[] | select(.name == "kubeflow/training-operator").newTag) = env(KUBEFLOW_IMAGE_VERSION)' -i "$KUBEFLOW_MANIFEST_MANAGER/kustomization.yaml"
-    # MANAGER
-    # Only install the CRDs and not the controller to be able to
-    # have Kubeflow Jobs admitted without execution in the manager cluster.
-    kubectl config use-context "kind-${MANAGER_KIND_CLUSTER_NAME}"
-    kubectl apply --server-side -k "${KUBEFLOW_MANIFEST_MANAGER}"
-
-    # WORKERS
+    install_kubeflow "$MANAGER_KIND_CLUSTER_NAME"
     install_kubeflow "$WORKER1_KIND_CLUSTER_NAME"
     install_kubeflow "$WORKER2_KIND_CLUSTER_NAME"
     
