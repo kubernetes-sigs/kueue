a92cabd51161f5a604abc7499d9dff6e9f13d416
Michał Szadkowski <michal_szadkowski@epam.com>
2025-02-03 11:57:04 +0100

    Fix naming in MultiKueue adapters tests (#4123)

--- pkg/controller/jobs/raycluster/raycluster_multikueue_adapter_test.go
+++ pkg/controller/jobs/raycluster/raycluster_multikueue_adapter_test.go
@@ -40,7 +40,7 @@ const (
 	TestNamespace = "ns"
 )
 
-func TestMultikueueAdapter(t *testing.T) {
+func TestMultiKueueAdapter(t *testing.T) {
 	objCheckOpts := []cmp.Option{
 		cmpopts.IgnoreFields(metav1.ObjectMeta{}, "ResourceVersion"),
 		cmpopts.EquateEmpty(),
