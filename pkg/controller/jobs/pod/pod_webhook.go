67b63114b94767d95473a84c23a45ceb30c254a7
Mykhailo Bobrovskyi <mikhail.bobrovsky@gmail.com>
2025-02-05 16:30:16 +0200

    Automated cherry pick of #3952: [StatefulSet] Disallow changes to PodSpec fields other than the image field #4081: Allow to change mutable PodSpec fields. (#4154)
    * Disallow changes to PodSpec fields other than the image field.
    
    * Update pkg/controller/jobs/statefulset/statefulset_webhook_test.go
    
    Co-authored-by: Michał Woźniak <mimowo@users.noreply.github.com>
    
    * Update pkg/controller/jobs/statefulset/statefulset_webhook_test.go
    
    Co-authored-by: Michał Woźniak <mimowo@users.noreply.github.com>
    
    * Allow to change mutable PodSpec fields.
    
    * Review remarks.
    
    * Use *corev1.PodSpec instead of corev1.PodSpec
    
    ---------
    
    Co-authored-by: Michał Woźniak <mimowo@users.noreply.github.com>

--- pkg/controller/jobs/pod/pod_webhook.go
+++ pkg/controller/jobs/pod/pod_webhook.go
@@ -110,19 +110,6 @@ func getPodOptions(integrationOpts map[string]any) (*configapi.PodIntegrationOpt
 
 var _ admission.CustomDefaulter = &PodWebhook{}
 
-func containersShape(containers []corev1.Container) (result []map[string]interface{}) {
-	for _, c := range containers {
-		result = append(result, map[string]interface{}{
-			"resources": map[string]interface{}{
-				"requests": c.Resources.Requests,
-			},
-			"ports": c.Ports,
-		})
-	}
-
-	return result
-}
-
 // addRoleHash calculates the role hash and adds it to the pod's annotations
 func (p *Pod) addRoleHash() error {
 	if p.pod.Annotations == nil {
