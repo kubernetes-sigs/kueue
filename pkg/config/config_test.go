4ac885bb53d0c721b7a1578c164cfeab8df49a1f
David Grove <groved@us.ibm.com>
2025-02-17 21:22:11 -0500

    review comment: restore portion of config_test

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -248,6 +248,28 @@ queueVisibility:
 		t.Fatal(err)
 	}
 
+	podIntegrationOptionsConfig := filepath.Join(tmpDir, "podIntegrationOptions.yaml")
+	if err := os.WriteFile(podIntegrationOptionsConfig, []byte(`
+apiVersion: config.kueue.x-k8s.io/v1beta1
+kind: Configuration
+integrations:
+  frameworks:
+  - pod
+  podOptions:
+    namespaceSelector:
+      matchExpressions:
+      - key: kubernetes.io/metadata.name
+        operator: NotIn
+        values: [ kube-system, kueue-system, prohibited-namespace ]
+    podSelector:
+      matchExpressions:
+      - key: kueue-job
+        operator: In
+        values: [ "true", "True", "yes" ]
+`), os.FileMode(0600)); err != nil {
+		t.Fatal(err)
+	}
+
 	multiKueueConfig := filepath.Join(tmpDir, "multiKueue.yaml")
 	if err := os.WriteFile(multiKueueConfig, []byte(`
 apiVersion: config.kueue.x-k8s.io/v1beta1
@@ -746,6 +768,66 @@ webhook:
 					UpdateIntervalSeconds: 10,
 					ClusterQueues: &configapi.ClusterQueueVisibility{
 						MaxCount: 0,
+					},
+				},
+				MultiKueue:                   defaultMultiKueue,
+				ManagedJobsNamespaceSelector: defaultManagedJobsNamespaceSelector,
+			},
+			wantOptions: ctrl.Options{
+				HealthProbeBindAddress: configapi.DefaultHealthProbeBindAddress,
+				Metrics: metricsserver.Options{
+					BindAddress: configapi.DefaultMetricsBindAddress,
+				},
+				LeaderElection:                true,
+				LeaderElectionID:              configapi.DefaultLeaderElectionID,
+				LeaderElectionResourceLock:    resourcelock.LeasesResourceLock,
+				LeaderElectionReleaseOnCancel: true,
+				LeaseDuration:                 ptr.To(configapi.DefaultLeaderElectionLeaseDuration),
+				RenewDeadline:                 ptr.To(configapi.DefaultLeaderElectionRenewDeadline),
+				RetryPeriod:                   ptr.To(configapi.DefaultLeaderElectionRetryPeriod),
+				WebhookServer: &webhook.DefaultServer{
+					Options: webhook.Options{
+						Port: configapi.DefaultWebhookPort,
+					},
+				},
+			},
+		},
+		{
+			name:       "pod integration options config",
+			configFile: podIntegrationOptionsConfig,
+			wantConfiguration: configapi.Configuration{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: configapi.GroupVersion.String(),
+					Kind:       "Configuration",
+				},
+				Namespace:                  ptr.To(configapi.DefaultNamespace),
+				ManageJobsWithoutQueueName: false,
+				InternalCertManagement:     enableDefaultInternalCertManagement,
+				ClientConnection:           defaultClientConnection,
+				QueueVisibility:            defaultQueueVisibility,
+				Integrations: &configapi.Integrations{
+					Frameworks: []string{
+						"pod",
+					},
+					PodOptions: &configapi.PodIntegrationOptions{
+						NamespaceSelector: &metav1.LabelSelector{
+							MatchExpressions: []metav1.LabelSelectorRequirement{
+								{
+									Key:      corev1.LabelMetadataName,
+									Operator: metav1.LabelSelectorOpNotIn,
+									Values:   []string{"kube-system", "kueue-system", "prohibited-namespace"},
+								},
+							},
+						},
+						PodSelector: &metav1.LabelSelector{
+							MatchExpressions: []metav1.LabelSelectorRequirement{
+								{
+									Key:      "kueue-job",
+									Operator: metav1.LabelSelectorOpIn,
+									Values:   []string{"true", "True", "yes"},
+								},
+							},
+						},
 					},
 				},
 				MultiKueue:                   defaultMultiKueue,
