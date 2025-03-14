4d3a24cfdb2a555da90532a4e2ed59974cc6072e
vladikkuzn <vladikkuzn1@gmail.com>
2025-03-14 13:58:04 +0200

    Support JAX in Kueue using training-operator 1.9

---
+++ test/e2e/singlecluster/jaxjob_test.go
@@ -0,0 +1,142 @@
+/*
+Copyright The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package e2e
+
+import (
+	kftraining "github.com/kubeflow/training-operator/pkg/apis/kubeflow.org/v1"
+	"github.com/onsi/ginkgo/v2"
+	"github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	kueue "sigs.k8s.io/kueue/apis/kueue/v1beta1"
+	"sigs.k8s.io/kueue/pkg/controller/jobs/kubeflow/jobs/jaxjob"
+	"sigs.k8s.io/kueue/pkg/util/testing"
+	jaxjobtesting "sigs.k8s.io/kueue/pkg/util/testingjobs/jaxjob"
+	"sigs.k8s.io/kueue/test/util"
+)
+
+var _ = ginkgo.Describe("JAX integration", func() {
+	const (
+		resourceFlavorName = "jax-rf"
+		clusterQueueName   = "jax-cq"
+		localQueueName     = "jax-lq"
+	)
+
+	var (
+		ns *corev1.Namespace
+		rf *kueue.ResourceFlavor
+		cq *kueue.ClusterQueue
+		lq *kueue.LocalQueue
+	)
+
+	ginkgo.BeforeEach(func() {
+		ns = util.CreateNamespaceFromPrefixWithLog(ctx, k8sClient, "jax-e2e-")
+
+		rf = testing.MakeResourceFlavor(resourceFlavorName).NodeLabel("instance-type", "on-demand").Obj()
+		gomega.Expect(k8sClient.Create(ctx, rf)).To(gomega.Succeed())
+
+		cq = testing.MakeClusterQueue(clusterQueueName).
+			ResourceGroup(
+				*testing.MakeFlavorQuotas(resourceFlavorName).
+					Resource(corev1.ResourceCPU, "5").
+					Resource(corev1.ResourceMemory, "10Gi").
+					Obj(),
+			).
+			Preemption(kueue.ClusterQueuePreemption{
+				WithinClusterQueue: kueue.PreemptionPolicyLowerPriority,
+			}).
+			Obj()
+		gomega.Expect(k8sClient.Create(ctx, cq)).To(gomega.Succeed())
+
+		lq = testing.MakeLocalQueue(localQueueName, ns.Name).ClusterQueue(cq.Name).Obj()
+		gomega.Expect(k8sClient.Create(ctx, lq)).To(gomega.Succeed())
+	})
+	ginkgo.AfterEach(func() {
+		gomega.Expect(util.DeleteAllJAXJobsInNamespace(ctx, k8sClient, ns)).To(gomega.Succeed())
+		gomega.Expect(util.DeleteNamespace(ctx, k8sClient, ns)).To(gomega.Succeed())
+		util.ExpectObjectToBeDeleted(ctx, k8sClient, cq, true)
+		util.ExpectObjectToBeDeleted(ctx, k8sClient, rf, true)
+		util.ExpectAllPodsInNamespaceDeleted(ctx, k8sClient, ns)
+	})
+
+	ginkgo.When("JAX created", func() {
+		ginkgo.It("should admit group with leader only", func() {
+			jax := jaxjobtesting.MakeJAXJob("jax-simple", ns.Name).
+				Label("kueue.x-k8s.io/queue-name", localQueueName).
+				Suspend(false).
+				SetTypeMeta().
+				JAXReplicaSpecsDefault().
+				Parallelism(kftraining.JAXJobReplicaTypeWorker, 2).
+				Image(kftraining.JAXJobReplicaTypeWorker, util.E2eTestAgnHostImage, util.BehaviorWaitForDeletion).
+				//Command(kftraining.JAXJobReplicaTypeWorker, []string{"python3", "train.py"}).
+				Request(kftraining.JAXJobReplicaTypeWorker, corev1.ResourceCPU, "1").
+				Request(kftraining.JAXJobReplicaTypeWorker, corev1.ResourceMemory, "200Mi").
+				Obj()
+
+			ginkgo.By("Create a JAX", func() {
+				gomega.Expect(k8sClient.Create(ctx, jax)).To(gomega.Succeed())
+			})
+
+			ginkgo.By("Waiting for replicas is ready", func() {
+				createdJAX := &kftraining.JAXJob{}
+
+				gomega.Eventually(func() bool {
+					err := k8sClient.Get(ctx, client.ObjectKeyFromObject(jax), createdJAX)
+					if err != nil {
+						ginkgo.GinkgoLogr.Info("Failed to fetch JAX", "error", err)
+						return false
+					}
+
+					workerReplicaStatus, ok := createdJAX.Status.ReplicaStatuses[kftraining.JAXJobReplicaTypeWorker]
+					if !ok {
+						return false
+					}
+
+					if workerReplicaStatus.Active != int32(2) {
+						return false
+					}
+
+					for _, condition := range createdJAX.Status.Conditions {
+						if condition.Type == kftraining.JobRunning && condition.Status == corev1.ConditionTrue {
+							return true
+						}
+					}
+					return false
+				}, util.LongTimeout, util.Interval).Should(gomega.BeTrue())
+			})
+
+			wlLookupKey := types.NamespacedName{
+				Name:      jaxjob.GetWorkloadNameForJAXJob(jax.Name, jax.UID),
+				Namespace: ns.Name,
+			}
+			createdWorkload := &kueue.Workload{}
+			ginkgo.By("Check workload is created", func() {
+				gomega.Expect(k8sClient.Get(ctx, wlLookupKey, createdWorkload)).To(gomega.Succeed())
+			})
+
+			ginkgo.By("Delete the JAX", func() {
+				util.ExpectObjectToBeDeleted(ctx, k8sClient, jax, true)
+			})
+
+			ginkgo.By("Check workload is deleted", func() {
+				util.ExpectObjectToBeDeletedWithTimeout(ctx, k8sClient, createdWorkload, false, util.LongTimeout)
+			})
+		})
+	})
+})
