00bfc4352f7898a71ed6a8e640a7b843fb431536
Akram Ben Aissi <akram.benaissi@gmail.com>
2025-03-20 08:52:39 +0100

    linting kueue-viz test bash scripts

--- hack/e2e-viz-dind-frontend.sh
+++ hack/e2e-viz-dind-frontend.sh
@@ -5,15 +5,10 @@ set -e
 # Install missing dependencies
 apt-get update && apt-get install -y curl make xdg-utils
 
-SOURCE_DIR="$(cd "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
-# ROOT_DIR="$SOURCE_DIR/.."
-# source "${SOURCE_DIR}/e2e-common.sh"
-
-
 # Function to clean up background processes
 cleanup() {
   echo "Cleaning up kueue-viz processes"
-  kill $FRONTEND_PID
+  kill "${FRONTEND_PID}"
 }
 
 # Start kueue-viz frontend
--- hack/e2e-viz.sh
+++ hack/e2e-viz.sh
@@ -4,6 +4,7 @@ set -e
 
 SOURCE_DIR="$(cd "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
 ROOT_DIR="$SOURCE_DIR/.."
+# shellcheck source=hack/e2e-common.sh
 source "${SOURCE_DIR}/e2e-common.sh"
 
 
@@ -17,16 +18,16 @@ cleanup() {
 # Set trap to clean up on exit
 trap cleanup EXIT
 
-cluster_create "$KIND_CLUSTER_NAME"
-echo Waiting for kind cluster $KIND_CLUSTER_NAME to start...
-cluster_kueue_deploy "$KIND_CLUSTER_NAME"
+cluster_create "${KIND_CLUSTER_NAME}"
+echo Waiting for kind cluster "${KIND_CLUSTER_NAME}" to start...
+cluster_kueue_deploy "${KIND_CLUSTER_NAME}"
 kubectl wait deploy/kueue-controller-manager -nkueue-system --for=condition=available --timeout=5m
 
 # Deploy kueue-viz resources
-kubectl create -f $i "$ROOT_DIR/cmd/experimental/kueue-viz/examples/"
+kubectl create -f "${ROOT_DIR}/cmd/experimental/kueue-viz/examples/"
 
 # Start kueue-viz backend
-cd cmd/experimental/kueue-viz/backend
+cd "${ROOT_DIR}/cmd/experimental/kueue-viz/backend"
 go build -o bin/kueue-viz
 ./bin/kueue-viz & BACKEND_PID=$!
 cd -
