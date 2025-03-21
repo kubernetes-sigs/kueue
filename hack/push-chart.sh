4613150256f22edfe82a468eea2e2444d9d02561
Akram Ben Aissi <akram.benaissi@gmail.com>
2025-03-21 17:14:32 +0700

    Fix push-char for kueue-viz images (#4740)

--- hack/push-chart.sh
+++ hack/push-chart.sh
@@ -48,9 +48,15 @@ readonly default_image_repo
 # Update the image repo, tag and policy
 ${YQ}  e  ".controllerManager.manager.image.repository = \"${image_repository}\" | .controllerManager.manager.image.tag = \"${chart_version}\" | .controllerManager.manager.image.pullPolicy = \"IfNotPresent\"" -i charts/kueue/values.yaml
 
+# Update the kueueViz images in values.yaml
+${YQ} e ".kueueViz.backend.image = \"${image_repository}/kueue-viz-backend:${chart_version}\" | .kueueViz.frontend.image = \"${image_repository}/kueue-viz-frontend:${chart_version}\"" -i charts/kueue/values.yaml
+
 ${HELM} package --version "${chart_version}" --app-version "${chart_version}" charts/kueue -d "${DEST_CHART_DIR}"
 
 # Revert the image changes
 ${YQ}  e  ".controllerManager.manager.image.repository = \"${default_image_repo}\" | .controllerManager.manager.image.tag = \"main\" | .controllerManager.manager.image.pullPolicy = \"Always\"" -i charts/kueue/values.yaml
+
+# Revert the kueueViz image changes
+${YQ} e ".kueueViz.backend.image = \"us-central1-docker.pkg.dev/k8s-staging-images/kueue/kueue-viz-backend:main-latest\" | .kueueViz.frontend.image = \"us-central1-docker.pkg.dev/k8s-staging-images/kueue/kueue-viz-frontend:main-latest\"" -i charts/kueue/values.yaml
 
 ${HELM} push "bin/kueue-${chart_version}.tgz" "oci://${HELM_CHART_REPO}"
