8b1a961d661adcd922c551f1600233cbb0fa91d8
Mykhailo Bobrovskyi <mikhail.bobrovsky@gmail.com>
2025-04-11 18:58:15 +0300

    Return ErrManagedOwnersChainLimitReached if managedOwnersChainLimit reached.

--- pkg/controller/jobframework/defaults.go
+++ pkg/controller/jobframework/defaults.go
@@ -49,7 +49,7 @@ func ApplyDefaultForSuspend(ctx context.Context, job GenericJob, k8sClient clien
 func WorkloadShouldBeSuspended(ctx context.Context, jobObj client.Object, k8sClient client.Client,
 	manageJobsWithoutQueueName bool, managedJobsNamespaceSelector labels.Selector) (bool, error) {
 	// Do not default suspend a job whose ancestor is already managed by Kueue
-	ancestorJob, err := FindAncestorJobManagedByKueue(ctx, k8sClient, nil, jobObj, manageJobsWithoutQueueName)
+	ancestorJob, err := FindAncestorJobManagedByKueue(ctx, k8sClient, jobObj, manageJobsWithoutQueueName)
 	if err != nil || ancestorJob != nil {
 		return false, err
 	}
