epam/kubernetes-kueue

Allow to delete queue-name on deployment.

pkg/controller/jobs/deployment/deployment_webhook.go
@@ -27,7 +27,8 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 
-	"sigs.k8s.io/kueue/pkg/controller/constants"
+	"sigs.k8s.io/kueue/pkg/constants"
+	controllerconsts "sigs.k8s.io/kueue/pkg/controller/constants"
 	"sigs.k8s.io/kueue/pkg/controller/jobframework"
 	"sigs.k8s.io/kueue/pkg/controller/jobframework/webhook"
 )
@@ -62,7 +63,11 @@ func (wh *Webhook) Default(ctx context.Context, obj runtime.Object) error {
 		if deployment.Spec.Template.Labels == nil {
 			deployment.Spec.Template.Labels = make(map[string]string, 1)
 		}
-		deployment.Spec.Template.Labels[constants.QueueLabel] = queueName
+		deployment.Labels[constants.ManagedByKueueLabel] = "true"
+		deployment.Spec.Template.Labels[controllerconsts.QueueLabel] = queueName
+	} else if deployment.Labels[constants.ManagedByKueueLabel] == "true" {
+		delete(deployment.Labels, constants.ManagedByKueueLabel)
+		delete(deployment.Spec.Template.Labels, controllerconsts.QueueLabel)
 	}
 
 	return nil
@@ -85,7 +90,7 @@ func (wh *Webhook) ValidateCreate(ctx context.Context, obj runtime.Object) (warn
 
 var (
 	labelsPath         = field.NewPath("metadata", "labels")
-	queueNameLabelPath = labelsPath.Key(constants.QueueLabel)
+	queueNameLabelPath = labelsPath.Key(controllerconsts.QueueLabel)
 )
 
 func (wh *Webhook) ValidateUpdate(ctx context.Context, oldObj, newObj runtime.Object) (warnings admission.Warnings, err error) {
@@ -101,9 +106,8 @@ func (wh *Webhook) ValidateUpdate(ctx context.Context, oldObj, newObj runtime.Ob
 	allErrs := field.ErrorList{}
 	allErrs = append(allErrs, jobframework.ValidateQueueName(newDeployment.Object())...)
 
-	// Prevents updating the queue-name if at least one Pod is not suspended
-	// or if the queue-name has been deleted.
-	if oldDeployment.Status.ReadyReplicas > 0 || newQueueName == "" {
+	// Prevents updating the queue-name if at least one Pod is not suspended.
+	if oldDeployment.Status.ReadyReplicas > 0 {
 		allErrs = append(allErrs, apivalidation.ValidateImmutableField(oldQueueName, newQueueName, queueNameLabelPath)...)
 	}
 

kubernetes-sigs/kueue

pkg/controller/jobs/deployment/deployment_webhook.go
@@ -74,6 +74,8 @@ func (wh *Webhook) Default(ctx context.Context, obj runtime.Object) error {
 	if err != nil {
 		return err
 	}
+
+	queueName := jobframework.QueueNameForObject(deployment.Object())
 	if suspend {
 		if deployment.Spec.Template.Annotations == nil {
 			deployment.Spec.Template.Annotations = make(map[string]string, 1)
@@ -90,6 +92,10 @@ func (wh *Webhook) Default(ctx context.Context, obj runtime.Object) error {
 		if priorityClass := jobframework.WorkloadPriorityClassName(deployment.Object()); priorityClass != "" {
 			deployment.Spec.Template.Labels[controllerconstants.WorkloadPriorityClassLabel] = priorityClass
 		}
+	}
+
+	if queueName == "" {
+		delete(deployment.Spec.Template.Labels, controllerconstants.QueueLabel)
 	}
 
 	return nil
@@ -128,9 +134,8 @@ func (wh *Webhook) ValidateUpdate(ctx context.Context, oldObj, newObj runtime.Ob
 	allErrs := jobframework.ValidateQueueName(newDeployment.Object())
 	allErrs = append(allErrs, jobframework.ValidateUpdateForWorkloadPriorityClassName(oldDeployment.Object(), newDeployment.Object())...)
 
-	// Prevents updating the queue-name if at least one Pod is not suspended
-	// or if the queue-name has been deleted.
-	if oldDeployment.Status.ReadyReplicas > 0 || newQueueName == "" {
+	// Prevents updating the queue-name if at least one Pod is not suspended.
+	if oldDeployment.Status.ReadyReplicas > 0 {
 		allErrs = append(allErrs, apivalidation.ValidateImmutableField(oldQueueName, newQueueName, queueNameLabelPath)...)
 	}
 