332b90f00564f081998fa861c7927778d86e98d2
Mykhailo Bobrovskyi <mikhail.bobrovsky@gmail.com>
2025-04-15 15:03:45 +0300

    Update test/e2e/singlecluster/leaderworkerset_test.go
    Co-authored-by: Patryk Bundyra <pbundyra@google.com>

--- test/e2e/singlecluster/leaderworkerset_test.go
+++ test/e2e/singlecluster/leaderworkerset_test.go
@@ -954,7 +954,7 @@ var _ = ginkgo.Describe("LeaderWorkerSet integration", func() {
 				}, util.Timeout, util.Interval).Should(gomega.Succeed())
 			})
 
-			ginkgo.By("Waiting for replicas is not ready", func() {
+			ginkgo.By("Waiting for replicas to not be ready", func() {
 				createdLeaderWorkerSet := &leaderworkersetv1.LeaderWorkerSet{}
 
 				gomega.Eventually(func(g gomega.Gomega) {
