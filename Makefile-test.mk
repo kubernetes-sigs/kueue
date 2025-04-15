d2fc74ac426b1ba0d6e9e5694eaac020fac5d2c4
Tomas Tormo <tomas.tormo@nearform.com>
2025-04-15 11:59:07 +0100

    Set the E2E_KIND_VERSION mk variable based on the E2E_K8S_VERSION (#4978)

--- Makefile-test.mk
+++ Makefile-test.mk
@@ -55,9 +55,12 @@ endif
 
 # Folder where the e2e tests are located.
 E2E_TARGET ?= ./test/e2e/...
-E2E_KIND_VERSION ?= kindest/node:v1.32.0
-# E2E_K8S_VERSIONS sets the list of k8s versions included in test-e2e-all
-E2E_K8S_VERSIONS ?= 1.29.13 1.30.9 1.31.5 1.32.1
+E2E_K8S_VERSIONS ?= 1.30.10 1.31.6 1.32.3
+E2E_K8S_VERSION ?= 1.32
+E2E_K8S_FULL_VERSION ?= $(filter $(E2E_K8S_VERSION).%,$(E2E_K8S_VERSIONS))
+# Default to E2E_K8S_VERSION.0 if no match is found
+E2E_K8S_FULL_VERSION := $(or $(E2E_K8S_FULL_VERSION),$(E2E_K8S_VERSION).0)
+E2E_KIND_VERSION ?= kindest/node:v$(E2E_K8S_FULL_VERSION)
 
 # For local testing, we should allow user to use different kind cluster name
 # Default will delete default kind cluster
