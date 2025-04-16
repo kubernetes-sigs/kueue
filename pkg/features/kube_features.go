1ade8c4dc698454e922c3ecdb15f099860736fc9
Yuki Iwai <yuki.iwai.tz@gmail.com>
2025-04-17 00:47:08 +0900

    Fix HierarchicalCohorts feature gate (#5012)
    Signed-off-by: Yuki Iwai <yuki.iwai.tz@gmail.com>

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -164,11 +164,11 @@ const (
 	// Enable to set use LeastAlloactedFit algorithm for TAS
 	TASProfileMixed featuregate.Feature = "TASProfileMixed"
 
-	// owner: @kannon92
+	// owner: @mwielgus
 	// kep: https://github.com/kubernetes-sigs/kueue/tree/main/keps/79-hierarchical-cohorts
 	//
-	// Enable hierarchical cohorts controller
-	HierarchialCohorts featuregate.Feature = "HierarchialCohorts"
+	// Enable hierarchical cohorts
+	HierarchicalCohorts featuregate.Feature = "HierarchicalCohorts"
 )
 
 func init() {
@@ -258,7 +258,7 @@ var defaultVersionedFeatureGates = map[featuregate.Feature]featuregate.Versioned
 	TASProfileMixed: {
 		{Version: version.MustParse("0.11"), Default: false, PreRelease: featuregate.Deprecated},
 	},
-	HierarchialCohorts: {
+	HierarchicalCohorts: {
 		{Version: version.MustParse("0.11"), Default: true, PreRelease: featuregate.Beta},
 	},
 }
