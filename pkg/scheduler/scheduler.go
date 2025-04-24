5398d53855635cc97e0d327783549cc06604f571
Ace Eldeib <alexeldeib@gmail.com>
2025-04-24 10:22:32 -0400

    Automated cherry pick of #4935: fix: invalid SSA patch due to field manager (#5088)
    * fix: invalid SSA patch due to field manager
    
    * test: add integration test to repro unreliably
    
    * fix: spurious diffs and requeue when skipped
    
    * chore: cleanup requeue condition
    
    * chore: tidy again
    
    * chore: patch helper

--- pkg/scheduler/scheduler.go
+++ pkg/scheduler/scheduler.go
@@ -594,8 +594,7 @@ func (s *Scheduler) requeueAndUpdate(ctx context.Context, e entry) {
 	log.V(2).Info("Workload re-queued", "workload", klog.KObj(e.Obj), "clusterQueue", klog.KRef("", e.ClusterQueue), "queue", klog.KRef(e.Obj.Namespace, e.Obj.Spec.QueueName), "requeueReason", e.requeueReason, "added", added, "status", e.status)
 
 	if e.status == notNominated || e.status == skipped {
-		patch := workload.BaseSSAWorkload(e.Obj)
-		workload.AdmissionStatusPatch(e.Obj, patch, true)
+		patch := workload.PrepareWorkloadPatch(e.Obj, true)
 		reservationIsChanged := workload.UnsetQuotaReservationWithCondition(patch, "Pending", e.inadmissibleMsg, s.clock.Now())
 		resourceRequestsIsChanged := workload.PropagateResourceRequests(patch, &e.Info)
 		if reservationIsChanged || resourceRequestsIsChanged {
