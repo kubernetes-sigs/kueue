98449239f2b8b7d950c55026eb7df871522a704c
k8s-infra-cherrypick-robot <90416843+k8s-infra-cherrypick-robot@users.noreply.github.com>
2025-06-03 01:44:38 -0700

    [release-0.12] Prevent removal of queue-name label in namespaces with default localqueue (#5460)
    * block removal of localqueue label if namespace has default localqueue
    
    * add unit tests
    
    * fix linter error in imports
    
    * unit test for localqueue deletion on leaderworkerset
    
    ---------
    
    Co-authored-by: David Grove <groved@us.ibm.com>

--- pkg/controller/jobframework/validation_test.go
+++ pkg/controller/jobframework/validation_test.go
@@ -21,15 +21,70 @@ import (
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
+	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 	"k8s.io/utils/ptr"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	kueue "sigs.k8s.io/kueue/apis/kueue/v1beta1"
+	"sigs.k8s.io/kueue/pkg/controller/constants"
+	"sigs.k8s.io/kueue/pkg/features"
+	"sigs.k8s.io/kueue/pkg/podset"
+	utiltestingjob "sigs.k8s.io/kueue/pkg/util/testingjobs/job"
 )
 
 var (
 	testPath = field.NewPath("spec")
 )
+
+type testGenericJob struct {
+	*batchv1.Job
+}
+
+var _ GenericJob = (*testGenericJob)(nil)
+
+func (j *testGenericJob) Object() client.Object {
+	return j.Job
+}
+
+func (j *testGenericJob) IsSuspended() bool {
+	return ptr.Deref(j.Spec.Suspend, false)
+}
+
+func (j *testGenericJob) Suspend() {
+	j.Spec.Suspend = ptr.To(true)
+}
+
+func (j *testGenericJob) RunWithPodSetsInfo([]podset.PodSetInfo) error {
+	panic("not implemented")
+}
+
+func (j *testGenericJob) RestorePodSetsInfo([]podset.PodSetInfo) bool {
+	panic("not implemented")
+}
+
+func (j *testGenericJob) Finished() (string, bool, bool) {
+	panic("not implemented")
+}
+
+func (j *testGenericJob) PodSets() ([]kueue.PodSet, error) {
+	panic("not implemented")
+}
+
+func (j *testGenericJob) IsActive() bool {
+	panic("not implemented")
+}
+
+func (j *testGenericJob) PodsReady() bool {
+	panic("not implemented")
+}
+
+func (j *testGenericJob) GVK() schema.GroupVersionKind {
+	panic("not implemented")
+}
 
 func TestValidateImmutablePodSpec(t *testing.T) {
 	testCases := map[string]struct {
@@ -234,6 +289,78 @@ func TestValidateImmutablePodSpec(t *testing.T) {
 	for name, tc := range testCases {
 		t.Run(name, func(t *testing.T) {
 			gotErr := ValidateImmutablePodGroupPodSpec(tc.newPodSpec, tc.oldPodSpec, testPath)
+			if diff := cmp.Diff(tc.wantErr, gotErr, cmpopts.IgnoreFields(field.Error{}, "BadValue", "Detail")); diff != "" {
+				t.Errorf("Unexpected error (-want,+got):\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestValidateUpdateForQueueName(t *testing.T) {
+	t.Cleanup(EnableIntegrationsForTest(t, "batch/job"))
+	fieldString := field.NewPath("metadata").Child("labels").Key(constants.QueueLabel).String()
+	testCases := map[string]struct {
+		oldJob                   *batchv1.Job
+		newJob                   *batchv1.Job
+		defaultLocalQueueEnabled bool
+		nsHasDefaultQueue        bool
+		wantErr                  field.ErrorList
+	}{
+		"local queue cannot be changed if job is not suspended": {
+			oldJob:                   utiltestingjob.MakeJob("test-job", "ns1").Queue("lq1").Suspend(false).Obj(),
+			newJob:                   utiltestingjob.MakeJob("test-job", "ns1").Queue("lq2").Suspend(false).Obj(),
+			defaultLocalQueueEnabled: true,
+			wantErr: field.ErrorList{
+				&field.Error{
+					Type:  field.ErrorTypeInvalid,
+					Field: fieldString,
+				},
+			},
+		},
+		"local queue can be changed": {
+			oldJob:                   utiltestingjob.MakeJob("test-job", "ns1").Queue("lq1").Suspend(true).Obj(),
+			newJob:                   utiltestingjob.MakeJob("test-job", "ns1").Queue("lq2").Suspend(true).Obj(),
+			nsHasDefaultQueue:        true,
+			defaultLocalQueueEnabled: true,
+		},
+		"local queue can be changed from default": {
+			oldJob:                   utiltestingjob.MakeJob("test-job", "ns1").Queue("default").Suspend(true).Obj(),
+			newJob:                   utiltestingjob.MakeJob("test-job", "ns1").Queue("lq2").Suspend(true).Obj(),
+			nsHasDefaultQueue:        true,
+			defaultLocalQueueEnabled: true,
+		},
+		"local queue cannot be removed if default queue exists and feature is enabled": {
+			oldJob:                   utiltestingjob.MakeJob("test-job", "ns1").Suspend(true).Queue("lq1").Obj(),
+			newJob:                   utiltestingjob.MakeJob("test-job", "ns1").Suspend(true).Queue("").Obj(),
+			nsHasDefaultQueue:        true,
+			defaultLocalQueueEnabled: true,
+			wantErr: field.ErrorList{
+				&field.Error{
+					Type:  field.ErrorTypeInvalid,
+					Field: fieldString,
+				},
+			},
+		},
+		"local queue can be removed if default queue does not exists and feature is enabled": {
+			oldJob:                   utiltestingjob.MakeJob("test-job", "ns1").Suspend(true).Queue("lq1").Obj(),
+			newJob:                   utiltestingjob.MakeJob("test-job", "ns1").Suspend(true).Queue("").Obj(),
+			nsHasDefaultQueue:        false,
+			defaultLocalQueueEnabled: true,
+		},
+		"local queue can be removed if feature is not enabled": {
+			oldJob:                   utiltestingjob.MakeJob("test-job", "ns1").Suspend(true).Queue("lq1").Obj(),
+			newJob:                   utiltestingjob.MakeJob("test-job", "ns1").Suspend(true).Queue("").Obj(),
+			nsHasDefaultQueue:        true,
+			defaultLocalQueueEnabled: false,
+		},
+	}
+
+	for tcName, tc := range testCases {
+		t.Run(tcName, func(t *testing.T) {
+			features.SetFeatureGateDuringTest(t, features.LocalQueueDefaulting, tc.defaultLocalQueueEnabled)
+			oldGJ := &testGenericJob{Job: tc.oldJob}
+			newGJ := &testGenericJob{Job: tc.newJob}
+			gotErr := validateUpdateForQueueName(oldGJ, newGJ, func(string) bool { return tc.nsHasDefaultQueue })
 			if diff := cmp.Diff(tc.wantErr, gotErr, cmpopts.IgnoreFields(field.Error{}, "BadValue", "Detail")); diff != "" {
 				t.Errorf("Unexpected error (-want,+got):\n%s", diff)
 			}
