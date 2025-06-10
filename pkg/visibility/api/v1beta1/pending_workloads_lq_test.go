f31a4a4d035efd7c5790fea692f92888ee82646b
Tomas Tormo <tomas.tormo@nearform.com>
2025-06-10 22:51:27 +0200

    Remove redundant scheme setup code in unit tests

--- pkg/visibility/api/v1beta1/pending_workloads_lq_test.go
+++ pkg/visibility/api/v1beta1/pending_workloads_lq_test.go
@@ -52,11 +52,7 @@ func TestPendingWorkloadsInLQ(t *testing.T) {
 		Limit:  constants.DefaultPendingWorkloadsLimit,
 	}
 
-	scheme := runtime.NewScheme()
-	if err := kueue.AddToScheme(scheme); err != nil {
-		t.Fatalf("Failed adding kueue scheme: %s", err)
-	}
-	if err := visibility.AddToScheme(scheme); err != nil {
+	if err := visibility.AddToScheme(runtime.NewScheme()); err != nil {
 		t.Fatalf("Failed adding kueue scheme: %s", err)
 	}
 
