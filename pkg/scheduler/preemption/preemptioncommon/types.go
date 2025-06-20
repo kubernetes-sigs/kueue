e4a0e3d25de9d79578081671676c1713241c86f3
Gabe <15304068+gabesaba@users.noreply.github.com>
2025-06-20 11:12:30 +0000

    Rename PriorityBased to Preempt

--- pkg/scheduler/preemption/preemptioncommon/types.go
+++ pkg/scheduler/preemption/preemptioncommon/types.go
@@ -16,17 +16,17 @@ limitations under the License.
 
 package preemptioncommon
 
+// PreemptionPossibility represents the result
+// of a preemption simulation.
 type PreemptionPossibility int
 
-// Preemption Oracle returns one of three possible
-// results of the preemption simulation.
 const (
-	// Preemption in the given resource flavor is
-	// impossible
-	None PreemptionPossibility = iota
-	// Priority-based preemption may be possible
-	// but reclaim is impossible
-	PriorityBased
-	// Reclaim may be possible
+	// NoCandidates were found.
+	NoCandidates PreemptionPossibility = iota
+	// Preemption targets were found.
+	Preempt
+	// Preemption targets were found, and
+	// all of them are outside of preempting
+	// ClusterQueue.
 	Reclaim
 )
