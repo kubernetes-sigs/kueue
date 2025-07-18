da68f91889f19d61604424f50eb75b1eba6df842
Amy Chen <hi@amycod.es>
2025-07-18 10:42:35 -0700

    update test

--- test/integration/singlecluster/scheduler/scheduler_test.go
+++ test/integration/singlecluster/scheduler/scheduler_test.go
@@ -2631,21 +2631,21 @@ var _ = ginkgo.Describe("Scheduler", func() {
 				Cohort("bug-test").
 				ResourceGroup(*testing.MakeFlavorQuotas("on-demand").Resource(corev1.ResourceCPU, "8").Obj()).
 				Obj()
-			gomega.ExpectWithOffset(1, k8sClient.Create(ctx, cq1)).Should(gomega.Succeed())
+			gomega.Expect(k8sClient.Create(ctx, cq1)).Should(gomega.Succeed())
 
 			// ClusterQueue with 0 CPU nominal quota - this will need to borrow
 			cq2 = testing.MakeClusterQueue("cluster-queue-2").
 				Cohort("bug-test").
 				ResourceGroup(*testing.MakeFlavorQuotas("on-demand").Resource(corev1.ResourceCPU, "0").Obj()).
 				Obj()
-			gomega.ExpectWithOffset(1, k8sClient.Create(ctx, cq2)).Should(gomega.Succeed())
+			gomega.Expect(k8sClient.Create(ctx, cq2)).Should(gomega.Succeed())
 
 			ginkgo.By("Creating LocalQueues for the corresponding ClusterQueues")
 			lq1 = testing.MakeLocalQueue("local-queue-1", ns.Name).ClusterQueue(cq1.Name).Obj()
-			gomega.ExpectWithOffset(1, k8sClient.Create(ctx, lq1)).Should(gomega.Succeed())
+			gomega.Expect(k8sClient.Create(ctx, lq1)).Should(gomega.Succeed())
 
 			lq2 = testing.MakeLocalQueue("local-queue-2", ns.Name).ClusterQueue(cq2.Name).Obj()
-			gomega.ExpectWithOffset(1, k8sClient.Create(ctx, lq2)).Should(gomega.Succeed())
+			gomega.Expect(k8sClient.Create(ctx, lq2)).Should(gomega.Succeed())
 
 			ginkgo.By("Deleting cluster-queue-1 which has 8 CPU nominal quota")
 			gomega.Expect(util.DeleteObject(ctx, k8sClient, cq1)).To(gomega.Succeed())
@@ -2653,7 +2653,7 @@ var _ = ginkgo.Describe("Scheduler", func() {
 			ginkgo.By("Creating a workload that requests 8 CPU - should not be admitted due to no borrowable resources")
 			wl := testing.MakeWorkload("should-be-pending-wl", ns.Name).
 				Queue(lq2.Name).Request(corev1.ResourceCPU, "8").Obj()
-			gomega.ExpectWithOffset(1, k8sClient.Create(ctx, wl)).Should(gomega.Succeed())
+			gomega.Expect(k8sClient.Create(ctx, wl)).Should(gomega.Succeed())
 
 			ginkgo.By("Verifying metrics reflect the correct state")
 			util.ExpectPendingWorkloadsMetric(cq2, 0, 1)
