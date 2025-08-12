628e5c8df1044b3166a402fc3f9f3beefb057d9b
Michal Szadkowski <michal_szadkowski@epam.com>
2025-08-12 12:57:56 +0200

    Verify object got deleted after reconnection

--- test/e2e/multikueue/e2e_test.go
+++ test/e2e/multikueue/e2e_test.go
@@ -985,19 +985,6 @@ var _ = ginkgo.Describe("MultiKueue", func() {
 				cmd := exec.Command("docker", "network", "connect", "kind", worker1Container)
 				output, err := cmd.CombinedOutput()
 				gomega.Expect(err).NotTo(gomega.HaveOccurred(), "%s: %s", err, output)
-				gomega.Eventually(func(g gomega.Gomega) {
-					g.Expect(util.DeleteObject(ctx, k8sWorker1Client, worker1Cq2)).Should(gomega.Succeed())
-				}, util.LongTimeout, util.Interval).Should(gomega.Succeed())
-
-				// After reconnecting the container to the network, when we try to get pods,
-				// we get it with the previous values (as before disconnect). Therefore, it
-				// takes some time for the cluster to restore them, and we got actually values.
-				// To be sure that the leader of kueue-control-manager successfully recovered
-				// we can check it by removing already created Cluster Queue.
-				var cq kueue.ClusterQueue
-				gomega.Eventually(func(g gomega.Gomega) {
-					g.Expect(k8sWorker1Client.Get(ctx, client.ObjectKeyFromObject(worker1Cq2), &cq)).Should(utiltesting.BeNotFoundError())
-				}, util.LongTimeout, util.Interval).Should(gomega.Succeed())
 			})
 
 			ginkgo.By("Waiting for the cluster to become active", func() {
@@ -1018,6 +1005,10 @@ var _ = ginkgo.Describe("MultiKueue", func() {
 			ginkgo.By("Waiting for Kueue and kube-system pods to become active again", func() {
 				util.WaitForKubeSystemControllersAvailability(ctx, k8sWorker1Client, worker1Container)
 				util.WaitForKueueAvailabilityNoRestartCountCheck(ctx, k8sWorker1Client)
+			})
+
+			ginkgo.By("Checking that the Kueue is operational after reconnection", func() {
+				util.ExpectObjectToBeDeletedWithTimeout(ctx, k8sWorker1Client, worker1Cq2, true, util.StartUpTimeout)
 			})
 		})
 	})
