cc796760a92357d92f7e6e1cd96189d9c0b7848b
Patryk Bundyra <pbundyra@google.com>
2025-08-20 12:40:20 +0000

    Rename s/ExpectLocalQueueUsageToBe/ExpectLocalQueueFairSharingUsageToBe

--- test/util/util.go
+++ test/util/util.go
@@ -1110,7 +1110,7 @@ func SetNodeCondition(ctx context.Context, k8sClient client.Client, node *corev1
 	}, Timeout, Interval).Should(gomega.Succeed(), "Failed to set node condition %s to %s for node %s", newCondition.Type, newCondition.Status, node.Name)
 }
 
-func ExpectLocalQueueUsageToBe(ctx context.Context, k8sClient client.Client, lqKey client.ObjectKey, comparator string, compareTo any) {
+func ExpectLocalQueueFairSharingUsageToBe(ctx context.Context, k8sClient client.Client, lqKey client.ObjectKey, comparator string, compareTo any) {
 	lq := &kueue.LocalQueue{}
 	gomega.Eventually(func(g gomega.Gomega) {
 		g.Expect(k8sClient.Get(ctx, lqKey, lq)).Should(gomega.Succeed())
