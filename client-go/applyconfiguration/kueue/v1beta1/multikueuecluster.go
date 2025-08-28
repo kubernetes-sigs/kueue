b77245620262d95fec1abd893adedbd72e9ac9fe
Mykhailo Bobrovskyi <mikhail.bobrovsky@gmail.com>
2025-08-28 20:53:54 +0530

    Bump Kubernetes dependencies to v0.34.x

--- client-go/applyconfiguration/kueue/v1beta1/multikueuecluster.go
+++ client-go/applyconfiguration/kueue/v1beta1/multikueuecluster.go
@@ -41,6 +41,7 @@ func MultiKueueCluster(name string) *MultiKueueClusterApplyConfiguration {
 	b.WithAPIVersion("kueue.x-k8s.io/v1beta1")
 	return b
 }
+func (b MultiKueueClusterApplyConfiguration) IsApplyConfiguration() {}
 
 // WithKind sets the Kind field in the declarative configuration to the given value
 // and returns the receiver, so that objects can be built by chaining "With" function invocations.
@@ -216,8 +217,24 @@ func (b *MultiKueueClusterApplyConfiguration) WithStatus(value *MultiKueueCluste
 	return b
 }
 
+// GetKind retrieves the value of the Kind field in the declarative configuration.
+func (b *MultiKueueClusterApplyConfiguration) GetKind() *string {
+	return b.TypeMetaApplyConfiguration.Kind
+}
+
+// GetAPIVersion retrieves the value of the APIVersion field in the declarative configuration.
+func (b *MultiKueueClusterApplyConfiguration) GetAPIVersion() *string {
+	return b.TypeMetaApplyConfiguration.APIVersion
+}
+
 // GetName retrieves the value of the Name field in the declarative configuration.
 func (b *MultiKueueClusterApplyConfiguration) GetName() *string {
 	b.ensureObjectMetaApplyConfigurationExists()
 	return b.ObjectMetaApplyConfiguration.Name
+}
+
+// GetNamespace retrieves the value of the Namespace field in the declarative configuration.
+func (b *MultiKueueClusterApplyConfiguration) GetNamespace() *string {
+	b.ensureObjectMetaApplyConfigurationExists()
+	return b.ObjectMetaApplyConfiguration.Namespace
 }
