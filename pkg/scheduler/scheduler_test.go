1c45a6221b3f75ef66f320f2f0759efac4e9e3c4
Patryk Bundyra <pbundyra@google.com>
2025-09-19 13:35:45 +0000

    rm fg

--- pkg/scheduler/scheduler_test.go
+++ pkg/scheduler/scheduler_test.go
@@ -7068,7 +7068,6 @@ func TestScheduleForTAS(t *testing.T) {
 				utiltesting.MakeEventRecord("default", "foo", "EvictedDueToNodeFailures", corev1.EventTypeNormal).
 					Message("Workload was evicted as there was no replacement for a failed node: x0").Obj(),
 			},
-			featureGates: []featuregate.Feature{features.TASFailedNodeReplacementFailFast},
 		},
 	}
 	for name, tc := range cases {
