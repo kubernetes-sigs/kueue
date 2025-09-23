4e1363d9777014bd85733b830d211a0c3ebe8d08
Michal Szadkowski <michal_szadkowski@epam.com>
2025-09-23 11:40:04 +0200

    Use PatchStatus to set WorkloadFinished in MultiKueue

--- pkg/controller/admissionchecks/multikueue/workload.go
+++ pkg/controller/admissionchecks/multikueue/workload.go
@@ -47,6 +47,7 @@ import (
 	"sigs.k8s.io/kueue/pkg/controller/jobframework"
 	"sigs.k8s.io/kueue/pkg/util/admissioncheck"
 	"sigs.k8s.io/kueue/pkg/util/api"
+	clientutil "sigs.k8s.io/kueue/pkg/util/client"
 	utilmaps "sigs.k8s.io/kueue/pkg/util/maps"
 	"sigs.k8s.io/kueue/pkg/workload"
 )
@@ -340,14 +341,16 @@ func (w *wlReconciler) reconcileGroup(ctx context.Context, group *wlGroup) (reco
 		}
 
 		// copy the status to the local one
-		wlPatch := workload.BaseSSAWorkload(group.local, false)
-		apimeta.SetStatusCondition(&wlPatch.Status.Conditions, metav1.Condition{
-			Type:    kueue.WorkloadFinished,
-			Status:  metav1.ConditionTrue,
-			Reason:  remoteFinishedCond.Reason,
-			Message: remoteFinishedCond.Message,
+		return reconcile.Result{}, clientutil.PatchStatus(ctx, w.client, group.local, func() (client.Object, bool, error) {
+			apimeta.SetStatusCondition(&group.local.Status.Conditions, metav1.Condition{
+				Type:               kueue.WorkloadFinished,
+				Status:             metav1.ConditionTrue,
+				Reason:             remoteFinishedCond.Reason,
+				Message:            remoteFinishedCond.Message,
+				LastTransitionTime: metav1.NewTime(w.clock.Now()),
+			})
+			return group.local, true, nil
 		})
-		return reconcile.Result{}, w.client.Status().Patch(ctx, wlPatch, client.Apply, client.FieldOwner(kueue.MultiKueueControllerName+"-finish"), client.ForceOwnership)
 	}
 
 	// 2. delete all workloads that are out of sync or are not in the chosen worker
