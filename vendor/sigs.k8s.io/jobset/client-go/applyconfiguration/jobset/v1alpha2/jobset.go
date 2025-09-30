b122eb3c469ee425c00db66f9327d876e62b311b
Mykhailo Bobrovskyi <mikhail.bobrovsky@gmail.com>
2025-09-30 15:37:09 +0530

    Bump sigs.k8s.io/jobset from 0.9.0 to 0.10.1.

--- vendor/sigs.k8s.io/jobset/client-go/applyconfiguration/jobset/v1alpha2/jobset.go
+++ vendor/sigs.k8s.io/jobset/client-go/applyconfiguration/jobset/v1alpha2/jobset.go
@@ -39,6 +39,7 @@ func JobSet(name, namespace string) *JobSetApplyConfiguration {
 	b.WithAPIVersion("jobset.x-k8s.io/v1alpha2")
 	return b
 }
+func (b JobSetApplyConfiguration) IsApplyConfiguration() {}
 
 // WithKind sets the Kind field in the declarative configuration to the given value
 // and returns the receiver, so that objects can be built by chaining "With" function invocations.
@@ -214,8 +215,24 @@ func (b *JobSetApplyConfiguration) WithStatus(value *JobSetStatusApplyConfigurat
 	return b
 }
 
+// GetKind retrieves the value of the Kind field in the declarative configuration.
+func (b *JobSetApplyConfiguration) GetKind() *string {
+	return b.TypeMetaApplyConfiguration.Kind
+}
+
+// GetAPIVersion retrieves the value of the APIVersion field in the declarative configuration.
+func (b *JobSetApplyConfiguration) GetAPIVersion() *string {
+	return b.TypeMetaApplyConfiguration.APIVersion
+}
+
 // GetName retrieves the value of the Name field in the declarative configuration.
 func (b *JobSetApplyConfiguration) GetName() *string {
 	b.ensureObjectMetaApplyConfigurationExists()
 	return b.ObjectMetaApplyConfiguration.Name
+}
+
+// GetNamespace retrieves the value of the Namespace field in the declarative configuration.
+func (b *JobSetApplyConfiguration) GetNamespace() *string {
+	b.ensureObjectMetaApplyConfigurationExists()
+	return b.ObjectMetaApplyConfiguration.Namespace
 }
