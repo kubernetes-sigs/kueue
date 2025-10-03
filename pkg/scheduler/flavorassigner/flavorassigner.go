6af8e24eb95c5737d0732f16a916dad0b057a9c1
Olek Zabłocki <olekz@google.com>
2025-10-03 20:42:48 +0000

    Again, describe `true` instead of `false`.

--- pkg/scheduler/flavorassigner/flavorassigner.go
+++ pkg/scheduler/flavorassigner/flavorassigner.go
@@ -845,15 +845,11 @@ func shouldTryNextFlavor(representativeMode granularMode, flavorFungibility kueu
 		return true
 	}
 
-	if representativeMode.isPreemptMode() && policyPreempt == kueue.Preempt {
-		return false
+	if representativeMode.isPreemptMode() && policyPreempt == kueue.TryNextFlavor {
+		return true
 	}
 
-	if !representativeMode.isPreemptMode() {
-		return false
-	}
-
-	return true
+	return false
 }
 
 func flavorSelector(spec *corev1.PodSpec, allowedKeys sets.Set[string]) nodeaffinity.RequiredNodeAffinity {
