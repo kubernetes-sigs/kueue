ff9ddd05829a0536480851a88fa954640d500d56
Olek Zabłocki <olekz@google.com>
2025-10-03 20:27:28 +0000

    Factor `preemptionMode == fit` out of 2 `&&` clauses

--- pkg/scheduler/flavorassigner/flavorassigner.go
+++ pkg/scheduler/flavorassigner/flavorassigner.go
@@ -828,12 +828,14 @@ func shouldTryNextFlavor(representativeMode granularMode, flavorFungibility kueu
 		}
 	}
 
-	if representativeMode.preemptionMode == fit && !representativeMode.borrowingLevel.optimal() && policyBorrow == kueue.Borrow {
-		return false
-	}
+	if representativeMode.preemptionMode == fit {
+		if !representativeMode.borrowingLevel.optimal() && policyBorrow == kueue.Borrow {
+			return false
+		}
 
-	if representativeMode.preemptionMode == fit && representativeMode.borrowingLevel.optimal() {
-		return false
+		if representativeMode.borrowingLevel.optimal() {
+			return false
+		}
 	}
 
 	return true
