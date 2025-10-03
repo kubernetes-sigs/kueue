b545199cf537a71c64474f5e47f2fe4f2412a4ac
Olek Zabłocki <olekz@google.com>
2025-10-03 20:34:54 +0000

    Remove an always-true `if`.
    (If the condition is false, we'll have already returned in line 841).

--- pkg/scheduler/flavorassigner/flavorassigner.go
+++ pkg/scheduler/flavorassigner/flavorassigner.go
@@ -842,9 +842,7 @@ func shouldTryNextFlavor(representativeMode granularMode, flavorFungibility kueu
 	}
 
 	if representativeMode.isPreemptMode() && policyPreempt == kueue.Preempt {
-		if representativeMode.borrowingLevel.optimal() || policyBorrow == kueue.Borrow {
-			return false
-		}
+		return false
 	}
 
 	if representativeMode.preemptionMode == fit {
