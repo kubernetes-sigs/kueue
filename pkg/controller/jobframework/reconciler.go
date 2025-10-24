7d9c13cbae78187bd37629ac966b190a02f0c250
Dominik Pająk <dominikpajak@google.com>
2025-10-24 13:15:34 +0200

    Automated cherry pick of #7376: Requeue workload after eviction due to failed hotswap (#7380)
    * hotswap reschedule evicted
    
    * Enable FailFast in e2e hotswap testing
    
    * Enable feature gate for e2e tests

--- pkg/controller/jobframework/reconciler.go
+++ pkg/controller/jobframework/reconciler.go
@@ -546,7 +546,7 @@ func (r *JobReconciler) ReconcileGenericJob(ctx context.Context, req ctrl.Reques
 			if !job.IsActive() {
 				log.V(6).Info("The job is no longer active, clear the workloads admission")
 				// The requeued condition status set to true only on EvictedByPreemption
-				setRequeued := evCond.Reason == kueue.WorkloadEvictedByPreemption
+				setRequeued := (evCond.Reason == kueue.WorkloadEvictedByPreemption) || (evCond.Reason == kueue.WorkloadEvictedDueToNodeFailures)
 				workload.SetRequeuedCondition(wl, evCond.Reason, evCond.Message, setRequeued)
 				_ = workload.UnsetQuotaReservationWithCondition(wl, "Pending", evCond.Message, r.clock.Now())
 				err := workload.ApplyAdmissionStatus(ctx, r.client, wl, true, r.clock)
