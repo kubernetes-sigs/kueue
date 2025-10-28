787d5380fd93dffbae7d414b4cf01014683540cb
Omar Sayed <omarsayed@google.com>
2025-10-28 08:23:44 +0000

    Enable TAS Mixed Profile for default

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -258,7 +258,7 @@ var defaultVersionedFeatureGates = map[featuregate.Feature]featuregate.Versioned
 	},
 	TASProfileMixed: {
 		{Version: version.MustParse("0.10"), Default: false, PreRelease: featuregate.Alpha},
-		{Version: version.MustParse("0.11"), Default: false, PreRelease: featuregate.Deprecated},
+		{Version: version.MustParse("0.11"), Default: true, PreRelease: featuregate.Deprecated},
 	},
 	HierarchicalCohorts: {
 		{Version: version.MustParse("0.11"), Default: true, PreRelease: featuregate.Beta},
