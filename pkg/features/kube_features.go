a492eacf44a2aeff02b4458875bc40e4ce552440
Michal Szadkowski <michal_szadkowski@epam.com>
2025-10-29 14:33:33 +0100

    Add skip validation flag

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -196,6 +196,13 @@ const (
 	// are resolved by keeping only the last occurrence, allowing workload creation to succeed
 	// even when duplicates are present in the spec.
 	SanitizePodSets featuregate.Feature = "SanitizePodSets"
+
+	// owner: @mszadkow
+	//
+	// Allow insecure kubeconfigs in MultiKueue setup.
+	// Requires careful consideration as it may lead to security issues.
+	// To be removed in Kueue v0.17.
+	MultiKueueAllowInsecureKubeconfigs featuregate.Feature = "MultiKueueAllowInsecureKubeconfigs"
 )
 
 func init() {
@@ -300,6 +307,10 @@ var defaultVersionedFeatureGates = map[featuregate.Feature]featuregate.Versioned
 	},
 	SanitizePodSets: {
 		{Version: version.MustParse("0.13"), Default: true, PreRelease: featuregate.Beta},
+	},
+	MultiKueueAllowInsecureKubeconfigs: {
+		{Version: version.MustParse("0.13"), Default: false, PreRelease: featuregate.Alpha},
+		{Version: version.MustParse("0.17"), Default: false, PreRelease: featuregate.Deprecated}, // remove in 0.17
 	},
 }
 
