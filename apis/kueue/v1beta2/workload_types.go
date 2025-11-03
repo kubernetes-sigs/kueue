ef12e7260f5e079ca0aa162856f2838dae1f0daa
Kevin Hannon <kehannon@redhat.com>
2025-11-03 09:18:11 -0500

    Enable nomaps and nobools kube api linter (#7489)
    * enable no maps and no bools but disable the failures.
    
    * Update apis/kueue/v1beta2/workload_types.go
    
    Co-authored-by: Michał Woźniak <mimowo@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Michał Woźniak <mimowo@users.noreply.github.com>

--- apis/kueue/v1beta2/workload_types.go
+++ apis/kueue/v1beta2/workload_types.go
@@ -75,7 +75,7 @@ type WorkloadSpec struct {
 	//
 	// Defaults to true
 	// +kubebuilder:default=true
-	Active *bool `json:"active,omitempty"`
+	Active *bool `json:"active,omitempty"` //nolint:kubeapilinter // bool for the ease of use, inspired by k8s Job's Suspend field.
 
 	// maximumExecutionTimeSeconds if provided, determines the maximum time, in seconds,
 	// the workload can be admitted before it's automatically deactivated.
@@ -113,7 +113,7 @@ type PodSetTopologyRequest struct {
 	//
 	// +optional
 	// +kubebuilder:validation:Type=boolean
-	Unconstrained *bool `json:"unconstrained,omitempty"`
+	Unconstrained *bool `json:"unconstrained,omitempty"` //nolint:kubeapilinter // disabling to avoid changing existing API
 
 	// podIndexLabel indicates the name of the label indexing the pods.
 	// For example, in the context of
@@ -190,7 +190,7 @@ type PodSetAssignment struct {
 	// Beside what is provided in podSet's specs, this calculation takes into account
 	// the LimitRange defaults and RuntimeClass overheads at the moment of admission.
 	// This field will not change in case of quota reclaim.
-	ResourceUsage corev1.ResourceList `json:"resourceUsage,omitempty"`
+	ResourceUsage corev1.ResourceList `json:"resourceUsage,omitempty"` //nolint:kubeapilinter // map type is required for standard Kubernetes ResourceList
 
 	// count is the number of pods taken into account at admission time.
 	// This field will not change in case of quota reclaim.
@@ -627,7 +627,7 @@ type PodSetRequest struct {
 	// the LimitRange defaults and RuntimeClass overheads at the moment of consideration
 	// and the application of resource.excludeResourcePrefixes and resource.transformations.
 	// +optional
-	Resources corev1.ResourceList `json:"resources,omitempty"`
+	Resources corev1.ResourceList `json:"resources,omitempty"` //nolint:kubeapilinter // map type is required for standard Kubernetes ResourceList
 }
 
 const (
