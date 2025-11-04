7a8b75bc06450cc4e319280eb2091f95e84f2de5
Omar Sayed <omarsayed@google.com>
2025-11-04 10:22:15 +0100

    Switch Default TAS Placement Algorithm from BestFit to Mixed. (#7416)
    * Enable TAS Mixed Profile for default
    
    * adjust feature and kep
    
    * remove deprecation of current version for TASProfileMixed
    
    * cleanups
    
    * fix typo

--- pkg/cache/scheduler/tas_cache_test.go
+++ pkg/cache/scheduler/tas_cache_test.go
@@ -658,18 +658,11 @@ func TestFindTopologyAssignments(t *testing.T) {
 				wantAssignment: &kueue.TopologyAssignment{
 					Levels: defaultOneLevel,
 					Domains: []kueue.TopologyDomainAssignment{
-						{
-							Count: 4,
-							Values: []string{
-								"x3",
-							},
-						},
-						{
-							Count: 2,
-							Values: []string{
-								"x6",
-							},
-						},
+						{Count: 1, Values: []string{"x1"}},
+						{Count: 1, Values: []string{"x3"}},
+						{Count: 1, Values: []string{"x5"}},
+						{Count: 1, Values: []string{"x2"}},
+						{Count: 2, Values: []string{"x6"}},
 					},
 				},
 			}},
@@ -688,18 +681,11 @@ func TestFindTopologyAssignments(t *testing.T) {
 				wantAssignment: &kueue.TopologyAssignment{
 					Levels: defaultOneLevel,
 					Domains: []kueue.TopologyDomainAssignment{
-						{
-							Count: 4,
-							Values: []string{
-								"x3",
-							},
-						},
-						{
-							Count: 2,
-							Values: []string{
-								"x6",
-							},
-						},
+						{Count: 1, Values: []string{"x1"}},
+						{Count: 1, Values: []string{"x3"}},
+						{Count: 1, Values: []string{"x5"}},
+						{Count: 1, Values: []string{"x2"}},
+						{Count: 2, Values: []string{"x6"}},
 					},
 				},
 			}},
