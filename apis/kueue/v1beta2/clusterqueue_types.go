83735c63531df9af5a5a65666f207f6b89502f76
Jakub Skiba <jakubskiba@google.com>
2025-11-12 14:18:18 +0000

    cleanup API definition and generated output

--- apis/kueue/v1beta2/clusterqueue_types.go
+++ apis/kueue/v1beta2/clusterqueue_types.go
@@ -515,7 +515,6 @@ type BorrowWithinCohort struct {
 
 // +genclient
 // +genclient:nonNamespaced
-// +kubebuilder:storageversion
 // +kubebuilder:object:root=true
 // +kubebuilder:resource:scope=Cluster,shortName={cq}
 // +kubebuilder:subresource:status
