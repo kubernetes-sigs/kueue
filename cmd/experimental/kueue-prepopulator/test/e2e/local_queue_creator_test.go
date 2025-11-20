bcc1d0ee57af8064ebb14e1e9fe6930a4a80409f
Jakub Skiba <jakubskiba@google.com>
2025-11-20 13:56:01 +0100

    Create Default LocalQueues Based on NamespaceSelector (#7655)
    * add autoLocalQueue settings to ClusterQueue API
    
    * handle creation of lq
    
    * add cq webhook validation
    
    * add integration tests for the new feature
    
    * rename feature gate - DefaultLocalQueue -> AutoLocalQueue
    
    * auto-generated structs
    
    * add integration tests - wip
    
    * cleanup API definition and generated output
    
    * improve log messages
    
    * optimize cq webhook validtion for lqs
    
    * cleanup
    
    * rename autoLocalQueue -> defaultLocalQueue
    
    * rename autoLocalQueue -> defaultLocalQueue
    
    * generate warning when duplicate lq exists
    
    * cleanup
    
    * remove warning emit integration test case - hard to reproduce
    
    * fix format
    
    * fix gomega issues in tests
    
    * fix format
    
    * make Name field required
    
    * update helm
    
    * rename fg DefaultLocalQueue -> DefaultLocalQueueCreation
    
    * create separate defaultlocalqueue controller
    
    * add tests for the new controller
    
    * remove webhook validation for defaultlocalqueue
    
    * respect global namespaceselector
    
    * cover more test cases
    
    * fix linter
    
    * make controller reconcile on namespace
    
    * add new localqueue-creator binary\
    * add test for that binary\
    * add build procedures
    
    * fix e2e tests
    
    * cleanup
    
    * fix linter issues
    
    * cleanup
    
    * cleanup
    
    * fix code generation and tests
    
    * remove image tag
    
    * fix OWNERS file
    
    * fix owners file
    
    * handle global namespaceSelector
    
    * add handling of global namespaceselector
    
    * fix linter issue
    
    * move Reconcile function
    
    * leave OWNERS file empty
    
    * remove annotation from auto-generated localqueues
    
    * exit early when namespaceselector is invalid
    
    * add testing section to readme
    
    * use time constants in e2e tests
    
    * rename deployment
    
    * extract start function
    
    * rename deployment
    
    * update name of the image
    
    * increase memory requests
    
    * rename localqueue-creator to kueue-prepopulator
    
    * replace again
    
    * cleanup kustomization
    
    * set newTag to main

---
+++ cmd/experimental/kueue-prepopulator/test/e2e/local_queue_creator_test.go
@@ -0,0 +1,191 @@
+/*
+Copyright The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package e2e
+
+import (
+	"github.com/onsi/ginkgo/v2"
+	"github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+
+	kueue "sigs.k8s.io/kueue/apis/kueue/v1beta1"
+	"sigs.k8s.io/kueue/test/util"
+)
+
+var _ = ginkgo.Describe("DefaultLocalQueue", func() {
+	var (
+		ns *corev1.Namespace
+		cq *kueue.ClusterQueue
+	)
+
+	ginkgo.BeforeEach(func() {
+		ns = &corev1.Namespace{
+			ObjectMeta: metav1.ObjectMeta{
+				GenerateName: "e2e-dlq-",
+			},
+		}
+		gomega.Expect(k8sClient.Create(ctx, ns)).To(gomega.Succeed())
+	})
+
+	ginkgo.AfterEach(func() {
+		gomega.Expect(k8sClient.Delete(ctx, ns)).To(gomega.Succeed())
+		if cq != nil {
+			gomega.Expect(k8sClient.Delete(ctx, cq)).To(gomega.Succeed())
+			cq = nil
+		}
+	})
+
+	ginkgo.When("DefaultLocalQueue controller is enabled", func() {
+		ginkgo.It("Should create a default LocalQueue when namespace matches ClusterQueue selector", func() {
+			cq = &kueue.ClusterQueue{
+				ObjectMeta: metav1.ObjectMeta{
+					GenerateName: "cq-dlq-",
+				},
+				Spec: kueue.ClusterQueueSpec{
+					NamespaceSelector: &metav1.LabelSelector{
+						MatchLabels: map[string]string{"foo": "bar"},
+					},
+				},
+			}
+			gomega.Expect(k8sClient.Create(ctx, cq)).To(gomega.Succeed())
+
+			ginkgo.By("updating the namespace to match the clusterqueue selector")
+			ns.Labels = map[string]string{"foo": "bar"}
+			gomega.Expect(k8sClient.Update(ctx, ns)).To(gomega.Succeed())
+
+			ginkgo.By("checking that the localqueue is created")
+			createdLQ := &kueue.LocalQueue{}
+			gomega.Eventually(func() error {
+				return k8sClient.Get(ctx, types.NamespacedName{Name: "default", Namespace: ns.Name}, createdLQ)
+			}, util.LongTimeout, util.Interval).Should(gomega.Succeed())
+
+			gomega.Expect(createdLQ.Spec.ClusterQueue).To(gomega.Equal(kueue.ClusterQueueReference(cq.Name)))
+		})
+
+		ginkgo.It("Should create a default LocalQueue when ClusterQueue is updated to match namespace", func() {
+			ns.Labels = map[string]string{"foo": "baz"}
+			gomega.Expect(k8sClient.Update(ctx, ns)).To(gomega.Succeed())
+
+			cq = &kueue.ClusterQueue{
+				ObjectMeta: metav1.ObjectMeta{
+					GenerateName: "cq-dlq-update-",
+				},
+				Spec: kueue.ClusterQueueSpec{
+					NamespaceSelector: &metav1.LabelSelector{
+						MatchLabels: map[string]string{"foo": "other"},
+					},
+				},
+			}
+			gomega.Expect(k8sClient.Create(ctx, cq)).To(gomega.Succeed())
+
+			ginkgo.By("verifying no localqueue is created initially")
+			createdLQ := &kueue.LocalQueue{}
+			gomega.Consistently(func() error {
+				return k8sClient.Get(ctx, types.NamespacedName{Name: "default", Namespace: ns.Name}, createdLQ)
+			}, util.Timeout, util.Interval).Should(gomega.Not(gomega.Succeed()))
+
+			ginkgo.By("updating the ClusterQueue to match the namespace")
+			gomega.Eventually(func() error {
+				var currentCQ kueue.ClusterQueue
+				gomega.Expect(k8sClient.Get(ctx, types.NamespacedName{Name: cq.Name}, &currentCQ)).To(gomega.Succeed())
+				currentCQ.Spec.NamespaceSelector = &metav1.LabelSelector{
+					MatchLabels: map[string]string{"foo": "baz"},
+				}
+				return k8sClient.Update(ctx, &currentCQ)
+			}, util.Timeout, util.Interval).Should(gomega.Succeed())
+
+			ginkgo.By("checking that the localqueue is created")
+			gomega.Eventually(func() error {
+				return k8sClient.Get(ctx, types.NamespacedName{Name: "default", Namespace: ns.Name}, createdLQ)
+			}, util.LongTimeout, util.Interval).Should(gomega.Succeed())
+
+			gomega.Expect(createdLQ.Spec.ClusterQueue).To(gomega.Equal(kueue.ClusterQueueReference(cq.Name)))
+		})
+
+		ginkgo.It("Should not overwrite existing LocalQueue with the same name", func() {
+			cq = &kueue.ClusterQueue{
+				ObjectMeta: metav1.ObjectMeta{
+					GenerateName: "cq-dlq-conflict-",
+				},
+				Spec: kueue.ClusterQueueSpec{
+					NamespaceSelector: &metav1.LabelSelector{
+						MatchLabels: map[string]string{"conflict": "true"},
+					},
+				},
+			}
+			gomega.Expect(k8sClient.Create(ctx, cq)).To(gomega.Succeed())
+
+			ginkgo.By("creating a conflicting LocalQueue manually")
+			existingLQ := &kueue.LocalQueue{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "default",
+					Namespace: ns.Name,
+				},
+				Spec: kueue.LocalQueueSpec{
+					ClusterQueue: "some-other-queue",
+				},
+			}
+			gomega.Expect(k8sClient.Create(ctx, existingLQ)).To(gomega.Succeed())
+
+			ns.Labels = map[string]string{"conflict": "true"}
+			gomega.Expect(k8sClient.Update(ctx, ns)).To(gomega.Succeed())
+
+			ginkgo.By("waiting to ensure controller doesn't modify it")
+			gomega.Consistently(func() string {
+				var lq kueue.LocalQueue
+				err := k8sClient.Get(ctx, types.NamespacedName{Name: "default", Namespace: ns.Name}, &lq)
+				if err != nil {
+					return ""
+				}
+				return string(lq.Spec.ClusterQueue)
+			}, util.Timeout, util.Interval).Should(gomega.Equal("some-other-queue"))
+		})
+
+		ginkgo.It("Should not delete LocalQueue when namespace no longer matches", func() {
+			ns.Labels = map[string]string{"persist": "true"}
+			gomega.Expect(k8sClient.Update(ctx, ns)).To(gomega.Succeed())
+
+			cq = &kueue.ClusterQueue{
+				ObjectMeta: metav1.ObjectMeta{
+					GenerateName: "cq-dlq-persist-",
+				},
+				Spec: kueue.ClusterQueueSpec{
+					NamespaceSelector: &metav1.LabelSelector{
+						MatchLabels: map[string]string{"persist": "true"},
+					},
+				},
+			}
+			gomega.Expect(k8sClient.Create(ctx, cq)).To(gomega.Succeed())
+
+			ginkgo.By("waiting for localqueue to be created")
+			createdLQ := &kueue.LocalQueue{}
+			gomega.Eventually(func() error {
+				return k8sClient.Get(ctx, types.NamespacedName{Name: "default", Namespace: ns.Name}, createdLQ)
+			}, util.LongTimeout, util.Interval).Should(gomega.Succeed())
+
+			ginkgo.By("updating namespace to no longer match")
+			ns.Labels = map[string]string{"persist": "false"}
+			gomega.Expect(k8sClient.Update(ctx, ns)).To(gomega.Succeed())
+
+			ginkgo.By("ensuring LocalQueue persists")
+			gomega.Consistently(func() error {
+				return k8sClient.Get(ctx, types.NamespacedName{Name: "default", Namespace: ns.Name}, &kueue.LocalQueue{})
+			}, util.Timeout, util.Interval).Should(gomega.Succeed())
+		})
+	})
+})
---
+++ cmd/experimental/kueue-prepopulator/test/integration/suite_test.go
@@ -0,0 +1,108 @@
+/*
+Copyright The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package integration
+
+import (
+	"context"
+	"testing"
+
+	"github.com/onsi/ginkgo/v2"
+	"github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/rest"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+
+	config "sigs.k8s.io/kueue/apis/config/v1beta1"
+	"sigs.k8s.io/kueue/cmd/experimental/kueue-prepopulator/pkg/controller"
+	qcache "sigs.k8s.io/kueue/pkg/cache/queue"
+	schdcache "sigs.k8s.io/kueue/pkg/cache/scheduler"
+	"sigs.k8s.io/kueue/pkg/controller/core"
+	"sigs.k8s.io/kueue/pkg/controller/core/indexer"
+	"sigs.k8s.io/kueue/pkg/webhooks"
+	"sigs.k8s.io/kueue/test/integration/framework"
+)
+
+var (
+	cfg       *rest.Config
+	k8sClient client.Client
+	ctx       context.Context
+	fwk       *framework.Framework
+)
+
+func TestAPIs(t *testing.T) {
+	gomega.RegisterFailHandler(ginkgo.Fail)
+
+	ginkgo.RunSpecs(t,
+		"kueue-prepopulator Controllers Suite",
+	)
+}
+
+var _ = ginkgo.BeforeSuite(func() {
+	fwk = &framework.Framework{}
+	cfg = fwk.Init()
+	ctx, k8sClient = fwk.SetupClient(cfg) //nolint:fatcontext // Global context is required for test suite setup
+})
+
+var _ = ginkgo.AfterSuite(func() {
+	fwk.Teardown()
+})
+
+func managerSetup(ctx context.Context, mgr manager.Manager) {
+	managerAndControllerSetup(nil)(ctx, mgr)
+}
+
+func managerAndControllerSetup(controllersCfg *config.Configuration) framework.ManagerSetup {
+	return func(ctx context.Context, mgr manager.Manager) {
+		err := indexer.Setup(ctx, mgr.GetFieldIndexer())
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+
+		failedWebhook, err := webhooks.Setup(mgr)
+		gomega.Expect(err).ToNot(gomega.HaveOccurred(), "webhook", failedWebhook)
+
+		if controllersCfg == nil {
+			controllersCfg = &config.Configuration{}
+		}
+
+		mgr.GetScheme().Default(controllersCfg)
+
+		controllersCfg.Metrics.EnableClusterQueueResources = true
+
+		cCache := schdcache.New(mgr.GetClient())
+		queues := qcache.NewManager(mgr.GetClient(), cCache)
+
+		failedCtrl, err := core.SetupControllers(mgr, queues, cCache, controllersCfg)
+		gomega.Expect(err).ToNot(gomega.HaveOccurred(), "controller", failedCtrl)
+
+		opts := []controller.KueuePrepopulatorReconcilerOption{
+			controller.WithLocalQueueName("default-lq"),
+		}
+		if controllersCfg.ManagedJobsNamespaceSelector != nil {
+			selector, err := metav1.LabelSelectorAsSelector(controllersCfg.ManagedJobsNamespaceSelector)
+			gomega.Expect(err).ToNot(gomega.HaveOccurred())
+			opts = append(opts, controller.WithNamespaceSelector(selector))
+		}
+
+		reconciler := controller.NewKueuePrepopulatorReconciler(
+			mgr.GetClient(),
+			mgr.GetEventRecorderFor(controller.ControllerName),
+			opts...,
+		)
+		err = reconciler.SetupWithManager(mgr)
+		gomega.Expect(err).ToNot(gomega.HaveOccurred())
+	}
+}
