2c5e833786e56286b184fe753d3a642be6460c5b
Olek Zabłocki <olekz@google.com>
2025-11-26 13:50:22 +0000

    Prevent admitting inactive workloads

--- pkg/cache/queue/manager.go
+++ pkg/cache/queue/manager.go
@@ -450,6 +450,12 @@ func (m *Manager) AddOrUpdateWorkload(w *kueue.Workload, opts ...workload.InfoOp
 }
 
 func (m *Manager) AddOrUpdateWorkloadWithoutLock(w *kueue.Workload, opts ...workload.InfoOption) error {
+	if !workload.IsActive(w) {
+		return fmt.Errorf("Workload %q is inactive and can't be added to a LocalQueue", w.ObjectMeta.Name)
+	}
+	if workload.HasQuotaReservation(w) {
+		return fmt.Errorf("Workload %q already has quota reserved and can't be added to a LocalQueue", w.ObjectMeta.Name)
+	}
 	qKey := queue.KeyFromWorkload(w)
 	q := m.localQueues[qKey]
 	if q == nil {
