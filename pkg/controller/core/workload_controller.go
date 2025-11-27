adb64933871e4499dd36db6bf727c020e02d5e3c
Olek Zabłocki <olekz@google.com>
2025-11-27 10:04:39 +0000

    Remove an empty line

--- pkg/controller/core/workload_controller.go
+++ pkg/controller/core/workload_controller.go
@@ -968,7 +968,6 @@ func (h *resourceUpdatesHandler) queueReconcileForPending(ctx context.Context, _
 		log.V(5).Info("Queue reconcile for")
 		workload.AdjustResources(ctrl.LoggerInto(ctx, log), h.r.client, wlCopy)
 		if workload.IsActive(wlCopy) && !workload.HasQuotaReservation(wlCopy) {
-
 			if err = h.r.queues.AddOrUpdateWorkload(wlCopy); err != nil {
 				log.V(2).Info("ignored an error for now", "error", err)
 			}
