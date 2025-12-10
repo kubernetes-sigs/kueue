d08a6e7778a33961240a5535293ee3b08033334a
Bo (AIML) Yang <bo_yang6@apple.com>
2025-12-10 07:56:01 -0800

    Set idleTimeoutSeconds to 5 in EnableInTreeAutoscaling

--- pkg/util/testingjobs/rayjob/wrappers.go
+++ pkg/util/testingjobs/rayjob/wrappers.go
@@ -306,7 +306,7 @@ func (j *JobWrapper) Annotation(key string, value string) *JobWrapper {
 func (j *JobWrapper) EnableInTreeAutoscaling() *JobWrapper {
 	enable := true
 	aggressive := rayv1.UpscalingMode("Aggressive")
-	idleTimeoutSeconds := int32(30)
+	idleTimeoutSeconds := int32(5)
 	j.Spec.RayClusterSpec.EnableInTreeAutoscaling = &enable
 	j.Spec.RayClusterSpec.AutoscalerOptions = &rayv1.AutoscalerOptions{
 		UpscalingMode:      &aggressive,
