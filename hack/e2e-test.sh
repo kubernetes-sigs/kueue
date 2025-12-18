b6859e34f86a4775834a72735093d5b03f3c6f3a
Bo (AIML) Yang <bo_yang6@apple.com>
2025-12-18 13:07:21 -0800

    Revert "[TEST] Add --focus "Should run a rayjob if admitted""
    This reverts commit ea0d63343dd3956a7179d89ed6325db2a6bdc9e9.

--- hack/e2e-test.sh
+++ hack/e2e-test.sh
@@ -62,5 +62,5 @@ if [ "$E2E_RUN_ONLY_ENV" = "true" ]; then
 fi
 
 # shellcheck disable=SC2086
-$GINKGO $GINKGO_ARGS --focus "Should run a rayjob if admitted" --junit-report=junit.xml --json-report=e2e.json --output-dir="$ARTIFACTS" -v ./test/e2e/$E2E_TARGET_FOLDER/...
+$GINKGO $GINKGO_ARGS --junit-report=junit.xml --json-report=e2e.json --output-dir="$ARTIFACTS" -v ./test/e2e/$E2E_TARGET_FOLDER/...
 "$ROOT_DIR/bin/ginkgo-top" -i "$ARTIFACTS/e2e.json" > "$ARTIFACTS/e2e-top.yaml"
