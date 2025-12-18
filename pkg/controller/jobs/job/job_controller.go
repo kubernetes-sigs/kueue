f6247b8034c47e8091a27bbd060e6c12488e92e6
Bo (AIML) Yang <bo_yang6@apple.com>
2025-12-18 10:21:05 -0800

    Make RayCluster always top level for RayJob

--- pkg/controller/jobs/job/job_controller.go
+++ pkg/controller/jobs/job/job_controller.go
@@ -44,7 +44,6 @@ import (
 	"sigs.k8s.io/kueue/pkg/features"
 	"sigs.k8s.io/kueue/pkg/podset"
 	clientutil "sigs.k8s.io/kueue/pkg/util/client"
-	"sigs.k8s.io/kueue/pkg/workloadslicing"
 )
 
 var (
@@ -222,11 +221,9 @@ func (j *Job) IsTopLevel() bool {
 		return false
 	}
 
-	// This is job created by RayJob, check it with following logic:
-	// if it is not elastic job, meaning RayJob not enabled autoscaling, treat it as managed by RayJob, return false.
-	// if it is elastic job, meaning RayJob enabled autoscaling, treat it as top level (not managed by RayJob), return true.
-	// See discussion in https://github.com/kubernetes-sigs/kueue/pull/8082
-	return j.Annotations[workloadslicing.EnabledAnnotationKey] == workloadslicing.EnabledAnnotationValue
+	// This is job created by RayJob, treat it as top level, since Kueue will not manage RayJob
+	// see https://github.com/kubernetes-sigs/kueue/issues/8201
+	return true
 }
 
 func (j *Job) PodLabelSelector() string {
