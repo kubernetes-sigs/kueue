a7f408688dae1adcbd340083453f2093aa839249
skools-here <sajalkulshreshtha9@gmail.com>
2025-12-30 15:04:40 +0530

    Fix second-pass queue cleanup when workload no longer needs retry

--- pkg/cache/queue/manager.go
+++ pkg/cache/queue/manager.go
@@ -815,16 +815,20 @@ func (m *Manager) DeleteSecondPassWithoutLock(wl *kueue.Workload) {
 // QueueSecondPassIfNeeded queues for the second pass of scheduling with exponential
 // delay.
 func (m *Manager) QueueSecondPassIfNeeded(ctx context.Context, w *kueue.Workload, iteration int) bool {
+	log := ctrl.LoggerFrom(ctx)
 	if workload.NeedsSecondPass(w) {
 		iteration++
 		delay := m.secondPassQueue.nextDelay(iteration)
-		log := ctrl.LoggerFrom(ctx)
 		log.V(3).Info("Workload pre-queued for second pass (with backoff)", "workload", workload.Key(w), "delay", delay)
 		m.secondPassQueue.prequeue(w)
 		m.clock.AfterFunc(delay, func() {
 			m.queueSecondPass(ctx, w, iteration)
 		})
 		return true
+	}
+	if !workload.NeedsSecondPass(w) && iteration > 0{
+	log.V(3).Info("Workload removed from second pass queue","workload", workload.Key(w))
+	m.secondPassQueue.deleteByKey(workload.Key(w))	
 	}
 	return false
 }
