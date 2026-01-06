6267c9886f7faadeac488776398bfc88c91fe697
Bo (AIML) Yang <bo_yang6@apple.com>
2026-01-05 17:47:59 -0800

    Fix lint issue

--- test/integration/singlecluster/webhook/jobs/raycluster_webhook_test.go
+++ test/integration/singlecluster/webhook/jobs/raycluster_webhook_test.go
@@ -21,6 +21,7 @@ import (
 	"github.com/onsi/gomega"
 	corev1 "k8s.io/api/core/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
+
 	"sigs.k8s.io/kueue/pkg/constants"
 	"sigs.k8s.io/kueue/pkg/controller/core/indexer"
 	"sigs.k8s.io/kueue/pkg/controller/jobframework"
