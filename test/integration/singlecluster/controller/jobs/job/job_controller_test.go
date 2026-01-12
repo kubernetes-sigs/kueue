254e981356f35ab29b67489e0b0851b8bcc59b09
AndrewSeif <andrew.seif@outlook.com>
2026-01-12 11:18:21 +0200

    add new util func to use wl key, and update job int test

--- test/integration/singlecluster/controller/jobs/job/job_controller_test.go
+++ test/integration/singlecluster/controller/jobs/job/job_controller_test.go
@@ -1601,7 +1601,7 @@ var _ = ginkgo.Describe("Interacting with scheduler", ginkgo.Ordered, ginkgo.Con
 			util.ExpectWorkloadsToBePreempted(ctx, k8sClient, lowWl)
 
 			ginkgo.By("Verifying the high-priority workload gets admitted")
-			util.ExpectWorkloadsToHaveQuotaReservationByKey(ctx, k8sClient, devClusterQ.Name, noPriorityWlKey)
+			util.ExpectWorkloadsToBePreemptedByKey(ctx, k8sClient, lowWlKey)
 		})
 	})
 
