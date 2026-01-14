2cf0dda287f3ffa845457dec251520208f754a73
Mykhailo Bobrovskyi <mikhail.bobrovsky@gmail.com>
2026-01-14 13:19:35 +0530

    Fix comments for SkipFinalizersForPodsSuspendedByParent feature. (#8575)

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -341,7 +341,7 @@ var defaultVersionedFeatureGates = map[featuregate.Feature]featuregate.Versioned
 		{Version: version.MustParse("0.15"), Default: false, PreRelease: featuregate.Alpha},
 	},
 	SkipFinalizersForPodsSuspendedByParent: {
-		{Version: version.MustParse("0.16"), Default: true, PreRelease: featuregate.Beta},
+		{Version: version.MustParse("0.16"), Default: true, PreRelease: featuregate.Beta}, // GA in 0.18
 	},
 }
 
