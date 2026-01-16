155a75b2e5ab0e75a8d7c2423cf43edaf04fe36c
Yuki Iwai <yuki.iwai.tz@gmail.com>
2026-01-17 06:45:17 +0900

    chore: Fix a typo comment (#8641)
    Signed-off-by: Yuki Iwai <yuki.iwai.tz@gmail.com>

--- test/integration/framework/framework.go
+++ test/integration/framework/framework.go
@@ -258,7 +258,7 @@ var (
 	//  curl https://storage.googleapis.com/kubernetes-jenkins/pr-logs/pull/kubernetes-sigs_kueue/3054/pull-kueue-test-integration-main/1836045641336229888/artifacts/integration-top.yaml \
 	// | yq '.[] | select(.name != "") | .f.duration = .duration | .f.name = .name | .f.suite=.suite | .f | [] + .' | yq '.[0:30]' -oc
 	//
-	// taking the item which run for more then 5 sec
+	// taking the item which run for more than 5 sec
 	SlowSpec = ginkgo.Label("slow")
 
 	// RedundantSpec label used to decorate test specs that largely cover generic code covered by other specs also. (eg. Kubeflow jobs)
