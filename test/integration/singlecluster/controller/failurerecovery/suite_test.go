9ea06ce46fa80ffdb8d614000d74c9d6fc7ae332
Irving Mondragón <IrvingMg@users.noreply.github.com>
2026-01-27 18:27:50 +0100

    Add waitForWebhooks to restartManager for webhook readiness (#8790)

--- test/integration/singlecluster/controller/failurerecovery/suite_test.go
+++ test/integration/singlecluster/controller/failurerecovery/suite_test.go
@@ -30,6 +30,7 @@ import (
 	configapi "sigs.k8s.io/kueue/apis/config/v1beta2"
 	"sigs.k8s.io/kueue/pkg/constants"
 	"sigs.k8s.io/kueue/pkg/controller/failurerecovery"
+	"sigs.k8s.io/kueue/pkg/webhooks"
 	"sigs.k8s.io/kueue/test/integration/framework"
 )
 
@@ -68,4 +69,7 @@ func managerSetup(_ context.Context, mgr manager.Manager) {
 
 	_, err := terminatingPodReconciler.SetupWithManager(mgr, &configapi.Configuration{})
 	gomega.Expect(err).NotTo(gomega.HaveOccurred())
+
+	failedWebhook, err := webhooks.Setup(mgr, nil)
+	gomega.Expect(err).ToNot(gomega.HaveOccurred(), "webhook", failedWebhook)
 }
