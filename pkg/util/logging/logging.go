f37a560aed9bc3b172c2cd0abd9d96f0a648933e
dkaluza <17752074+dkaluza@users.noreply.github.com>
2026-02-04 20:30:30 +0100

    Add log level override for concurrent modification errors (#8708)
    * Add log level override for reconciler errors
    
    * Replace custom encoder with custom zap core to correctly handle log observing in tests
    
    * Add unit tests fro logging wrapper
    
    * Refine tests to include loggers created by With
    
    * Fix issues spoted by linter
    
    * Narrow down reconciler error level overriding to just concurrent modification errors.
    
    Rename ErrorLogLevelOverridenZapCore to CustomLogProcessor.
    Addjust tests to check for more specific errors.
    Remove stack trace from the handled concurrent modification errors.
    
    * Change log level to one aligned with klog.V(3)
    
    * Remove unnecessary TargetLeveL from CustomLogProcessor
    
    * Repeatedly set the status to pending in logger integration test if no warning is issued
    
    * Fix linting issue in logger integration test
    
    * Extend concurrent modfication errors logs detection to beyond reonciler errors
    
    * Change concurrent modification logs detection to use contains on prefixes
    
    * Refactor tests to follow table-like approach, add after suite logs validation
    
    * Remove inconsistent space suffix from jobframework reconciller error formating

--- pkg/util/logging/logging.go
+++ pkg/util/logging/logging.go
@@ -17,6 +17,11 @@ limitations under the License.
 package logging
 
 import (
+	arrayslices "slices"
+	"strings"
+	"unicode"
+
+	"go.uber.org/zap/zapcore"
 	"k8s.io/klog/v2"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
@@ -34,4 +39,70 @@ func GetObjectReferences[T ObjectRefProvider](items []T) []klog.ObjectRef {
 	return slices.Map(items, func(item *T) klog.ObjectRef {
 		return klog.KObj((*item).GetObject())
 	})
+}
+
+// as indicated in https://pkg.go.dev/github.com/go-logr/zapr#hdr-Usage
+// logr log levels correspond to custom zapcore levels and
+// zapLevel = -1*logrLevel, so we set it to -3 as it means first verbosity
+// not visible by users in default settings,
+// see https://github.com/kubernetes/community/blob/88841374e9558803b5b2ec81beb450e246283f09/contributors/devel/sig-instrumentation/logging.md?plain=1#L109
+const klogV3Level = zapcore.Level(-3)
+
+// zapcore.Core that overrides log level of
+// expected errors connected to concurrent resources modification and
+// omits their stack trace.
+// Those errors are emitted with klog V3 level.
+// Other logs are left intact, and written using original core.
+type CustomLogProcessor struct {
+	zapcore.Core
+}
+
+func (core CustomLogProcessor) Check(entry zapcore.Entry, checkedEntry *zapcore.CheckedEntry) *zapcore.CheckedEntry {
+	if core.Enabled(entry.Level) {
+		return checkedEntry.AddCore(entry, core)
+	}
+	return checkedEntry
+}
+
+func (core CustomLogProcessor) Write(entry zapcore.Entry, fields []zapcore.Field) error {
+	if isEntryAConcurrentModificationError(entry, fields) {
+		entry.Level = klogV3Level
+		entry.Stack = ""
+	}
+	return core.Core.Write(entry, fields)
+}
+
+const concurrentModificationErrorSuffix = "the object has been modified; please apply your changes to the latest version and try again"
+const concurrentModificationErrorPrefix = "Operation cannot be fulfilled on"
+
+func isEntryAConcurrentModificationError(entry zapcore.Entry, fields []zapcore.Field) bool {
+	if entry.Level != zapcore.ErrorLevel {
+		return false
+	}
+	return arrayslices.ContainsFunc(fields, func(field zapcore.Field) bool {
+		if field.Key != "error" || field.Type != zapcore.ErrorType {
+			return false
+		}
+		err := field.Interface.(error)
+		if err == nil {
+			return false
+		}
+		errorDescription := strings.TrimFunc(err.Error(), unicode.IsSpace)
+		// Error messages sometimes are additionally prefixed like: "clearing admission: %w"
+		// therefore we use Contains instead of HasPrefix to also detect those messages
+		return strings.Contains(errorDescription, concurrentModificationErrorPrefix) && strings.HasSuffix(errorDescription, concurrentModificationErrorSuffix)
+	})
+}
+
+func (c CustomLogProcessor) With(fields []zapcore.Field) zapcore.Core {
+	wrappedClone := c.Core.With(fields)
+	return CustomLogProcessor{
+		Core: wrappedClone,
+	}
+}
+
+func NewCustomLogProcessor(core zapcore.Core) zapcore.Core {
+	return CustomLogProcessor{
+		Core: core,
+	}
 }
