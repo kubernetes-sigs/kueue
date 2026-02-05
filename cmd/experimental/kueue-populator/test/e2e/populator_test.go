8d17b48712036866722a603746cad448d9e8ec76
Irving Mondragón <IrvingMg@users.noreply.github.com>
2026-02-05 13:38:31 +0100

    Bump k8s deps to v0.35.0 and kueue to v0.16.0 in kueue-populator (#9002)

--- cmd/experimental/kueue-populator/test/e2e/populator_test.go
+++ cmd/experimental/kueue-populator/test/e2e/populator_test.go
@@ -23,7 +23,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 
-	kueue "sigs.k8s.io/kueue/apis/kueue/v1beta1"
+	kueue "sigs.k8s.io/kueue/apis/kueue/v1beta2"
 	"sigs.k8s.io/kueue/test/util"
 )
 
