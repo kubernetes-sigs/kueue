3805b08567f429a329f31bbde16724d4c306c03a
k8s-infra-cherrypick-robot <90416843+k8s-infra-cherrypick-robot@users.noreply.github.com>
2026-02-06 12:10:32 -0800

    [release-0.16] Make RayJob top level for autoscaling (#9039)
    * Make RayJob toplevel for autoscaling
    
    Co-Authored-By: Michał Woźniak <mimowo@users.noreply.github.com>
    
    * Add TODO https://github.com/kubernetes-sigs/kueue/issues/9022 (Eliminate the global state RayJob reconciler)
    
    ---------
    
    Co-authored-by: Bo (AIML) Yang <bo_yang6@apple.com>
    Co-authored-by: Michał Woźniak <mimowo@users.noreply.github.com>

--- pkg/controller/jobs/rayjob/rayjob_webhook.go
+++ pkg/controller/jobs/rayjob/rayjob_webhook.go
@@ -18,7 +18,6 @@ package rayjob
 
 import (
 	"context"
-	"errors"
 	"fmt"
 
 	rayv1 "github.com/ray-project/kuberay/ray-operator/apis/ray/v1"
@@ -83,18 +82,8 @@ func (w *RayJobWebhook) Default(ctx context.Context, obj runtime.Object) error {
 	log := ctrl.LoggerFrom(ctx).WithName("rayjob-webhook")
 	log.V(5).Info("Applying defaults")
 	jobframework.ApplyDefaultLocalQueue(job.Object(), w.queues.DefaultLocalQueueExist)
-	if job.Spec.RayClusterSpec == nil || !ptr.Deref(job.Spec.RayClusterSpec.EnableInTreeAutoscaling, false) {
-		if err := jobframework.ApplyDefaultForSuspend(ctx, job, w.client, w.manageJobsWithoutQueueName, w.managedJobsNamespaceSelector); err != nil {
-			return err
-		}
-	} else {
-		if !workloadslicing.Enabled(job.Object()) {
-			return errors.New("RayJob should enable workload slicing if autoscaling is enabled")
-		}
-		if job.Spec.Suspend {
-			return errors.New("RayJob should not set suspend to true if autoscaling is enabled")
-		}
-		log.V(5).Info("Do not apply default for suspend due to EnableInTreeAutoscaling", "jobName", job.Name, "jobNamespace", job.Namespace)
+	if err := jobframework.ApplyDefaultForSuspend(ctx, job, w.client, w.manageJobsWithoutQueueName, w.managedJobsNamespaceSelector); err != nil {
+		return err
 	}
 	jobframework.ApplyDefaultForManagedBy(job, w.queues, w.cache, log)
 	return nil
