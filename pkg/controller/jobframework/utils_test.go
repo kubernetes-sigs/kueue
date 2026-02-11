3d641e2ffb56054ef511bfc507d3ababadb12715
k8s-infra-cherrypick-robot <90416843+k8s-infra-cherrypick-robot@users.noreply.github.com>
2026-02-11 07:40:02 -0800

    [release-0.16] Add init containers sanitization (#9126)
    * Add init containers sanitization
    
    Sort imports
    
    * PR comments
    
    * Remove redundant test
    
    * Verification fix
    
    ---------
    
    Co-authored-by: monabil08 <mohnabil0808@gmail.com>

---
+++ pkg/controller/jobframework/utils_test.go
@@ -0,0 +1,127 @@
+/*
+Copyright The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package jobframework_test
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	corev1 "k8s.io/api/core/v1"
+
+	kueue "sigs.k8s.io/kueue/apis/kueue/v1beta2"
+	"sigs.k8s.io/kueue/pkg/controller/jobframework"
+	"sigs.k8s.io/kueue/pkg/features"
+	utiltesting "sigs.k8s.io/kueue/pkg/util/testing"
+	utiltestingapi "sigs.k8s.io/kueue/pkg/util/testing/v1beta2"
+)
+
+func TestSanitizePodSets(t *testing.T) {
+	testCases := map[string]struct {
+		podSets         []kueue.PodSet
+		featureEnabled  bool
+		expectedPodSets []kueue.PodSet
+	}{
+
+		"disabled feature gate": {
+			featureEnabled: false,
+			podSets: []kueue.PodSet{
+				*utiltestingapi.MakePodSet("test", 1).
+					Containers(*utiltesting.MakeContainer().
+						Name("c1").
+						WithEnvVar(corev1.EnvVar{Name: "ENV1", Value: "value1"}).
+						WithEnvVar(corev1.EnvVar{Name: "ENV1", Value: "value2"}).
+						Obj()).
+					Obj(),
+			},
+			expectedPodSets: []kueue.PodSet{
+				*utiltestingapi.MakePodSet("test", 1).
+					Containers(*utiltesting.MakeContainer().
+						Name("c1").
+						WithEnvVar(corev1.EnvVar{Name: "ENV1", Value: "value1"}).
+						WithEnvVar(corev1.EnvVar{Name: "ENV1", Value: "value2"}).
+						Obj()).
+					Obj(),
+			},
+		},
+		"enabled feature gate, init containers and containers": {
+			featureEnabled: true,
+			podSets: []kueue.PodSet{
+				*utiltestingapi.MakePodSet("test", 1).
+					Containers(*utiltesting.MakeContainer().
+						Name("c1").
+						WithEnvVar(corev1.EnvVar{Name: "ENV1", Value: "value1"}).
+						WithEnvVar(corev1.EnvVar{Name: "ENV1", Value: "value2"}).
+						Obj()).
+					InitContainers(*utiltesting.MakeContainer().
+						Name("init1").
+						WithEnvVar(corev1.EnvVar{Name: "ENV2", Value: "value3"}).
+						WithEnvVar(corev1.EnvVar{Name: "ENV2", Value: "value4"}).
+						Obj()).
+					Obj(),
+			},
+			expectedPodSets: []kueue.PodSet{
+				*utiltestingapi.MakePodSet("test", 1).
+					Containers(*utiltesting.MakeContainer().
+						Name("c1").
+						WithEnvVar(corev1.EnvVar{Name: "ENV1", Value: "value2"}).
+						Obj()).
+					InitContainers(*utiltesting.MakeContainer().
+						Name("init1").
+						WithEnvVar(corev1.EnvVar{Name: "ENV2", Value: "value4"}).
+						Obj()).
+					Obj(),
+			},
+		},
+		"enabled feature gate, containers only": {
+			featureEnabled: true,
+			podSets: []kueue.PodSet{
+				*utiltestingapi.MakePodSet("test", 1).
+					Containers(*utiltesting.MakeContainer().
+						Name("c1").
+						WithEnvVar(corev1.EnvVar{Name: "ENV1", Value: "value1"}).
+						WithEnvVar(corev1.EnvVar{Name: "ENV1", Value: "value2"}).
+						Obj()).
+					Obj(),
+			},
+			expectedPodSets: []kueue.PodSet{
+				*utiltestingapi.MakePodSet("test", 1).
+					Containers(*utiltesting.MakeContainer().
+						Name("c1").
+						WithEnvVar(corev1.EnvVar{Name: "ENV1", Value: "value2"}).
+						Obj()).
+					Obj(),
+			},
+		},
+		"empty podsets": {
+			featureEnabled:  true,
+			podSets:         []kueue.PodSet{},
+			expectedPodSets: []kueue.PodSet{},
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			_ = features.SetEnable(features.SanitizePodSets, tc.featureEnabled)
+
+			jobframework.SanitizePodSets(tc.podSets)
+
+			if diff := cmp.Diff(tc.expectedPodSets, tc.podSets); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
