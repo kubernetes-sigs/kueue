bc20b112a4524d6564634d5cb8fd179c04704955
Singularity23x0 <dpastuszka@google.com>
2026-02-20 11:36:19 +0000

    Test setup correction.

--- test/integration/singlecluster/controller/core/clusterqueue_controller_test.go
+++ test/integration/singlecluster/controller/core/clusterqueue_controller_test.go
@@ -966,10 +966,11 @@ var _ = ginkgo.Describe("ClusterQueue controller", ginkgo.Label("controller:clus
 
 	ginkgo.When("Deleting clusterQueues", func() {
 		var (
-			cq     *kueue.ClusterQueue
-			lq     *kueue.LocalQueue
-			check  *kueue.AdmissionCheck
-			flavor *kueue.ResourceFlavor
+			cq          *kueue.ClusterQueue
+			lq          *kueue.LocalQueue
+			check       *kueue.AdmissionCheck
+			flavor      *kueue.ResourceFlavor
+			cqAdmission *kueue.Admission
 		)
 
 		ginkgo.BeforeEach(func() {
@@ -986,9 +987,17 @@ var _ = ginkgo.Describe("ClusterQueue controller", ginkgo.Label("controller:clus
 			lq = utiltestingapi.MakeLocalQueue("queue", ns.Name).ClusterQueue(cq.Name).Obj()
 			util.MustCreate(ctx, k8sClient, lq)
 			util.MustCreate(ctx, k8sClient, cq)
+
+			podSetAssignment := utiltestingapi.MakePodSetAssignment(kueue.DefaultPodSetName).Flavor(
+				resourceGPU,
+				kueue.ResourceFlavorReference(flavor.Name),
+			).Obj()
+			cqAdmission = utiltestingapi.MakeAdmission(cq.Name).PodSets(podSetAssignment).Obj()
 		})
 
 		ginkgo.AfterEach(func() {
+			util.ExpectObjectToBeDeleted(ctx, k8sClient, cq, true)
+			util.ExpectObjectToBeDeleted(ctx, k8sClient, lq, true)
 			util.ExpectObjectToBeDeleted(ctx, k8sClient, check, true)
 			util.ExpectObjectToBeDeleted(ctx, k8sClient, flavor, true)
 		})
@@ -1006,11 +1015,7 @@ var _ = ginkgo.Describe("ClusterQueue controller", ginkgo.Label("controller:clus
 			wl := utiltestingapi.MakeWorkload("workload", ns.Name).Queue(kueue.LocalQueueName(lq.Name)).Obj()
 			util.MustCreate(ctx, k8sClient, wl)
 			key := client.ObjectKeyFromObject(wl)
-			podSetAssignment := utiltestingapi.MakePodSetAssignment(kueue.DefaultPodSetName).Flavor(
-				resourceGPU,
-				kueue.ResourceFlavorReference(flavor.Name),
-			).Obj()
-			util.SetQuotaReservation(ctx, k8sClient, key, utiltestingapi.MakeAdmission(cq.Name).PodSets(podSetAssignment).Obj())
+			util.SetQuotaReservation(ctx, k8sClient, key, cqAdmission)
 
 			ginkgo.By("Set admission check ready")
 			util.SetWorkloadsAdmissionCheck(ctx, k8sClient, wl, kueue.AdmissionCheckReference(check.Name), kueue.CheckStateReady, true)
@@ -1060,7 +1065,7 @@ var _ = ginkgo.Describe("ClusterQueue controller", ginkgo.Label("controller:clus
 			wl := utiltestingapi.MakeWorkload("workload", ns.Name).Queue(kueue.LocalQueueName(lq.Name)).Obj()
 			util.MustCreate(ctx, k8sClient, wl)
 			key := client.ObjectKeyFromObject(wl)
-			util.SetQuotaReservation(ctx, k8sClient, key, utiltestingapi.MakeAdmission(cq.Name).Obj())
+			util.SetQuotaReservation(ctx, k8sClient, key, cqAdmission)
 			util.SetWorkloadsAdmissionCheck(ctx, k8sClient, wl, kueue.AdmissionCheckReference(check.Name), kueue.CheckStateReady, true)
 			gomega.Eventually(func(g gomega.Gomega) {
 				updatedWl := &kueue.Workload{}
