ff4b1bee68aee0ab694ddd8fa1f911e001a306b6
Michał Woźniak <michalwozniak@google.com>
2026-02-26 13:06:37 +0000

    move certs test

--- test/e2e/customconfigs/certs_test.go
+++ test/e2e/customconfigs/certs_test.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package e2e
+package customconfigse2e
 
 import (
 	"github.com/onsi/ginkgo/v2"
