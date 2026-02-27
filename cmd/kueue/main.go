5c26862d0022c8beca9b6b184bd79689c37d341c
nilsachy <nils.achy@gmail.com>
2026-02-27 13:50:43 +0000

    feat(visibility): allow visibility server to use custom kubeconfig path

--- cmd/kueue/main.go
+++ cmd/kueue/main.go
@@ -136,6 +136,11 @@ func main() {
 	zapOptions.BindFlags(flag.CommandLine)
 	flag.Parse()
 
+	var kubeConfigPath string
+	if kubeConfigFlag := flag.Lookup("kubeconfig"); kubeConfigFlag != nil {
+		kubeConfigPath = kubeConfigFlag.Value.String()
+	}
+
 	logger := zap.New(zap.UseFlagOptions(&zapOptions))
 	ctrl.SetLogger(logger)
 
@@ -347,7 +352,7 @@ func main() {
 
 	if features.Enabled(features.VisibilityOnDemand) {
 		go func() {
-			if err := visibility.CreateAndStartVisibilityServer(ctx, queues, *cfg.InternalCertManagement.Enable, kubeConfig, parsedTLSConfig); err != nil {
+			if err := visibility.CreateAndStartVisibilityServer(ctx, queues, *cfg.InternalCertManagement.Enable, kubeConfig, kubeConfigPath, parsedTLSConfig); err != nil {
 				setupLog.Error(err, "Unable to create and start visibility server")
 				os.Exit(1)
 			}
