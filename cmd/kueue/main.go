649d66c719d68fed8837e4e07433506b20821d22
nilsachy <nils.achy@gmail.com>
2026-03-04 16:54:50 +0000

    Add visibility server configuration support in API and update server initialization

--- cmd/kueue/main.go
+++ cmd/kueue/main.go
@@ -353,7 +353,7 @@ func main() {
 
 	if features.Enabled(features.VisibilityOnDemand) {
 		go func() {
-			if err := visibility.CreateAndStartVisibilityServer(ctx, queues, *cfg.InternalCertManagement.Enable, kubeConfig, parsedTLSConfig, visibilityServerFlags); err != nil {
+			if err := visibility.CreateAndStartVisibilityServer(ctx, queues, &cfg, kubeConfig, parsedTLSConfig, visibilityServerFlags); err != nil {
 				setupLog.Error(err, "Unable to create and start visibility server")
 				os.Exit(1)
 			}
