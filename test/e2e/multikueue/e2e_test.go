ef295b1a920bf4948855e6dd6d41c4883deb334d
Singularity23x0 <dpastuszka@google.com>
2026-03-04 14:55:32 +0000

    Redjusted test to use only valid resource requests.

--- test/e2e/multikueue/e2e_test.go
+++ test/e2e/multikueue/e2e_test.go
@@ -791,13 +791,20 @@ var _ = ginkgo.Describe("MultiKueue", func() {
 		})
 
 		ginkgo.It("Should preempt a running low-priority workload when a high-priority workload is admitted (same worker)", func() {
+			ginkgo.By("Ensuring worker2 does not have enough quota to handle created workloads", func() {
+				gomega.Eventually(func(g gomega.Gomega) {
+					g.Expect(k8sWorker2Client.Get(ctx, client.ObjectKeyFromObject(worker2Cq), worker2Cq)).To(gomega.Succeed())
+					worker2Cq.Spec.ResourceGroups[0].Flavors[0].Resources[0].NominalQuota = resource.MustParse("100m")
+					g.Expect(k8sWorker2Client.Update(ctx, worker2Cq)).To(gomega.Succeed())
+				}, util.Timeout, util.Interval).Should(gomega.Succeed())
+			})
+
 			lowJob := testingjob.MakeJob("low-job", managerNs.Name).
 				Image(util.GetAgnHostImage(), util.BehaviorWaitForDeletion).
 				WorkloadPriorityClass(managerLowWPC.Name).
 				Queue(kueue.LocalQueueName(managerLq.Name)).
 				RequestAndLimit(corev1.ResourceCPU, "500m").
 				RequestAndLimit(corev1.ResourceMemory, "1G").
-				RequestAndLimit(corev1.ResourceStorage, "5G").
 				Obj()
 			util.MustCreate(ctx, k8sManagerClient, lowJob)
 
@@ -836,7 +843,6 @@ var _ = ginkgo.Describe("MultiKueue", func() {
 				Queue(kueue.LocalQueueName(managerLq.Name)).
 				RequestAndLimit(corev1.ResourceCPU, "1500m").
 				RequestAndLimit(corev1.ResourceMemory, "1G").
-				RequestAndLimit(corev1.ResourceStorage, "5G").
 				Obj()
 			util.MustCreate(ctx, k8sManagerClient, highJob)
 
