dac0aedb901e069e0a39223d0f64bea684de0d3d
nilsachy <nils.achy@gmail.com>
2026-03-05 09:36:38 +0000

    Revert "test(visibility): add unit tests for createVisibilityServerOptions with kubeconfig flag"
    This reverts commit a4add98a15f4a581dd70d0aa57b87d7059f3ba85.

--- pkg/visibility/server.go
+++ pkg/visibility/server.go
@@ -112,15 +112,14 @@ func applyVisibilityServerOptions(config *genericapiserver.RecommendedConfig, en
 	}
 
 	if f := flag.Lookup("kubeconfig"); f != nil {
-		if kubeConfigPath := f.Value.String(); kubeConfigPath != "" {
-			o.Authentication.RemoteKubeConfigFile = kubeConfigPath
-			o.Authorization.RemoteKubeConfigFile = kubeConfigPath
-		}
+		kubeConfigPath := f.Value.String()
+		o.Authentication.RemoteKubeConfigFile = kubeConfigPath
+		o.Authorization.RemoteKubeConfigFile = kubeConfigPath
 	}
 
 	o.Admission.DisablePlugins = disabledPlugins
 	if err := o.SecureServing.MaybeDefaultWithSelfSignedCerts("localhost", nil, []net.IP{net.ParseIP("127.0.0.1")}); err != nil {
-		return nil, fmt.Errorf("error creating self-signed certificates: %v", err)
+		return fmt.Errorf("error creating self-signed certificates: %v", err)
 	}
 	return o.ApplyTo(config)
 }
