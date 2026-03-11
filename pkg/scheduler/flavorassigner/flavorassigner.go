31f63c3aa3a930869ee087d01bdaa41545dfeb26
vladikkuzn <vladikkuzn1@gmail.com>
2026-03-11 19:40:37 +0200

    Temporarily restore the FlavorFungibilityImplicitPreferenceDefault feature gate to allow upgrades #9790

--- pkg/scheduler/flavorassigner/flavorassigner.go
+++ pkg/scheduler/flavorassigner/flavorassigner.go
@@ -437,6 +437,10 @@ func isPreferred(a, b granularMode, fungibilityConfig kueue.FlavorFungibility) b
 		}
 	}
 
+	if features.Enabled(features.FlavorFungibilityImplicitPreferenceDefault) && fungibilityConfig.WhenCanBorrow == kueue.TryNextFlavor {
+		return preemptionOverBorrowing()
+	}
+
 	return borrowingOverPreemption()
 }
 
