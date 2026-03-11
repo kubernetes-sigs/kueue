5a52597c9f4a32fd5dd163bcd1faae8f17229b52
Mykhailo Bobrovskyi <mikhail.bobrovsky@gmail.com>
2026-03-11 19:04:24 +0200

    Introduce SchedulerLongRequeueInterval feature gate.

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -271,6 +271,12 @@ const (
 	// issue: https://github.com/kubernetes-sigs/kueue/issues/9694
 	// Skip equivalent inadmissible workloads in BestEffortFIFO scheduling.
 	SchedulingEquivalenceHashing featuregate.Feature = "SchedulingEquivalenceHashing"
+
+	// owner: @mbobrovskyi
+	//
+	// issue: https://github.com/kubernetes-sigs/kueue/issues/9799
+	// Use 10s interval for scheduler requeuing.
+	SchedulerLongRequeueInterval featuregate.Feature = "SchedulerLongRequeueInterval"
 )
 
 func init() {
@@ -420,9 +426,11 @@ var defaultVersionedFeatureGates = map[featuregate.Feature]featuregate.Versioned
 	TASReplaceNodeOnNodeTaints: {
 		{Version: version.MustParse("0.15"), Default: false, PreRelease: featuregate.Alpha},
 	},
-
 	SchedulingEquivalenceHashing: {
 		{Version: version.MustParse("0.15"), Default: true, PreRelease: featuregate.Beta},
+	},
+	SchedulerLongRequeueInterval: {
+		{Version: version.MustParse("0.15"), Default: false, PreRelease: featuregate.Alpha}, // remove in 0.20
 	},
 }
 
