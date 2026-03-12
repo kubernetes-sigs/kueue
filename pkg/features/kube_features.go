f4ff04df510b4f772ca0fb8b7e6bd0b6199fe77e
Mykhailo Bobrovskyi <mikhail.bobrovsky@gmail.com>
2026-03-13 00:59:35 +0530

    Introduce SchedulerTimestampPreemptionBuffer feature. (#9837)

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -274,6 +274,13 @@ const (
 	// issue: https://github.com/kubernetes-sigs/kueue/issues/9799
 	// Use 10s interval for scheduler requeuing.
 	SchedulerLongRequeueInterval featuregate.Feature = "SchedulerLongRequeueInterval"
+
+	// owner: @mbobrovskyi
+	//
+	// issue: https://github.com/kubernetes-sigs/kueue/issues/9799
+	// Use a 5min buffer so that workloads with scheduling timestamps within this
+	// buffer do not preempt each other based on LowerOrNewerEqualPriority.
+	SchedulerTimestampPreemptionBuffer featuregate.Feature = "SchedulerTimestampPreemptionBuffer"
 )
 
 func init() {
@@ -422,6 +429,9 @@ var defaultVersionedFeatureGates = map[featuregate.Feature]featuregate.Versioned
 		{Version: version.MustParse("0.16"), Default: true, PreRelease: featuregate.Beta},
 	},
 	SchedulerLongRequeueInterval: {
+		{Version: version.MustParse("0.16"), Default: false, PreRelease: featuregate.Alpha}, // remove in 0.20
+	},
+	SchedulerTimestampPreemptionBuffer: {
 		{Version: version.MustParse("0.16"), Default: false, PreRelease: featuregate.Alpha}, // remove in 0.20
 	},
 }
