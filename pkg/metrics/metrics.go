39dfede5da90dd918105fe425e417f92be83fdb1
nilsachy <nils.achy@gmail.com>
2026-04-13 15:01:10 +0000

    refactor: update job_to_workload_latency_seconds histogram buckets to use exponential distribution

--- pkg/metrics/metrics.go
+++ pkg/metrics/metrics.go
@@ -518,7 +518,7 @@ The label 'underlying_cause' can have the following values:
 			Subsystem: constants.KueueName,
 			Name:      "job_to_workload_latency_seconds",
 			Help:      "The time between a job was created until its workload was created, per 'job_kind'",
-			Buckets:   []float64{0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 0.75, 1, 1.5, 2, 3, 5, 10},
+			Buckets:   prometheus.ExponentialBuckets(0.01, 2, 11),
 		}, append([]string{"job_kind", "replica_role"}, extraLabels...),
 	)
 
