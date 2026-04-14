16a3a111fb03dcbf092db900697a16d34e6a84eb
Boshi LIAN <bolian@microsoft.com>
2026-04-14 04:56:39 +0000

    TAS: recognize PodSetSliceRequiredTopologyConstraintsAnnotation in IsExplicitTAS()

--- pkg/util/tas/tas.go
+++ pkg/util/tas/tas.go
@@ -50,6 +50,9 @@ func IsExplicitTAS(annots map[string]string) bool {
 	if _, ok := annots[kueue.PodSetSliceRequiredTopologyAnnotation]; ok {
 		return true
 	}
+	if _, ok := annots[kueue.PodSetSliceRequiredTopologyConstraintsAnnotation]; ok {
+		return true
+	}
 	return false
 }
 
