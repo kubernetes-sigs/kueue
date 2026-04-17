f0c56e31110f4cc2dbaee22d4934575b2d895922
Shaanveer Singh <shaanver.singh@gmail.com>
2026-04-17 07:32:25 +0200

    Fix promote_pull.sh to update remotes before branching
    Signed-off-by: Shaanveer Singh <shaanver.singh@gmail.com>

--- hack/releasing/promote_pull.sh
+++ hack/releasing/promote_pull.sh
@@ -122,6 +122,9 @@ if [[ ! "$RELEASE_VERSION" =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
   exit 1
 fi
 
+echo "+++ Updating upstream remote..."
+git remote update "${KUBERNETES_K8S_IO_UPSTREAM_REMOTE}"
+
 IFS='.' read -r MAJOR MINOR _ <<< "${RELEASE_VERSION#v}"
 
 declare -r MAJOR_MINOR="$MAJOR.$MINOR"
