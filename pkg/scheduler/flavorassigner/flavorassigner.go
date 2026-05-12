789964bb2f786b149ab7eaa29ca1515b5f799a22
Dominik Pajak <dominikpajak@google.com>
2026-05-12 11:18:21 +0000

    Move as utility func

--- pkg/scheduler/flavorassigner/flavorassigner.go
+++ pkg/scheduler/flavorassigner/flavorassigner.go
@@ -40,6 +40,7 @@ import (
 	"sigs.k8s.io/kueue/pkg/scheduler/preemption/classical"
 	preemptioncommon "sigs.k8s.io/kueue/pkg/scheduler/preemption/common"
 	utilmaps "sigs.k8s.io/kueue/pkg/util/maps"
+	utilmath "sigs.k8s.io/kueue/pkg/util/math"
 	"sigs.k8s.io/kueue/pkg/util/orderedgroups"
 	"sigs.k8s.io/kueue/pkg/util/tas"
 	"sigs.k8s.io/kueue/pkg/workload"
@@ -1073,12 +1074,7 @@ func (a *FlavorAssigner) fitsResourceQuota(log logr.Logger, fr resources.FlavorR
 	available := a.cq.Available(fr)
 	maxCapacity := a.cq.PotentialAvailable(fr)
 
-	var val int64
-	if assumedUsage > math.MaxInt64-requestUsage {
-		val = math.MaxInt64
-	} else {
-		val = assumedUsage + requestUsage
-	}
+	val := utilmath.SaturatingAdd(assumedUsage, requestUsage)
 
 	// No Fit
 	if val > maxCapacity {
