19779968dea47ae4b4d2069f1148447cfd52c0dd
ekam-walia <164983405+ekam-walia@users.noreply.github.com>
2026-05-19 12:58:35 +0530

    final change with all suggestions

--- test/e2e/tas/baseline/suite_test.go
+++ test/e2e/tas/baseline/suite_test.go
@@ -27,15 +27,13 @@ import (
 	"k8s.io/apimachinery/pkg/api/resource"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
-	configapi "sigs.k8s.io/kueue/apis/config/v1beta2"
 	clientutil "sigs.k8s.io/kueue/pkg/util/client"
 	"sigs.k8s.io/kueue/test/util"
 )
 
 var (
-	k8sClient       client.WithWatch
-	ctx             context.Context
-	defaultKueueCfg *configapi.Configuration
+	k8sClient client.WithWatch
+	ctx       context.Context
 )
 
 func TestAPIs(t *testing.T) {
@@ -49,8 +47,6 @@ var _ = ginkgo.BeforeSuite(func() {
 	k8sClient, _, err = util.CreateClientUsingCluster("")
 	gomega.Expect(err).NotTo(gomega.HaveOccurred())
 	ctx = ginkgo.GinkgoT().Context()
-
-	defaultKueueCfg = util.GetKueueConfiguration(ctx, k8sClient)
 
 	waitForAvailableStart := time.Now()
 	util.WaitForKueueAvailability(ctx, k8sClient)
