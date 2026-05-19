9cd16b33d80dd0ca7a76375cf7d0a548d97168ed
Ekamveer Walia <164983405+ekam-walia@users.noreply.github.com>
2026-05-19 19:50:00 +0530

    follow up PR for my last PR i.e is merged (#11323)

--- cmd/kueue/main.go
+++ cmd/kueue/main.go
@@ -370,7 +370,7 @@ func main() {
 
 	if features.Enabled(features.VisibilityOnDemand) {
 		go func() {
-			if err := visibility.CreateAndStartVisibilityServer(ctx, queues, &cfg, kubeConfig, configapi.DefaultVisibilityBindPort, parsedTLSConfig); err != nil {
+			if err := visibility.CreateAndStartVisibilityServer(ctx, queues, &cfg, kubeConfig, parsedTLSConfig); err != nil {
 				setupLog.Error(err, "Unable to create and start visibility server")
 				os.Exit(1)
 			}
