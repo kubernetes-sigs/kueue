0daad6e296dbb6bf9e0c772331eadb0e6ced59fa
Vamsi Krishna Siddu <vamsikrishna.siddu@ibm.com>
2026-05-22 20:19:10 +0530

    test: fix AppWrapper completion flake by using LongTimeout
     MediumTimeout (45s) is insufficient for AppWrapper to transition
     from Running to Succeeded when pod teardown is slow in CI. Use
     LongTimeout (90s) for all AppWrapperSucceeded assertions.
    
    Signed-off-by: Vamsi Krishna Siddu <vamsikrishna.siddu@ibm.com>

--- test/e2e/sequential/extended/managejobswithoutqueuename_test.go
+++ test/e2e/sequential/extended/managejobswithoutqueuename_test.go
@@ -230,7 +230,7 @@ var _ = ginkgo.Describe("ManageJobsWithoutQueueName", ginkgo.Label("feature:mana
 				gomega.Eventually(func(g gomega.Gomega) {
 					g.Expect(k8sClient.Get(ctx, client.ObjectKeyFromObject(aw), createdAppWrapper)).To(gomega.Succeed())
 					g.Expect(createdAppWrapper.Status.Phase).To(gomega.Equal(awv1beta2.AppWrapperSucceeded))
-				}, util.MediumTimeout, util.Interval).Should(gomega.Succeed())
+				}, util.LongTimeout, util.Interval).Should(gomega.Succeed())
 			})
 		})
 
@@ -291,7 +291,7 @@ var _ = ginkgo.Describe("ManageJobsWithoutQueueName", ginkgo.Label("feature:mana
 				gomega.Eventually(func(g gomega.Gomega) {
 					g.Expect(k8sClient.Get(ctx, client.ObjectKeyFromObject(aw), createdAppWrapper)).To(gomega.Succeed())
 					g.Expect(createdAppWrapper.Status.Phase).To(gomega.Equal(awv1beta2.AppWrapperSucceeded))
-				}, util.MediumTimeout, util.Interval).Should(gomega.Succeed())
+				}, util.LongTimeout, util.Interval).Should(gomega.Succeed())
 			})
 		})
 
