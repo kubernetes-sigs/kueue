69a355e176cb6328d8aab9dada8a53189c2c5d29
Mykhailo Bobrovskyi <mikhail.bobrovsky@gmail.com>
2026-05-25 15:31:12 +0300

    Finish Workloads corresponding to Jobs deleted with --cascede=orphan

--- pkg/controller/jobs/pod/pod_controller_test.go
+++ pkg/controller/jobs/pod/pod_controller_test.go
@@ -3734,7 +3734,7 @@ func TestReconciler(t *testing.T) {
 				},
 			},
 		},
-		"finalize workload for non existent pod with FinishOrphanedWorkloads disabled": {
+		"finalize workload for non existent pod with FinalizeAndFinishOrphanedWorkloads disabled": {
 			featureGates: map[featuregate.Feature]bool{features.FinishOrphanedWorkloads: false},
 			reconcileKey: &types.NamespacedName{Namespace: "ns", Name: "deleted_pod"},
 			workloads: []kueue.Workload{
@@ -3750,7 +3750,7 @@ func TestReconciler(t *testing.T) {
 			},
 			workloadCmpOpts: defaultWorkloadCmpOpts,
 		},
-		"finalize workload for non existent pod with FinishOrphanedWorkloads enabled": {
+		"finalize workload for non existent pod with FinalizeAndFinishOrphanedWorkloads enabled": {
 			featureGates: map[featuregate.Feature]bool{features.FinishOrphanedWorkloads: true},
 			reconcileKey: &types.NamespacedName{Namespace: "ns", Name: "deleted_pod"},
 			workloads: []kueue.Workload{
