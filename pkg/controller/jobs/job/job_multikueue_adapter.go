95e79e44cd14282addfc020f663ca9e2a4f7b4c4
Olek Zabłocki <olekz@google.com>
2026-05-26 14:29:41 +0000

    Catch finished jobs

--- pkg/controller/jobs/job/job_multikueue_adapter.go
+++ pkg/controller/jobs/job/job_multikueue_adapter.go
@@ -25,6 +25,7 @@ import (
 	"github.com/go-logr/logr"
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/equality"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
@@ -64,15 +65,14 @@ func (b *multiKueueAdapter) SyncJob(ctx context.Context, localClient client.Clie
 
 	// the remote job exists
 	if err == nil {
-		if shouldSkipSyncForSuspendedLocalJob(log, &localJob, &remoteJob) {
-			return nil
-		}
-
-		if err := clientutil.PatchStatus(ctx, localClient, &localJob, func() (bool, error) {
-			localJob.Status = remoteJob.Status
-			return true, nil
-		}); err != nil {
-			return err
+		statusUpdate := determineStatusUpdate(ctx, log, &localJob, &remoteJob)
+		if !equality.Semantic.DeepEqual(localJob.Status, *statusUpdate) {
+			if err := clientutil.PatchStatus(ctx, localClient, &localJob, func() (bool, error) {
+				localJob.Status = *statusUpdate
+				return true, nil
+			}); err != nil {
+				return err
+			}
 		}
 
 		// Sync elastic workload if needed.
@@ -103,7 +103,7 @@ func (b *multiKueueAdapter) SyncJob(ctx context.Context, localClient client.Clie
 	return remoteClient.Create(ctx, &remoteJob)
 }
 
-func shouldSkipSyncForSuspendedLocalJob(log logr.Logger, localJob, remoteJob *batchv1.Job) bool {
+func determineStatusUpdate(ctx context.Context, log logr.Logger, localJob, remoteJob *batchv1.Job) *batchv1.JobStatus {
 	localJobInfo := fromObject(localJob)
 
 	if !localJobInfo.IsSuspended() {
@@ -119,25 +119,63 @@ func shouldSkipSyncForSuspendedLocalJob(log logr.Logger, localJob, remoteJob *ba
 		log.V(3).Info("Peforming the sync as the local and the remote Job are suspended")
 		return &remoteJob.Status
 	}
-	if localJob.Status.StartTime == nil {
-		newLocalStatus := localJob.Status.DeepCopy()
-		newLocalStatus.Active = 0
-		newConditions, _ := ensureJobConditionStatus(newLocalStatus.Conditions,
-			batchv1.JobSuspended,
-			corev1.ConditionTrue,
-			"MultiKueueAdapted",
-			"Set by MultiKueue adapted",
-			time.Now())
-		log.V(2).Info("Updating the localJob suspended Job without startTime to set the JobSuspended=True condition")
-		newLocalStatus.Conditions = newConditions
-		return newLocalStatus
+	if _, _, finished := remoteJobInfo.Finished(ctx); finished {
+		log.V(2).Info("Remote job is finished, returning the remote job status")
+		return &remoteJob.Status
 	}
+	newLocalStatus := localJob.Status.DeepCopy()
+	newLocalStatus.Active = 0
+	newLocalStatus.StartTime = nil
+	newConditions, _ := ensureJobConditionStatus(newLocalStatus.Conditions,
+		batchv1.JobSuspended,
+		corev1.ConditionTrue,
+		"MultiKueueAdapter",
+		"Set by MultiKueue adapter",
+		time.Now())
+	log.V(2).Info("Updating the localJob suspended Job to set the JobSuspended=True condition")
+	newLocalStatus.Conditions = newConditions
+	return newLocalStatus
+}
 
-	// We skip the sync when the localJob has suspend=true, and the remote job is suspend=false
-	// to prevent the race condition when the local Job is updated to suspend=false prematurely
-	// so that the injection of nodeSlectors does not work; see: https://github.com/kubernetes-sigs/kueue/pull/3685
-	log.V(2).Info("Skipping the sync when the localJob has suspend=true, and the remote job is suspend=false")
-	return true
+// ensureJobConditionStatus appends or updates an existing job condition of the
+// given type with the given status value. Note that this function will not
+// append to the conditions list if the new condition's status is false
+// (because going from nothing to false is meaningless); it can, however,
+// update the status condition to false. The function returns a bool to let the
+// caller know if the list was changed (either appended or updated).
+func ensureJobConditionStatus(list []batchv1.JobCondition, cType batchv1.JobConditionType, status corev1.ConditionStatus, reason, message string, now time.Time) ([]batchv1.JobCondition, bool) {
+	if condition := findConditionByType(list, cType); condition != nil {
+		if condition.Status != status {
+			*condition = *newCondition(cType, status, reason, message, now)
+			return list, true
+		}
+		return list, false
+	}
+	// A condition with that type doesn't exist in the list.
+	if status != corev1.ConditionFalse {
+		return append(list, *newCondition(cType, status, reason, message, now)), true
+	}
+	return list, false
+}
+
+func findConditionByType(list []batchv1.JobCondition, cType batchv1.JobConditionType) *batchv1.JobCondition {
+	for i := range list {
+		if list[i].Type == cType {
+			return &list[i]
+		}
+	}
+	return nil
+}
+
+func newCondition(conditionType batchv1.JobConditionType, status corev1.ConditionStatus, reason, message string, now time.Time) *batchv1.JobCondition {
+	return &batchv1.JobCondition{
+		Type:               conditionType,
+		Status:             status,
+		LastProbeTime:      metav1.NewTime(now),
+		LastTransitionTime: metav1.NewTime(now),
+		Reason:             reason,
+		Message:            message,
+	}
 }
 
 func (b *multiKueueAdapter) DeleteRemoteObject(ctx context.Context, _ client.Client, remoteClient client.Client, key types.NamespacedName) error {
