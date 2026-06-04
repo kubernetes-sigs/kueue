fa90b5d9b7af8c3c59641d2e3aca63f2e61d4f14
ekam-walia <164983405+ekam-walia@users.noreply.github.com>
2026-06-04 19:58:46 +0530

    adding labels in tas tests

--- test/e2e/tas/baseline/job_test.go
+++ test/e2e/tas/baseline/job_test.go
@@ -37,7 +37,7 @@ import (
 	"sigs.k8s.io/kueue/test/util"
 )
 
-var _ = ginkgo.Describe("TopologyAwareScheduling for Job", func() {
+var _ = ginkgo.Describe("TopologyAwareScheduling for Job", ginkgo.Label(util.Shard1, "area:tas", "feature:job"), func() {
 	var ns *corev1.Namespace
 	ginkgo.BeforeEach(func() {
 		ns = util.CreateNamespaceFromPrefixWithLog(ctx, k8sClient, "e2e-tas-job-")
