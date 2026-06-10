3238c58831ec3b86754daf58b21db15966c49937
Peter Chang <peter.yaochen.chang@gmail.com>
2026-06-10 07:13:51 -0400

    Optimize the sequential/extended e2e test suites by sharding

--- test/e2e/sequential/extended/managejobswithoutqueuename_test.go
+++ test/e2e/sequential/extended/managejobswithoutqueuename_test.go
@@ -850,7 +850,7 @@ var _ = ginkgo.Describe("ManageJobsWithoutQueueName", ginkgo.Label("feature:mana
 	})
 })
 
-var _ = ginkgo.Describe("ManageJobsWithoutQueueName with ManagedJobsNamespaceSelectorAlwaysRespected=false", func() {
+var _ = ginkgo.Describe("ManageJobsWithoutQueueName with ManagedJobsNamespaceSelectorAlwaysRespected=false", ginkgo.Label("feature:managejobswithoutqueuename"), func() {
 	var (
 		ns           *corev1.Namespace
 		defaultRf    *kueue.ResourceFlavor
@@ -954,7 +954,7 @@ var _ = ginkgo.Describe("ManageJobsWithoutQueueName with ManagedJobsNamespaceSel
 	})
 })
 
-var _ = ginkgo.Describe("ManageJobsWithoutQueueName without JobSet integration", ginkgo.Ordered, func() {
+var _ = ginkgo.Describe("ManageJobsWithoutQueueName without JobSet integration", ginkgo.Label("feature:managejobswithoutqueuename"), ginkgo.Ordered, func() {
 	var (
 		ns           *corev1.Namespace
 		defaultRf    *kueue.ResourceFlavor
