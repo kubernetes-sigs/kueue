a9213cf550c19ffe91d5e2c61e3af9b738e98cd9
yuluo-yx <yuluo08290126@gmail.com>
2026-06-24 23:12:50 +0800

    fix
    Signed-off-by: yuluo-yx <yuluo08290126@gmail.com>

--- test/integration/singlecluster/controller/jobs/job/suite_test.go
+++ test/integration/singlecluster/controller/jobs/job/suite_test.go
@@ -47,11 +47,10 @@ import (
 )
 
 var (
-	cfg                     *rest.Config
-	k8sClient               client.Client
-	ctx                     context.Context
-	fwk                     *framework.Framework
-	stopJobStatusController func()
+	cfg       *rest.Config
+	k8sClient client.Client
+	ctx       context.Context
+	fwk       *framework.Framework
 )
 
 func TestAPIs(t *testing.T) {
@@ -62,11 +61,9 @@ var _ = ginkgo.BeforeSuite(func() {
 	fwk = &framework.Framework{}
 	cfg = fwk.Init()
 	ctx, k8sClient = fwk.SetupClient(cfg)
-	stopJobStatusController = util.StartJobStatusController(ctx, k8sClient)
 })
 
 var _ = ginkgo.AfterSuite(func() {
-	stopJobStatusController()
 	fwk.Teardown()
 })
 
