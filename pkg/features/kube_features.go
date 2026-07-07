1ff62c7e7ae40ec0d7d88279b424bb1a9a958ca9
Mykhailo Bobrovskyi <mikhail.bobrovsky@gmail.com>
2026-07-07 12:26:29 +0300

    Defer workload finalization until owner job is gone.

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -961,6 +961,7 @@ var defaultVersionedFeatureGates = map[featuregate.Feature]featuregate.Versioned
 
 	DeferRayServiceFinalizationForRedisCleanup: {
 		{Version: version.MustParse("0.19"), Default: true, PreRelease: featuregate.Beta},
+		{Version: version.MustParse("0.20"), Default: false, PreRelease: featuregate.Deprecated}, // remove in 0.22
 	},
 
 	TASCacheNodeMatchResults: {
