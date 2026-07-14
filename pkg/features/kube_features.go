c8ea4eb2558728a621b475dc865a1447bd062ea4
Pannaga Rao Bhoja Ramamanohara <pbhojara@redhat.com>
2026-07-14 13:20:18 -0400

    Graduate ManagedJobsNamespaceSelectorAlwaysRespected to GA
    Signed-off-by: Pannaga Rao Bhoja Ramamanohara

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -580,6 +580,7 @@ var defaultVersionedFeatureGates = map[featuregate.Feature]featuregate.Versioned
 	ManagedJobsNamespaceSelectorAlwaysRespected: {
 		{Version: version.MustParse("0.13"), Default: false, PreRelease: featuregate.Alpha},
 		{Version: version.MustParse("0.15"), Default: true, PreRelease: featuregate.Beta},
+		{Version: version.MustParse("0.19"), Default: true, PreRelease: featuregate.GA, LockToDefault: true},
 	},
 	TASBalancedPlacement: {
 		{Version: version.MustParse("0.15"), Default: false, PreRelease: featuregate.Alpha},
