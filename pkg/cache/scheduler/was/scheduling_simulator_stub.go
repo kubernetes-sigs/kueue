925fda23dc94d602a2efa528fd0ff2fbe7787506
Karol Szuster <szuster@google.com>
2026-07-21 13:27:09 +0000

    Fix missing package name in WAS stub

--- pkg/cache/scheduler/was/scheduling_simulator_stub.go
+++ pkg/cache/scheduler/was/scheduling_simulator_stub.go
@@ -7,8 +7,9 @@ import (
 	"fmt"
 
 	"k8s.io/client-go/rest"
+	"sigs.k8s.io/kueue/pkg/cache/scheduler/simulator"
 )
 
-func NewWASSimulator(ctx context.Context, restConfig *rest.Config) (SchedulingSimulator, error) {
+func NewWASSimulator(ctx context.Context, restConfig *rest.Config) (simulator.SchedulingSimulator, error) {
 	return nil, fmt.Errorf("scheduler-library integration is compiled out of this binary. Disable the SchedulerLibraryIntegration feature gate.")
 }
