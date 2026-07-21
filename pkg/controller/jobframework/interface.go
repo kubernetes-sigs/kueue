097c2db149bec6e3842975bdb89f33412e830310
Shreesha001 <shettyshreesha552@gmail.com>
2026-07-21 15:13:05 +0530

    Clamp remaining count and add reclaimable pods tests
    Signed-off-by: Shreesha001 <shettyshreesha552@gmail.com>

--- pkg/controller/jobframework/interface.go
+++ pkg/controller/jobframework/interface.go
@@ -80,6 +80,10 @@ type JobWithPodLabelSelector interface {
 // when reclaimable pod information is needed.
 type JobWithReclaimablePods interface {
 	// ReclaimablePods returns the list of reclaimable pods.
+	//
+	// Note: for Jobs with ordered Pods that support elastic scaling, implementations
+	// must account for which ranks remain after scaling down to prevent quota leaks.
+	// See the batch Job implementation and kueue#12958, kueue#13117.
 	ReclaimablePods(ctx context.Context, c client.Client) ([]kueue.ReclaimablePod, error)
 }
 
