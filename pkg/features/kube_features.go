03e5e15faf237463748b1c31ad3f55c4f71cf812
Jakub Skiba <jakubskiba@google.com>
2026-07-27 09:45:30 +0000

    set fg to alpha

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -789,7 +789,7 @@ var defaultVersionedFeatureGates = map[featuregate.Feature]featuregate.Versioned
 		{Version: version.MustParse("0.18"), Default: true, PreRelease: featuregate.Beta},
 	},
 	UnadmittedWorkloadsObservability: {
-		{Version: version.MustParse("0.18"), Default: false, PreRelease: featuregate.Beta},
+		{Version: version.MustParse("0.18"), Default: false, PreRelease: featuregate.Alpha},
 	},
 }
 
