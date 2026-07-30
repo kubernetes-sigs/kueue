f617edd237efd7194eb0f619a8153664906329e8
ekam-walia <164983405+ekam-walia@users.noreply.github.com>
2026-07-30 11:06:50 +0530

    ..

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -722,10 +722,6 @@ var defaultVersionedFeatureGates = map[featuregate.Feature]featuregate.Versioned
 	RemoveFinalizersWithStrictPatch: {
 		{Version: version.MustParse("0.17"), Default: true, PreRelease: featuregate.Beta},
 	},
-	SchedulerLongRequeueInterval: {
-	},
-	SchedulerTimestampPreemptionBuffer: {
-	},
 	TASReplaceNodeOnNodeTaints: {
 		{Version: version.MustParse("0.17"), Default: true, PreRelease: featuregate.Beta},
 	},
@@ -739,6 +735,10 @@ var defaultVersionedFeatureGates = map[featuregate.Feature]featuregate.Versioned
 	SchedulingEquivalenceHashing: {
 		{Version: version.MustParse("0.17"), Default: false, PreRelease: featuregate.Beta},
 		{Version: version.MustParse("0.18"), Default: true, PreRelease: featuregate.Beta},
+	},
+	SchedulerLongRequeueInterval: {
+	},
+	SchedulerTimestampPreemptionBuffer: {
 	},
 	CustomMetricLabels: {
 		{Version: version.MustParse("0.17"), Default: false, PreRelease: featuregate.Alpha},
