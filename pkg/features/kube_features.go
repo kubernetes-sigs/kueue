786f95bc3af00c6d74d61e994f8dc69bf3728cc7
ekam-walia <164983405+ekam-walia@users.noreply.github.com>
2026-07-31 22:16:46 +0530

    Cleanup: follow up for PR #13651

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -646,10 +646,6 @@ var defaultVersionedFeatureGates = map[featuregate.Feature]featuregate.Versioned
 	ReclaimablePods: {
 		{Version: version.MustParse("0.15"), Default: true, PreRelease: featuregate.Beta},
 	},
-	// PropagateBatchJobLabelsToWorkload is enabled from 0.13.10 and 0.14.5.
-	PropagateBatchJobLabelsToWorkload: {
-		{Version: version.MustParse("0.15"), Default: true, PreRelease: featuregate.Beta},
-	},
 	MultiKueueClusterProfile: {
 		{Version: version.MustParse("0.15"), Default: false, PreRelease: featuregate.Alpha},
 	},
