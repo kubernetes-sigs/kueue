f5bea19e4ac42008e8473eba622da55357c2a8da
ekam-walia <164983405+ekam-walia@users.noreply.github.com>
2026-08-03 17:43:34 +0530

    should work

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -646,6 +646,9 @@ var defaultVersionedFeatureGates = map[featuregate.Feature]featuregate.Versioned
 	ReclaimablePods: {
 		{Version: version.MustParse("0.15"), Default: true, PreRelease: featuregate.Beta},
 	},
+	PropagateBatchJobLabelsToWorkload: {
+	  {Version: version.MustParse("0.19"), Default: false, PreRelease: featuregate.Deprecated},
+  },
 	MultiKueueClusterProfile: {
 		{Version: version.MustParse("0.15"), Default: false, PreRelease: featuregate.Alpha},
 	},
