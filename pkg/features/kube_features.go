befcc51ead44b5448e8846f1356a4f507b499b22
ekam-walia <164983405+ekam-walia@users.noreply.github.com>
2026-08-03 18:37:20 +0530

    should work

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -258,12 +258,6 @@ const (
 	// Enabled skip adding finalizers for serving workloads.
 	SkipFinalizersForPodsSuspendedByParent featuregate.Feature = "SkipFinalizersForPodsSuspendedByParent"
 
-	// owner: @IrvingMg
-	//
-	// issue: https://github.com/kubernetes-sigs/kueue/issues/8585
-	// Enable waiting for WorkloadAdmitted before cleaning up non-selected worker workloads.
-	MultiKueueWaitForWorkloadAdmitted featuregate.Feature = "MultiKueueWaitForWorkloadAdmitted"
-
 	// owner: @mszadkow
 	//
 	// issue: https://github.com/kubernetes-sigs/kueue/issues/8302
@@ -659,9 +653,6 @@ var defaultVersionedFeatureGates = map[featuregate.Feature]featuregate.Versioned
 	SkipFinalizersForPodsSuspendedByParent: {
 		{Version: version.MustParse("0.16"), Default: true, PreRelease: featuregate.Beta},                    // GA in 0.18
 		{Version: version.MustParse("0.18"), Default: true, PreRelease: featuregate.GA, LockToDefault: true}, // remove in 0.20
-	},
-	MultiKueueWaitForWorkloadAdmitted: {
-		{Version: version.MustParse("0.16"), Default: true, PreRelease: featuregate.Beta},                    // GA in 0.18
 	},
 	MultiKueueRedoAdmissionOnEvictionInWorker: {
 		{Version: version.MustParse("0.16"), Default: true, PreRelease: featuregate.Beta},                    // GA in 0.18
