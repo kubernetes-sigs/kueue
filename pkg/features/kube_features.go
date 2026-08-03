e4e9fdb4f578d8334b853052e1da1e5c22e6d3ab
ekam-walia <164983405+ekam-walia@users.noreply.github.com>
2026-08-03 08:21:42 +0530

    cleanup: Removed MultiKueueRedoAdmissionOnEvictionInWorker

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -666,7 +666,6 @@ var defaultVersionedFeatureGates = map[featuregate.Feature]featuregate.Versioned
 	},
 	MultiKueueRedoAdmissionOnEvictionInWorker: {
 		{Version: version.MustParse("0.16"), Default: true, PreRelease: featuregate.Beta},                    // GA in 0.18
-		{Version: version.MustParse("0.18"), Default: true, PreRelease: featuregate.GA, LockToDefault: true}, // remove in 0.20
 	},
 	TLSOptions: {
 		{Version: version.MustParse("0.16"), Default: true, PreRelease: featuregate.Beta},                    // GA in 0.20
