ae0a47856a09651e0dce85428ef7373743c32210
Ekamveer Walia <164983405+ekam-walia@users.noreply.github.com>
2026-08-04 20:20:33 +0530

    Cleanup : dropping MultiKueueWaitForWorkloadAdmitted  (#13731)
    * Cleanup : dropping MultiKueueWaitForWorkloadAdmitted
    
    * should work
    
    * Update site/data/featuregates/removed_feature_list.yaml
    
    Co-authored-by: Mykhailo Bobrovskyi <mikhail.bobrovsky@gmail.com>
    
    * del. MultiKueueWaitForWorkloadAdmitted from .md files
    
    * all suggested changes
    
    * rebase+suggested Changes
    
    * Update site/data/featuregates/removed_feature_list.yaml
    
    Co-authored-by: Mykhailo Bobrovskyi <mikhail.bobrovsky@gmail.com>
    
    ---------
    
    Co-authored-by: Mykhailo Bobrovskyi <mikhail.bobrovsky@gmail.com>

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -251,12 +251,6 @@ const (
 	// Enabled skip adding finalizers for serving workloads.
 	SkipFinalizersForPodsSuspendedByParent featuregate.Feature = "SkipFinalizersForPodsSuspendedByParent"
 
-	// owner: @IrvingMg
-	//
-	// issue: https://github.com/kubernetes-sigs/kueue/issues/8585
-	// Enable waiting for WorkloadAdmitted before cleaning up non-selected worker workloads.
-	MultiKueueWaitForWorkloadAdmitted featuregate.Feature = "MultiKueueWaitForWorkloadAdmitted"
-
 	// owner: @mszadkow
 	//
 	// issue: https://github.com/kubernetes-sigs/kueue/issues/8302
@@ -682,10 +676,6 @@ var defaultVersionedFeatureGates = map[featuregate.Feature]featuregate.Versioned
 		{Version: version.MustParse("0.15"), Default: false, PreRelease: featuregate.Alpha},
 	},
 	SkipFinalizersForPodsSuspendedByParent: {
-		{Version: version.MustParse("0.16"), Default: true, PreRelease: featuregate.Beta},                    // GA in 0.18
-		{Version: version.MustParse("0.18"), Default: true, PreRelease: featuregate.GA, LockToDefault: true}, // remove in 0.20
-	},
-	MultiKueueWaitForWorkloadAdmitted: {
 		{Version: version.MustParse("0.16"), Default: true, PreRelease: featuregate.Beta},                    // GA in 0.18
 		{Version: version.MustParse("0.18"), Default: true, PreRelease: featuregate.GA, LockToDefault: true}, // remove in 0.20
 	},
