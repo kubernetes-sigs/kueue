332c2528be2d2b649dc3183d3a278d9194d4b6c6
vladikkuzn <vladikkuzn1@gmail.com>
2026-08-06 11:55:25 +0300

    fix(pod): drop unused apierrors import in cherry-pick

--- test/integration/singlecluster/controller/jobs/pod/pod_controller_test.go
+++ test/integration/singlecluster/controller/jobs/pod/pod_controller_test.go
@@ -28,7 +28,6 @@ import (
 	"github.com/onsi/gomega"
 	corev1 "k8s.io/api/core/v1"
 	eventsv1 "k8s.io/api/events/v1"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
