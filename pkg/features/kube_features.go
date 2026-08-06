963069467663b35651fba6c9074362c84890b235
vladikkuzn <vladikkuzn1@gmail.com>
2026-08-06 03:58:50 +0300

    chore: version PodIntegrationValidateGroupOwner feature gate for 0.19

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -885,8 +885,10 @@ var defaultVersionedFeatureGates = map[featuregate.Feature]featuregate.Versioned
 
 	SkipAncestorCheckForDeletedWorkloads: {
 		{Version: version.MustParse("0.19"), Default: true, PreRelease: featuregate.Beta},
+	},
+
 	PodIntegrationValidateGroupOwner: {
-		{Version: version.MustParse("0.20"), Default: true, PreRelease: featuregate.Beta},
+		{Version: version.MustParse("0.19"), Default: true, PreRelease: featuregate.Beta},
 	},
 }
 
