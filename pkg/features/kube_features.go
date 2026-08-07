d9905bfaf69cfa4fa05c6a8a8a9a9a0091b94144
nilsachy <nilsachy@google.com>
2026-08-07 09:06:09 +0000

    bump PrioritizeWorkloadsPendingPreemption feature gate version to 0.20

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -844,7 +844,7 @@ var defaultVersionedFeatureGates = map[featuregate.Feature]featuregate.Versioned
 	},
 
 	PrioritizeWorkloadsPendingPreemption: {
-		{Version: version.MustParse("0.19"), Default: false, PreRelease: featuregate.Alpha},
+		{Version: version.MustParse("0.20"), Default: false, PreRelease: featuregate.Alpha},
 	},
 
 	LWSImmutableGroupSize: {
