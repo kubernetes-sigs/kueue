f8a0b3d5af48679e104e0dde9010414fcbc873b4
amirialy <amiribrahim646@gmail.com>
2026-08-08 11:22:50 +0300

    Revert "rayjob: treat ValidationFailed as a terminal state"
    This reverts commit 24e563b3f309b1a2ed632374455b922e18b6bda9.

--- pkg/controller/jobs/rayjob/rayjob_controller.go
+++ pkg/controller/jobs/rayjob/rayjob_controller.go
@@ -214,12 +214,7 @@ func (j *RayJob) RestorePodSetsInfo(podSetsInfo []podset.PodSetInfo) bool {
 func (j *RayJob) Finished(ctx context.Context) (message string, success, finished bool) {
 	message = j.Status.Message
 	success = j.Status.JobStatus == rayv1.JobStatusSucceeded
-
-	finished =
-		j.Status.JobDeploymentStatus == rayv1.JobDeploymentStatusFailed ||
-			j.Status.JobDeploymentStatus == rayv1.JobDeploymentStatusComplete ||
-			j.Status.JobDeploymentStatus == rayv1.JobDeploymentStatusValidationFailed
-
+	finished = j.Status.JobDeploymentStatus == rayv1.JobDeploymentStatusFailed || j.Status.JobDeploymentStatus == rayv1.JobDeploymentStatusComplete
 	return message, success, finished
 }
 
