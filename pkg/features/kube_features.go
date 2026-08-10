a9df3d355a687e045b943d70ee87f6f882f32c79
Dominik Pajak <dominikpajak@google.com>
2026-08-10 09:21:33 +0000

    Set default to false

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -895,7 +895,7 @@ var defaultVersionedFeatureGates = map[featuregate.Feature]featuregate.Versioned
 	},
 
 	RecomputePreemptionTargetsUponOverlap: {
-		{Version: version.MustParse("0.18"), Default: true, PreRelease: featuregate.Alpha},
+		{Version: version.MustParse("0.18"), Default: false, PreRelease: featuregate.Alpha},
 	},
 }
 
