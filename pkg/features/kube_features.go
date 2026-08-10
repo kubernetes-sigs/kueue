19cf58a4213241981cc4ed50fe0e04b0e227b275
Dominik Pająk <pajakd@gmail.com>
2026-08-10 21:12:02 +0200

    Update pkg/features/kube_features.go
    Co-authored-by: Michał Woźniak <mimowo@users.noreply.github.com>

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -916,7 +916,7 @@ var defaultVersionedFeatureGates = map[featuregate.Feature]featuregate.Versioned
 	},
 
 	RecomputeAssignmentUponPreemptionTargetsOverlap: {
-		{Version: version.MustParse("0.19"), Default: true, PreRelease: featuregate.Alpha},
+		{Version: version.MustParse("0.19"), Default: true, PreRelease: featuregate.Beta},
 	},
 }
 
