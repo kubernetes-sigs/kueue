d030ebfd38dbd50323ba8f0bdbbcbf6a7cfdbeec
Jakub Skiba <jakubskiba@google.com>
2026-08-12 12:05:10 +0000

    add effectivequota api for cq and cohort

--- apis/kueue/v1beta2/cohort_types.go
+++ apis/kueue/v1beta2/cohort_types.go
@@ -78,6 +78,14 @@ type CohortStatus struct {
 	// The is recorded only when Fair Sharing is enabled in the Kueue configuration.
 	// +optional
 	FairSharing *FairSharingStatus `json:"fairSharing,omitempty"`
+
+	// effectiveQuota is used for scheduling instead of spec.resourceGroups when
+	// present.
+	//
+	// This field is alpha-level, and is ignored by Kueue when the DynamicQuotaOrchestration
+	// feature gate is disabled.
+	// +optional
+	EffectiveQuota *EffectiveQuotaStatus `json:"effectiveQuota,omitempty"`
 }
 
 // +genclient
--- client-go/applyconfiguration/utils.go
+++ client-go/applyconfiguration/utils.go
@@ -220,6 +220,10 @@ func ForKind(kind schema.GroupVersionKind) interface{} {
 		return &kueuev1beta2.ConcurrentAdmissionMigrationApplyConfiguration{}
 	case v1beta2.SchemeGroupVersion.WithKind("ConcurrentAdmissionPolicy"):
 		return &kueuev1beta2.ConcurrentAdmissionPolicyApplyConfiguration{}
+	case v1beta2.SchemeGroupVersion.WithKind("EffectiveQuotaStatus"):
+		return &kueuev1beta2.EffectiveQuotaStatusApplyConfiguration{}
+	case v1beta2.SchemeGroupVersion.WithKind("EffectiveQuotaStatusManagerRef"):
+		return &kueuev1beta2.EffectiveQuotaStatusManagerRefApplyConfiguration{}
 	case v1beta2.SchemeGroupVersion.WithKind("FairSharing"):
 		return &kueuev1beta2.FairSharingApplyConfiguration{}
 	case v1beta2.SchemeGroupVersion.WithKind("FairSharingStatus"):
