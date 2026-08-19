da96ab7d566f09b90760ba6747439063bcdf94bf
Karol Szuster <szuster@google.com>
2026-08-19 14:30:22 +0000

    Fix time-to-readiness metrics being reported after failure recovery

--- pkg/controller/jobframework/reconciler.go
+++ pkg/controller/jobframework/reconciler.go
@@ -555,7 +555,7 @@ func (r *JobReconciler) ReconcileGenericJob(ctx context.Context, req ctrl.Reques
 				return ctrl.Result{}, client.IgnoreNotFound(err)
 			}
 			// update the metrics only when PodsReady condition status is true
-			if condition.Status == metav1.ConditionTrue {
+			if condition.Status == metav1.ConditionTrue && condition.Reason == kueue.WorkloadStarted {
 				cqName := wl.Status.Admission.ClusterQueue
 				priorityClassName := workloadpatching.PriorityClassName(wl)
 				queuedUntilReadyWaitTime := workload.QueuedWaitTime(wl, r.clock)
