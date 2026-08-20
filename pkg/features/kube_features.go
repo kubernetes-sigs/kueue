e3afcb1d15c712fdd2cedf305dba2d6a7f65348c
Yuki Iwai <yuki.iwai.tz@gmail.com>
2026-08-20 20:05:10 +0900

    Fix PodIntegrationValidateGroupOwner and ValidateRayAndSparkJobUpdates FG versions (#14680)
    Signed-off-by: Yuki Iwai <yuki.iwai.tz@gmail.com>

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -958,11 +958,11 @@ var defaultVersionedFeatureGates = map[featuregate.Feature]featuregate.Versioned
 	},
 
 	PodIntegrationValidateGroupOwner: {
-		{Version: version.MustParse("0.19"), Default: true, PreRelease: featuregate.Beta},
+		{Version: version.MustParse("0.18"), Default: true, PreRelease: featuregate.Beta},
 	},
 
 	ValidateRayAndSparkJobUpdates: {
-		{Version: version.MustParse("0.20"), Default: true, PreRelease: featuregate.Beta}, // GA in 0.21
+		{Version: version.MustParse("0.18"), Default: true, PreRelease: featuregate.Beta}, // GA in 0.21
 	},
 }
 
