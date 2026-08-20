023076a6cd37a58a73ff7f4eb62cc2bc5431f587
nilsachy <nilsachy@google.com>
2026-08-20 12:33:07 +0000

    chore: downgrade PrioritizePreemptorWorkloads feature gate version to 0.18

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -923,7 +923,7 @@ var defaultVersionedFeatureGates = map[featuregate.Feature]featuregate.Versioned
 	},
 
 	PrioritizePreemptorWorkloads: {
-		{Version: version.MustParse("0.20"), Default: false, PreRelease: featuregate.Alpha},
+		{Version: version.MustParse("0.18"), Default: false, PreRelease: featuregate.Alpha},
 	},
 
 	LWSImmutableGroupSize: {
