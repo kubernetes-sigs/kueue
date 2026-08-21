8c260233e387db38e220a5543e43fab9d35ebc52
vladikkuzn <vladikkuzn1@gmail.com>
2026-08-21 17:41:12 +0300

    Allow PodSet minCount 0
    Count already allows 0, so MinCount minimum 1 rejected Count=0 plus
    MinCount=0. Relax OpenAPI Minimum to 0 in v1beta1 and v1beta2, regenerate
    CRDs, and accept minCount=0 in webhook tests.

--- apis/kueue/v1beta1/workload_types.go
+++ apis/kueue/v1beta1/workload_types.go
@@ -304,7 +304,7 @@ type TopologyDomainAssignment struct {
 	Count int32 `json:"count"`
 }
 
-// +kubebuilder:validation:XValidation:rule="has(self.minCount) ? self.minCount <= self.count : true", message="minCount should be positive and less or equal to count"
+// +kubebuilder:validation:XValidation:rule="has(self.minCount) ? self.minCount <= self.count : true", message="minCount should be less or equal to count"
 type PodSet struct {
 	// name is the PodSet name.
 	// +kubebuilder:default=main
@@ -341,7 +341,7 @@ type PodSet struct {
 	// This is an alpha field and requires enabling PartialAdmission feature gate.
 	//
 	// +optional
-	// +kubebuilder:validation:Minimum=1
+	// +kubebuilder:validation:Minimum=0
 	MinCount *int32 `json:"minCount,omitempty"`
 
 	// topologyRequest defines the topology request for the PodSet.
