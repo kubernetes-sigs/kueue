820c576f84455750e8faf9b0af33116a95423ec0
Răzvan-Mihai Hanghicel <rhanghicel@google.com>
2026-08-24 10:26:01 +0000

    metrics: add dedicated metrics for workload recovery wait time
    Signed-off-by: Răzvan-Mihai Hanghicel <rhanghicel@google.com>

--- pkg/controller/jobframework/reconciler.go
+++ pkg/controller/jobframework/reconciler.go
@@ -563,6 +563,10 @@ func (r *JobReconciler) ReconcileGenericJob(ctx context.Context, req ctrl.Reques
 		if !workload.HasConditionWithTypeAndReason(wl, &condition) {
 			log.V(3).Info("Updating the PodsReady condition", "reason", condition.Reason, "status", condition.Status)
 			prevPodsReadyCond := apimeta.FindStatusCondition(wl.Status.Conditions, kueue.WorkloadPodsReady)
+			if prevPodsReadyCond != nil {
+				prevPodsReadyCondCopy := *prevPodsReadyCond
+				prevPodsReadyCond = &prevPodsReadyCondCopy
+			}
 			err := workload.SetConditionAndUpdate(ctx, r.client, wl, condition.Type, condition.Status, condition.Reason, condition.Message, constants.JobControllerName, r.clock)
 			if err != nil {
 				log.Error(err, "Updating workload status")
@@ -592,7 +596,7 @@ func (r *JobReconciler) ReconcileGenericJob(ctx context.Context, req ctrl.Reques
 						}
 					}
 				case kueue.WorkloadRecovered:
-					if prevPodsReadyCond != nil {
+					if prevPodsReadyCond != nil && prevPodsReadyCond.Reason == kueue.WorkloadWaitForRecovery {
 						recoveryWaitTime := condition.LastTransitionTime.Sub(prevPodsReadyCond.LastTransitionTime.Time)
 						metrics.ReportWorkloadRecoveryWaitTime(cqName, priorityClassName, recoveryWaitTime, r.customLabels.CQGet(cqName), r.roleTracker)
 						if r.cache.ShouldExposeLocalQueueMetricsForWorkload(log, wl) {
