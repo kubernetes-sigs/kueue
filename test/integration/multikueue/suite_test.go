6ed542b75d994a422509554fc7b3229bc366d1e3
k8s-infra-cherrypick-robot <90416843+k8s-infra-cherrypick-robot@users.noreply.github.com>
2026-08-29 10:28:04 -0700

    integration: Stop manager before tearing down (#14901)
    Co-authored-by: Jon Huhn <nojnhuh@users.noreply.github.com>

--- test/integration/multikueue/suite_test.go
+++ test/integration/multikueue/suite_test.go
@@ -87,7 +87,7 @@ func (c *cluster) kubeConfigBytes() ([]byte, error) {
 	return utiltesting.RestConfigToKubeConfig(c.cfg)
 }
 
-func (c *cluster) StopAndTeardown() {
+func (c *cluster) stopAndTeardown() {
 	c.fwk.StopManager(c.ctx)
 	c.fwk.Teardown()
 }
@@ -487,7 +487,7 @@ var _ = ginkgo.BeforeSuite(func() {
 })
 
 var _ = ginkgo.AfterSuite(func() {
-	managerTestCluster.StopAndTeardown()
-	worker1TestCluster.StopAndTeardown()
-	worker2TestCluster.StopAndTeardown()
+	managerTestCluster.stopAndTeardown()
+	worker1TestCluster.stopAndTeardown()
+	worker2TestCluster.stopAndTeardown()
 })
