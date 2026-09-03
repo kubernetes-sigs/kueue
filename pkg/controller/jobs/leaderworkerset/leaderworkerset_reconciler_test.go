431ed5dcf3291ca2ac076ae2abcd1dc8a2ef60b9
Nasit Sarwar Sony <nasitsony96@gmail.com>
2026-09-03 11:43:49 -0700

    leaderworkerset: populate WorkloadAnnotation on LeaderWorkerSet-managed Pods
    Signed-off-by: Nasit  Sarwar Sony <nasitsony96@gmail.com>

--- pkg/controller/jobs/leaderworkerset/leaderworkerset_reconciler_test.go
+++ pkg/controller/jobs/leaderworkerset/leaderworkerset_reconciler_test.go
@@ -1343,6 +1343,7 @@ func TestReconciler(t *testing.T) {
 					PrebuiltWorkloadLabel(GetWorkloadName(testLWS, testLWS, "0")).
 					Annotation(podconstants.SuspendedByParentAnnotation, FrameworkName).
 					Annotation(podconstants.RoleHashAnnotation, string(kueue.DefaultPodSetName)).
+					Annotation(kueue.WorkloadAnnotation, GetWorkloadName(testLWS, testLWS, "0")).
 					KueueSchedulingGate().
 					KueueFinalizer().
 					Obj(),
@@ -1422,6 +1423,7 @@ func TestReconciler(t *testing.T) {
 					PrebuiltWorkloadLabel(GetWorkloadName(testLWS, testLWS, "0")).
 					Annotation(podconstants.SuspendedByParentAnnotation, FrameworkName).
 					Annotation(podconstants.RoleHashAnnotation, string(kueue.DefaultPodSetName)).
+					Annotation(kueue.WorkloadAnnotation, GetWorkloadName(testLWS, testLWS, "0")).
 					KueueFinalizer().
 					Obj(),
 			},
@@ -1495,6 +1497,7 @@ func TestReconciler(t *testing.T) {
 					GroupTotalCount("1").
 					PrebuiltWorkloadAnnotation(GetWorkloadName(testLWS, testLWS, "0")).
 					Annotation(podconstants.RoleHashAnnotation, string(kueue.DefaultPodSetName)).
+					Annotation(kueue.WorkloadAnnotation, GetWorkloadName(testLWS, testLWS, "0")).
 					KueueSchedulingGate().
 					Obj(),
 			},
@@ -1893,6 +1896,7 @@ func TestReconciler(t *testing.T) {
 					Label(leaderworkersetv1.GroupIndexLabelKey, "0").
 					Annotation(podconstants.SuspendedByParentAnnotation, FrameworkName).
 					Annotation(podconstants.GroupServingAnnotationKey, podconstants.GroupServingAnnotationValue).
+					Annotation(kueue.WorkloadAnnotation, GetWorkloadName(testLWS, testLWS, "0")).
 					Obj(),
 			},
 		},
@@ -2044,6 +2048,7 @@ func TestReconciler(t *testing.T) {
 					Annotation(podconstants.SuspendedByParentAnnotation, FrameworkName).
 					Annotation(podconstants.GroupServingAnnotationKey, podconstants.GroupServingAnnotationValue).
 					Annotation(podconstants.RoleHashAnnotation, string(kueue.DefaultPodSetName)).
+					Annotation(kueue.WorkloadAnnotation, GetWorkloadName("origin-uid", testLWS, "0")).
 					Obj(),
 			},
 		},
@@ -2114,6 +2119,7 @@ func TestReconciler(t *testing.T) {
 					Annotation(podconstants.SuspendedByParentAnnotation, FrameworkName).
 					Annotation(podconstants.GroupServingAnnotationKey, podconstants.GroupServingAnnotationValue).
 					Annotation(podconstants.RoleHashAnnotation, string(kueue.DefaultPodSetName)).
+					Annotation(kueue.WorkloadAnnotation, GetWorkloadName(testLWS, testLWS, "0")).
 					Obj(),
 			},
 		},
@@ -2186,6 +2192,7 @@ func TestReconciler(t *testing.T) {
 					Annotation(podconstants.GroupServingAnnotationKey, podconstants.GroupServingAnnotationValue).
 					Annotation(leaderworkersetv1.LeaderPodNameAnnotationKey, "lws-0").
 					Annotation(podconstants.RoleHashAnnotation, string(kueue.DefaultPodSetName)).
+					Annotation(kueue.WorkloadAnnotation, GetWorkloadName(testLWS, testLWS, "0")).
 					Obj(),
 			},
 		},
@@ -2290,6 +2297,7 @@ func TestReconciler(t *testing.T) {
 					Annotation(podconstants.SuspendedByParentAnnotation, FrameworkName).
 					Annotation(podconstants.GroupServingAnnotationKey, podconstants.GroupServingAnnotationValue).
 					Annotation(podconstants.RoleHashAnnotation, leaderPodSetName).
+					Annotation(kueue.WorkloadAnnotation, GetWorkloadName(testLWS, testLWS, "0")).
 					Obj(),
 			},
 		},
@@ -2396,6 +2404,7 @@ func TestReconciler(t *testing.T) {
 					Annotation(podconstants.GroupServingAnnotationKey, podconstants.GroupServingAnnotationValue).
 					Annotation(leaderworkersetv1.LeaderPodNameAnnotationKey, "lws-0").
 					Annotation(podconstants.RoleHashAnnotation, workerPodSetName).
+					Annotation(kueue.WorkloadAnnotation, GetWorkloadName(testLWS, testLWS, "0")).
 					Obj(),
 			},
 		},
@@ -2471,6 +2480,7 @@ func TestReconciler(t *testing.T) {
 					Annotation(podconstants.SuspendedByParentAnnotation, FrameworkName).
 					Annotation(podconstants.GroupServingAnnotationKey, podconstants.GroupServingAnnotationValue).
 					Annotation(podconstants.RoleHashAnnotation, string(kueue.DefaultPodSetName)).
+					Annotation(kueue.WorkloadAnnotation, GetWorkloadName(testLWS, testLWS, "0")).
 					Obj(),
 			},
 		},
