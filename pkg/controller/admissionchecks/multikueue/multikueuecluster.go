c9c89473c7a5d1b2a7e79209ab5403ea2d4043fa
Răzvan-Mihai Hanghicel <rhanghicel@google.com>
2026-09-03 08:09:45 +0000

    Removed redundant flowcontrol
    Signed-off-by: Răzvan-Mihai Hanghicel <rhanghicel@google.com>

--- pkg/controller/admissionchecks/multikueue/multikueuecluster.go
+++ pkg/controller/admissionchecks/multikueue/multikueuecluster.go
@@ -47,7 +47,7 @@ import (
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/tools/events"
-	flowcontrol "k8s.io/client-go/util/flowcontrol"
+	"k8s.io/client-go/util/flowcontrol"
 	"k8s.io/client-go/util/workqueue"
 	"k8s.io/klog/v2"
 	"k8s.io/utils/clock"
