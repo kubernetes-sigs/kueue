2d2167bdb3ff22478a3b48181702be1924a7cac6
Nasit  Sarwar Sony <nasitsony96@gmail.com>
2026-09-05 09:21:56 -0700

    fix(test): construct cache/queues before reconcilers to avoid nil-cache panic
    Same nil-cache issue CodeRabbit caught on the LWS suite: pod.NewReconciler
    stored a nil cache since WithCache was appended to opts only after both
    reconcilers were constructed.
    
    Signed-off-by: Nasit  Sarwar Sony <nasitsony96@gmail.com>

--- test/integration/singlecluster/controller/jobs/statefulset/suite_test.go
+++ test/integration/singlecluster/controller/jobs/statefulset/suite_test.go
@@ -18,6 +18,7 @@ package statefulset
 
 import (
 	"context"
+	"slices"
 	"testing"
 
 	"github.com/onsi/ginkgo/v2"
@@ -72,7 +73,7 @@ var _ = ginkgo.AfterSuite(func() {
 func managerSetup(opts ...jobframework.Option) framework.ManagerSetup {
 	return func(ctx context.Context, mgr manager.Manager) {
 		integrationManager := jobcontrollers.NewIntegrationManager()
-		opts = append(opts, jobframework.WithIntegrationManager(integrationManager))
+
 		err := indexer.Setup(ctx, mgr.GetFieldIndexer())
 		gomega.Expect(err).NotTo(gomega.HaveOccurred())
 
@@ -82,12 +83,22 @@ func managerSetup(opts ...jobframework.Option) framework.ManagerSetup {
 		err = statefulset.SetupIndexes(ctx, mgr.GetFieldIndexer())
 		gomega.Expect(err).NotTo(gomega.HaveOccurred())
 
+		cCache := schdcache.New(mgr.GetClient())
+		preemptionExpectations := preemptexpectations.New()
+		queueOptions := []qcache.Option{qcache.WithPreemptionExpectations(preemptionExpectations)}
+		queues := util.NewManagerForIntegrationTests(ctx, mgr.GetClient(), cCache, queueOptions...)
+		setupOpts := append(slices.Clone(opts),
+			jobframework.WithIntegrationManager(integrationManager),
+			jobframework.WithQueues(queues),
+			jobframework.WithCache(cCache),
+		)
+
 		reconciler, err := statefulset.NewReconciler(
 			ctx,
 			mgr.GetClient(),
 			mgr.GetFieldIndexer(),
 			mgr.GetEventRecorder(constants.JobControllerName),
-			opts...)
+			setupOpts...)
 		gomega.Expect(err).NotTo(gomega.HaveOccurred())
 		err = reconciler.SetupWithManager(mgr)
 		gomega.Expect(err).NotTo(gomega.HaveOccurred())
@@ -97,7 +108,7 @@ func managerSetup(opts ...jobframework.Option) framework.ManagerSetup {
 			mgr.GetClient(),
 			mgr.GetFieldIndexer(),
 			mgr.GetEventRecorder(constants.JobControllerName),
-			opts...)
+			setupOpts...)
 		gomega.Expect(err).NotTo(gomega.HaveOccurred())
 		err = podReconciler.SetupWithManager(mgr)
 		gomega.Expect(err).NotTo(gomega.HaveOccurred())
@@ -107,16 +118,10 @@ func managerSetup(opts ...jobframework.Option) framework.ManagerSetup {
 			mgr.GetClient(),
 			mgr.GetFieldIndexer(),
 			mgr.GetEventRecorder(constants.JobControllerName),
-			opts...)
+			setupOpts...)
 		gomega.Expect(err).NotTo(gomega.HaveOccurred())
 		err = podJobReconciler.SetupWithManager(mgr)
 		gomega.Expect(err).NotTo(gomega.HaveOccurred())
-
-		cCache := schdcache.New(mgr.GetClient())
-		preemptionExpectations := preemptexpectations.New()
-		queueOptions := []qcache.Option{qcache.WithPreemptionExpectations(preemptionExpectations)}
-		queues := util.NewManagerForIntegrationTests(ctx, mgr.GetClient(), cCache, queueOptions...)
-		opts = append(opts, jobframework.WithQueues(queues), jobframework.WithCache(cCache))
 
 		configuration := &config.Configuration{}
 		mgr.GetScheme().Default(configuration)
@@ -140,9 +145,9 @@ func managerSetup(opts ...jobframework.Option) framework.ManagerSetup {
 		err = sched.Start(ctx)
 		gomega.Expect(err).NotTo(gomega.HaveOccurred())
 
-		err = statefulset.SetupWebhook(mgr, opts...)
+		err = statefulset.SetupWebhook(mgr, setupOpts...)
 		gomega.Expect(err).NotTo(gomega.HaveOccurred())
-		err = pod.SetupWebhook(mgr, opts...)
+		err = pod.SetupWebhook(mgr, setupOpts...)
 		gomega.Expect(err).NotTo(gomega.HaveOccurred())
 		failedWebhook, err := webhooks.Setup(mgr, nil)
 		gomega.Expect(err).ToNot(gomega.HaveOccurred(), "webhook", failedWebhook)
