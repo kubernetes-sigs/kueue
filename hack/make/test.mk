194ac3b18ca6c851596630ab977586960cfdb2f6
Yuki Iwai <yuki.iwai.tz@gmail.com>
2026-09-11 04:51:33 +0900

    fix make target name
    Signed-off-by: Yuki Iwai <yuki.iwai.tz@gmail.com>

--- hack/make/test.mk
+++ hack/make/test.mk
@@ -369,8 +369,8 @@ test-e2e-upgrade: setup-e2e-env run-test-e2e-upgrade-$(E2E_KIND_VERSION:kindest/
 .PHONY: test-e2e-certmanager-upgrade
 test-e2e-certmanager-upgrade: setup-e2e-env run-test-e2e-certmanager-upgrade-$(E2E_KIND_VERSION:kindest/node:v%=%)
 
-.PHONY: test-e2e-dra
-test-e2e-dra: setup-e2e-env run-test-e2e-dra-baseline-$(E2E_KIND_VERSION:kindest/node:v%=%)
+.PHONY: test-e2e-dra-baseline
+test-e2e-dra-baseline: setup-e2e-env run-test-e2e-dra-baseline-$(E2E_KIND_VERSION:kindest/node:v%=%)
 
 .PHONY: test-e2e-multikueue-dra
 test-e2e-multikueue-dra: setup-e2e-env run-test-e2e-multikueue-dra-$(E2E_KIND_VERSION:kindest/node:v%=%)
