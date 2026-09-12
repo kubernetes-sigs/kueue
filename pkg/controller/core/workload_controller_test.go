312511bfbe029e836c051df973111c1ec60ea2fc
Adib Mubarak <121048412+adibmbrk@users.noreply.github.com>
2026-09-12 19:55:18 +0530

    Consolidate requeue adjusted-resources test into TestReconcileRequeue (#15445)
    * Consolidate requeue adjusted-resources test into TestReconcileRequeue
    
    Move the CPU-limit-as-request accounting scenario from the standalone
    TestRequeueAfterBackoffUsesAdjustedResources into a TestReconcileRequeue
    table case, adding a generic wantPendingCPURequest assertion to the
    shared reconcile test harness.
    
    Signed-off-by: adibmbrk <adibmbrk@gmail.com>
    
    * Addressed review comments
    
    Signed-off-by: adibmbrk <adibmbrk@gmail.com>
    
    * Remove unnecessary cmpopts.EquateEmpty() from pendingWorkloadsCmpOpts
    
    Signed-off-by: adibmbrk <adibmbrk@gmail.com>
    
    ---------
    
    Signed-off-by: adibmbrk <adibmbrk@gmail.com>
    (cherry picked from commit 7d1533b406a7850ce48bc0788b675e1d68f756e2)
    Signed-off-by: adibmbrk <adibmbrk@gmail.com>

--- pkg/controller/core/workload_controller_test.go
+++ pkg/controller/core/workload_controller_test.go
@@ -54,6 +54,7 @@ import (
 	utilindexer "sigs.k8s.io/kueue/pkg/controller/core/indexer"
 	"sigs.k8s.io/kueue/pkg/dra"
 	"sigs.k8s.io/kueue/pkg/features"
+	"sigs.k8s.io/kueue/pkg/resources"
 	preemptexpectations "sigs.k8s.io/kueue/pkg/scheduler/preemption/expectations"
 	afs "sigs.k8s.io/kueue/pkg/util/admissionfairsharing"
 	utilqueue "sigs.k8s.io/kueue/pkg/util/queue"
@@ -404,6 +405,14 @@ var (
 		cmpopts.IgnoreFields(kueue.RequeueState{}, "RequeueAt"),
 		cmpopts.SortSlices(func(a, b metav1.Condition) bool { return a.Type < b.Type }),
 	}
+
+	pendingWorkloadsCmpOpts = cmp.Options{
+		cmpopts.IgnoreFields(workload.Info{},
+			"Obj", "FlavorScanState", "LocalQueueFSUsage", "SecondPassIteration",
+			"LastEvaluatedGeneration", "SchedulingHash", "NominationMapping",
+		),
+		cmp.Comparer(resources.Equal),
+	}
 )
 
 var (
@@ -429,6 +438,7 @@ type reconcileTestCase struct {
 	listErr                   error
 	wantDRAResourceTotal      *int64
 	wantWorkloadsInQueue      *int
+	wantPendingWorkloads      map[kueue.ClusterQueueReference]map[workload.Reference]*workload.Info
 	wantWorkload              *kueue.Workload
 	wantWorkloadUseMergePatch *kueue.Workload // workload version to compensate for the difference between use of Apply and Merge patch in FakeClient
 	wantError                 error
@@ -2070,6 +2080,21 @@ func runReconcileTestCases(t *testing.T, cases map[string]reconcileTestCase, fak
 				}
 				if diff := cmp.Diff(tc.wantEvents, recorder.RecordedEvents); diff != "" {
 					t.Errorf("unexpected events (-want/+got):\n%s", diff)
+				}
+
+				if tc.wantPendingWorkloads != nil {
+					gotPendingWorkloads := make(map[kueue.ClusterQueueReference]map[workload.Reference]*workload.Info)
+					for _, cqName := range qManager.GetClusterQueueNames() {
+						for _, wlInfo := range qManager.PendingWorkloadsInfo(cqName) {
+							if gotPendingWorkloads[cqName] == nil {
+								gotPendingWorkloads[cqName] = make(map[workload.Reference]*workload.Info)
+							}
+							gotPendingWorkloads[cqName][workload.Key(wlInfo.Obj)] = wlInfo
+						}
+					}
+					if diff := cmp.Diff(tc.wantPendingWorkloads, gotPendingWorkloads, pendingWorkloadsCmpOpts...); diff != "" {
+						t.Errorf("unexpected pending workloads (-want,+got):\n%s", diff)
+					}
 				}
 
 				// For DRA tests, verify that workloads are properly queued/cached
