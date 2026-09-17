ce75bd5e14c3be89107e7c1b22e810790cffe505
Kai-Hsun Chen <kaihsun@apache.org>
2026-09-17 13:53:48 -0700

    Document RayCluster worker placement constraint

--- test/e2e/multikueue/extended/ray_autoscaling_test.go
+++ test/e2e/multikueue/extended/ray_autoscaling_test.go
@@ -90,6 +90,8 @@ func runRayClusterAutoscalingTest(
 		FirstWorkerGroupReplicas(0, 0, 2).
 		RayStartParam(rayv1.HeadNode, "num-cpus", "0").
 		RayStartParam(rayv1.WorkerNode, "resources", fmt.Sprintf(`'{%q: 1}'`, workerResource)).
+		// The head and autoscaler request 1250m in total. That keeps the initial
+		// placement off worker2, whose ClusterQueue has only 1200m.
 		RequestAndLimit(rayv1.HeadNode, corev1.ResourceCPU, "750m").
 		RequestAndLimit(rayv1.WorkerNode, corev1.ResourceCPU, "250m").
 		Image(rayv1.HeadNode, util.GetKuberayTestImage(), []string{}).
