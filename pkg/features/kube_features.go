a287f95f0271c34262f246150cd48f16dc420230
Kai-Hsun Chen <kaihsun@apache.org>
2026-09-17 15:18:00 -0700

    Test MultiKueue Ray autoscaling as beta

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -760,7 +760,7 @@ var defaultVersionedFeatureGates = map[featuregate.Feature]featuregate.Versioned
 		{Version: version.MustParse("0.20"), Default: false, PreRelease: featuregate.Alpha},
 	},
 	MultiKueueRayInTreeAutoscaling: {
-		{Version: version.MustParse("0.20"), Default: true, PreRelease: featuregate.Alpha},
+		{Version: version.MustParse("0.20"), Default: true, PreRelease: featuregate.Beta},
 	},
 	TopologyAwareScheduling: {
 		{Version: version.MustParse("0.9"), Default: false, PreRelease: featuregate.Alpha},
