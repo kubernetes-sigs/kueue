1856498744ffbbbd818706fded281c422aa26c12
Kai-Hsun Chen <kaihsun@apache.org>
2026-09-18 01:02:37 -0700

    test: add MultiKueue RayCluster autoscaling E2E coverage (#15725)
    * Add MultiKueue RayCluster autoscaling E2E test
    
    * Limit RayCluster E2E integrations
    
    * Limit RayCluster E2E feature gates
    
    * Simplify RayCluster autoscaling E2E config
    
    * Run RayCluster autoscaling E2E in parallel
    
    * Preserve MultiKueue shard target grouping
    
    * Keep existing MultiKueue shard comment
    
    * Use shared detached Ray actor helpers
    
    * Document RayCluster worker placement constraint
    
    * Verify RayCluster placement supports autoscaling
    
    * Wait for RayCluster readiness before autoscaling

--- hack/make/test.mk
+++ hack/make/test.mk
@@ -223,6 +223,15 @@ test-multikueue-e2e-extended-shard-1: GINKGO_ARGS=--label-filter=feature:kuberay
 test-multikueue-e2e-extended-shard-1: E2E_CONFIG_FOLDER=multikueue/extended-shard-1
 test-multikueue-e2e-extended-shard-1: setup-e2e-env run-test-multikueue-e2e-extended-$(E2E_KIND_VERSION:kindest/node:v%=%)
 
+.PHONY: test-multikueue-e2e-extended-ray-autoscaling
+test-multikueue-e2e-extended-ray-autoscaling: export KUBERAY_VERSION := $(KUBERAY_VERSION)
+test-multikueue-e2e-extended-ray-autoscaling: export RAY_VERSION := $(RAY_VERSION)
+test-multikueue-e2e-extended-ray-autoscaling: export RAYMINI_VERSION := $(RAYMINI_VERSION)
+test-multikueue-e2e-extended-ray-autoscaling: E2E_NPROCS := 5
+test-multikueue-e2e-extended-ray-autoscaling: GINKGO_ARGS=--label-filter=feature:kuberay-multikueue-autoscaling
+test-multikueue-e2e-extended-ray-autoscaling: E2E_CONFIG_FOLDER=multikueue/ray-autoscaling
+test-multikueue-e2e-extended-ray-autoscaling: setup-e2e-env run-test-multikueue-e2e-extended-$(E2E_KIND_VERSION:kindest/node:v%=%) ## Run the extended MultiKueue Ray autoscaling e2e test suite.
+
 .PHONY: test-multikueue-e2e-sequential
 test-multikueue-e2e-sequential: setup-e2e-env kind-secretreader-plugin-image-build run-test-e2e-multikueue-sequential-$(E2E_KIND_VERSION:kindest/node:v%=%) ## Run the sequential MultiKueue e2e test suite.
 
