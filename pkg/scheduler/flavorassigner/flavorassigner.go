61a7150710c94dfe3a3d7d0551df8ba090fc2caa
Yuki Iwai <yuki.iwai.tz@gmail.com>
2026-09-20 06:17:44 +0900

    chore: Remove unused logger from fitsResourceQuota
    Signed-off-by: Yuki Iwai <yuki.iwai.tz@gmail.com>

--- pkg/scheduler/flavorassigner/flavorassigner.go
+++ pkg/scheduler/flavorassigner/flavorassigner.go
@@ -1159,7 +1159,7 @@ func (a *FlavorAssigner) findFlavorForPodSets(
 			// Check considering the flavor usage by previous pod sets.
 			fr := resources.FlavorResource{Flavor: fName, Resource: rName}
 
-			preemptionMode, borrow, s := a.fitsResourceQuota(ctx, log, fr, assignmentUsage[fr], val, resQuota)
+			preemptionMode, borrow, s := a.fitsResourceQuota(ctx, fr, assignmentUsage[fr], val, resQuota)
 			if s != nil {
 				flavorQuotaReasons = append(flavorQuotaReasons, s.reasons...)
 				status.reasons = append(status.reasons, s.reasons...)
@@ -1344,7 +1344,6 @@ func flavorSelector(spec *corev1.PodSpec, allowedKeys sets.Set[string]) nodeaffi
 // could help), it returns a Status with reasons.
 func (a *FlavorAssigner) fitsResourceQuota(
 	ctx context.Context,
-	log logr.Logger,
 	fr resources.FlavorResource,
 	assumedUsage resources.Amount,
 	requestUsage int64,
