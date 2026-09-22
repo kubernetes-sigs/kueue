046cab2a484997acb49d83ee30eeb8e9f28128e3
Kai-Hsun Chen <kaihsun@apache.org>
2026-09-21 22:50:33 -0700

    test: restore fullray label for RayService

--- test/e2e/multikueue/extended/kuberay_test.go
+++ test/e2e/multikueue/extended/kuberay_test.go
@@ -136,7 +136,7 @@ func registerKubeRayTests(contextProvider func() kubeRayTestContext) {
 			})
 		})
 
-		ginkgo.It("Should run a RayService on worker if admitted", func() {
+		ginkgo.It("Should run a RayService on worker if admitted", ginkgo.Label("requires:fullray"), func() {
 			kuberayTestImage := util.GetKuberayTestImage()
 
 			// Create ConfigMap with a simple Ray Serve application
