c35029ad1129b23f236e5fdebabb67d1b4a7fcd9
weizhoublue <weizhou.lan@daocloud.io>
2026-09-22 03:08:10 -0700

    Refresh MultiKueueCluster Active ObservedGeneration after unchanged-status configuration updates (#15989)
    (cherry picked from commit c21fbead4a9902a7bc17ca0fcf1509336303b93d)
    
    Signed-off-by: weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>

--- pkg/controller/admissionchecks/multikueue/multikueuecluster.go
+++ pkg/controller/admissionchecks/multikueue/multikueuecluster.go
@@ -1205,7 +1205,7 @@ func (c *clustersReconciler) updateStatus(ctx context.Context, cluster *kueue.Mu
 
 	// if the condition is up-to-date
 	oldCondition := apimeta.FindStatusCondition(cluster.Status.Conditions, kueue.MultiKueueClusterActive)
-	if cmpConditionState(oldCondition, &newCondition) {
+	if cmpConditionState(oldCondition, &newCondition) && oldCondition.ObservedGeneration == newCondition.ObservedGeneration {
 		return nil
 	}
 
