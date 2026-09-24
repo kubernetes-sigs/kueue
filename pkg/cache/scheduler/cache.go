c7e8c54354ec450a5c97acb09ede604eac601a5a
Sohan Kunkerkar <sohank2602@gmail.com>
2026-09-24 11:32:21 -0400

    TAS: requeue only the ClusterQueues that use the reconciled flavor (#16152) (#16161)

--- pkg/cache/scheduler/cache.go
+++ pkg/cache/scheduler/cache.go
@@ -287,18 +287,6 @@ func (c *Cache) updateClusterQueues(log logr.Logger) sets.Set[kueue.ClusterQueue
 	return cqs
 }
 
-func (c *Cache) ActiveClusterQueues() sets.Set[kueue.ClusterQueueReference] {
-	c.RLock()
-	defer c.RUnlock()
-	cqs := sets.New[kueue.ClusterQueueReference]()
-	for _, cq := range c.hm.ClusterQueues() {
-		if cq.Status == active {
-			cqs.Insert(cq.Name)
-		}
-	}
-	return cqs
-}
-
 func (c *Cache) TASCache() *tasCache {
 	return &c.tasCache
 }
--- pkg/controller/tas/resource_flavor_test.go
+++ pkg/controller/tas/resource_flavor_test.go
@@ -20,11 +20,21 @@ import (
 	"testing"
 	"time"
 
+	"github.com/google/go-cmp/cmp"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
+	kueue "sigs.k8s.io/kueue/apis/kueue/v1beta2"
+	qcache "sigs.k8s.io/kueue/pkg/cache/queue"
+	schdcache "sigs.k8s.io/kueue/pkg/cache/scheduler"
+	preemptexpectations "sigs.k8s.io/kueue/pkg/scheduler/preemption/expectations"
+	utiltesting "sigs.k8s.io/kueue/pkg/util/testing"
+	utiltestingapi "sigs.k8s.io/kueue/pkg/util/testing/v1beta2"
 	testingnode "sigs.k8s.io/kueue/pkg/util/testingjobs/node"
+	"sigs.k8s.io/kueue/pkg/workload"
 )
 
 func TestNodeHandler_Update(t *testing.T) {
@@ -272,6 +282,87 @@ func TestNodeHandler_Update(t *testing.T) {
 			got := checkNodeSchedulingPropertiesChanged(tc.oldNode, tc.newNode)
 			if got != tc.wantChanged {
 				t.Errorf("nodeSchedulingPropertiesChanged() = %v, want %v", got, tc.wantChanged)
+			}
+		})
+	}
+}
+
+func TestRfReconciler_Reconcile(t *testing.T) {
+	testCases := map[string]struct {
+		flavor           string
+		wantInadmissible map[kueue.ClusterQueueReference][]workload.Reference
+	}{
+		"TAS flavor requeues only the ClusterQueues using it": {
+			flavor: "tas-flavor",
+			wantInadmissible: map[kueue.ClusterQueueReference][]workload.Reference{
+				"other-cq": {workload.NewReference("ns", "other-wl")},
+			},
+		},
+		"flavor without a topology requeues nothing": {
+			flavor: "other-flavor",
+			wantInadmissible: map[kueue.ClusterQueueReference][]workload.Reference{
+				"tas-cq":   {workload.NewReference("ns", "tas-wl")},
+				"other-cq": {workload.NewReference("ns", "other-wl")},
+			},
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			ctx, log := utiltesting.ContextWithLog(t)
+			tasFlavor := utiltestingapi.MakeResourceFlavor("tas-flavor").TopologyName("default").Obj()
+			otherFlavor := utiltestingapi.MakeResourceFlavor("other-flavor").Obj()
+			tasCQ := utiltestingapi.MakeClusterQueue("tas-cq").
+				ResourceGroup(*utiltestingapi.MakeFlavorQuotas("tas-flavor").Resource(corev1.ResourceCPU, "1").Obj()).
+				Obj()
+			otherCQ := utiltestingapi.MakeClusterQueue("other-cq").
+				ResourceGroup(*utiltestingapi.MakeFlavorQuotas("other-flavor").Resource(corev1.ResourceCPU, "1").Obj()).
+				Obj()
+			tasLQ := utiltestingapi.MakeLocalQueue("tas-lq", "ns").ClusterQueue("tas-cq").Obj()
+			otherLQ := utiltestingapi.MakeLocalQueue("other-lq", "ns").ClusterQueue("other-cq").Obj()
+			cl := utiltesting.NewClientBuilder().
+				WithObjects(
+					utiltesting.MakeNamespace("ns"),
+					tasFlavor,
+					otherFlavor,
+					tasLQ,
+					otherLQ,
+					utiltestingapi.MakeWorkload("tas-wl", "ns").Queue("tas-lq").Request(corev1.ResourceCPU, "1").Obj(),
+					utiltestingapi.MakeWorkload("other-wl", "ns").Queue("other-lq").Request(corev1.ResourceCPU, "1").Obj(),
+				).
+				Build()
+			cache := schdcache.New(cl)
+			cache.AddOrUpdateTopology(log, utiltestingapi.MakeTopology("default").Levels(corev1.LabelHostname).Obj())
+			cache.AddOrUpdateResourceFlavor(log, tasFlavor)
+			cache.AddOrUpdateResourceFlavor(log, otherFlavor)
+			queues, requeuer := qcache.NewManagerForUnitTestsWithRequeuer(cl, cache, qcache.WithPreemptionExpectations(preemptexpectations.New()))
+			for _, cq := range []*kueue.ClusterQueue{tasCQ, otherCQ} {
+				if err := cache.AddClusterQueue(ctx, cq); err != nil {
+					t.Fatal(err)
+				}
+				if err := queues.AddClusterQueue(ctx, cq); err != nil {
+					t.Fatal(err)
+				}
+			}
+			for _, lq := range []*kueue.LocalQueue{tasLQ, otherLQ} {
+				if err := queues.AddLocalQueue(ctx, lq); err != nil {
+					t.Fatal(err)
+				}
+			}
+			// Setup notifies the ClusterQueues too; drain those so only Reconcile's notifications count.
+			requeuer.ProcessRequeues(ctx)
+			for _, head := range queues.Heads(ctx) {
+				queues.RequeueWorkload(ctx, &head.Info, qcache.RequeueReasonNoFit, qcache.QuotaReservedReasonWaitingForQuota)
+			}
+
+			r := newRfReconciler(cl, queues, cache, nil, nil)
+			if _, err := r.Reconcile(ctx, reconcile.Request{NamespacedName: types.NamespacedName{Name: tc.flavor}}); err != nil {
+				t.Fatal(err)
+			}
+			requeuer.ProcessRequeues(ctx)
+
+			if diff := cmp.Diff(tc.wantInadmissible, queues.DumpInadmissible()); diff != "" {
+				t.Errorf("Unexpected inadmissible workloads (-want,+got):\n%s", diff)
 			}
 		})
 	}
