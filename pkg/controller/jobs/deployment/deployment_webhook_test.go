9338755b8c69de5e0fbb2d34b2fc29b97e975665
Maysa Macedo <maysa.macedo95@gmail.com>
2026-09-25 12:04:44 -0300

    WorkloadLevelWaitForPodsReady: update KEP and expand testing
    This commit updates the KEP to detail the annotation behavior on
    the Deployment integration. Also, it adds two extra tests, one
    integration test that verifies the annotation update on the LeaderWorkerSet
    and one unit test for annotation propagation on the Deployment.

--- pkg/controller/jobs/deployment/deployment_webhook_test.go
+++ pkg/controller/jobs/deployment/deployment_webhook_test.go
@@ -650,6 +650,69 @@ func TestValidateUpdate(t *testing.T) {
 	}
 }
 
+func TestDefaultWaitForPodsReady(t *testing.T) {
+	const (
+		staleWFPR = `{"timeoutSeconds":60,"recoveryTimeoutSeconds":40}`
+		validWFPR = `{"timeoutSeconds":20,"recoveryTimeoutSeconds":20}`
+	)
+	testCases := map[string]struct {
+		deployment *appsv1.Deployment
+		want       *appsv1.Deployment
+	}{
+		"propagates top-level annotation clean up to template": {
+			deployment: testingdeployment.MakeDeployment("test-pod", "").
+				Queue("test-queue").
+				PodTemplateAnnotation(constants.WaitForPodsReadyAnnotation, staleWFPR).
+				Obj(),
+			want: testingdeployment.MakeDeployment("test-pod", "").
+				PodTemplateSpecManagedByKueue().
+				Queue("test-queue").
+				PodTemplateSpecQueue("test-queue").
+				PodTemplateAnnotation(podconstants.SuspendedByParentAnnotation, FrameworkName).
+				Obj(),
+		},
+		"syncs stale template annotation to updated top-level value": {
+			deployment: testingdeployment.MakeDeployment("test-pod", "").
+				Queue("test-queue").
+				SetAnnotation(constants.WaitForPodsReadyAnnotation, validWFPR).
+				PodTemplateAnnotation(constants.WaitForPodsReadyAnnotation, staleWFPR).
+				Obj(),
+			want: testingdeployment.MakeDeployment("test-pod", "").
+				PodTemplateSpecManagedByKueue().
+				Queue("test-queue").
+				SetAnnotation(constants.WaitForPodsReadyAnnotation, validWFPR).
+				PodTemplateSpecQueue("test-queue").
+				PodTemplateAnnotation(podconstants.SuspendedByParentAnnotation, FrameworkName).
+				PodTemplateAnnotation(constants.WaitForPodsReadyAnnotation, validWFPR).
+				Obj(),
+		},
+	}
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			features.SetFeatureGatesDuringTest(t, map[featuregate.Feature]bool{features.WorkloadLevelWaitForPodsReady: true})
+			ctx, _ := utiltesting.ContextWithLog(t)
+			integrationManager := newTestIntegrationManager(t)
+			t.Cleanup(integrationManager.EnableIntegrationsForTest(t, "pod"))
+			builder := utiltesting.NewClientBuilder()
+			client := builder.Build()
+			cqCache := schdcache.New(client)
+			queueManager := qcache.NewManagerForUnitTests(client, cqCache)
+			w := &Webhook{
+				integrationManager: integrationManager,
+				client:             client,
+				queues:             queueManager,
+			}
+
+			if err := w.Default(ctx, tc.deployment); err != nil {
+				t.Errorf("failed to set defaults for v1/deployment: %s", err)
+			}
+			if diff := cmp.Diff(tc.want, tc.deployment); diff != "" {
+				t.Errorf("Default() mismatch (-want,+got):\n%s", diff)
+			}
+		})
+	}
+}
+
 func newTestIntegrationManager(t *testing.T) *jobframework.IntegrationManager {
 	t.Helper()
 	manager := jobframework.NewIntegrationManager()
